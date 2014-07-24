e4ac301a14dff06365242567fc6f0077cf37ccb7
Mike Roup <roup.michael@gmail.com>
2014-07-24 14:21:26 -0400

    add particle selection screen

--- simulation/simulation.cpp
+++ simulation/simulation.cpp
@@ -4,8 +4,11 @@
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
 
+#define WINDOW "select"
+#define REFERENCE "/Users/mike/Desktop/italia/003.tif"
+
 #define FIELDSIZE 400
-#define ITERATION 400
+#define ITERATION 200
 
 #define SPRING_CONSTANT_K -0.1
 #define SPRING_RESTING_X 1
@@ -15,6 +18,7 @@ typedef Particle Force;
 
 Force*** field;
 std::vector<Particle> particle;
+std::vector<cv::Point> click_list;
 
 // based on the interpolation formula from
 // http://paulbourke.net/miscellaneous/interpolation/
@@ -70,12 +74,43 @@ void update_particles() {
     particle[i] += workspace[i];
 }
 
+void add_particle(int event, int x, int y, int flags, void* userdata) {
+  if  (event == cv::EVENT_LBUTTONDOWN)
+    click_list.push_back(cv::Point(x, y));
+}
+
 int main(int argc, char* argv[]) {
 
   if (argc == 1) {
     std::cout << "FEED MEEE" << std::endl;
     exit(EXIT_FAILURE);
   }
+
+  cv::Mat ref = cv::imread(REFERENCE);
+  if(!ref.data) {
+    std::cout << "Error loading reference image" << std::endl;
+    return -1;
+  }
+  cv::namedWindow(WINDOW, 1);
+  cv::setMouseCallback(WINDOW, add_particle, NULL);
+  imshow(WINDOW, ref);
+  cv::waitKey(0);
+  for (int i = 1; i < click_list.size(); ++i) {
+    cv::line(ref, click_list[i-1], click_list[i], cv::Scalar(0,255,0));
+  }
+  imshow(WINDOW, ref);
+  cv::waitKey(0);
+  cv::destroyWindow(WINDOW);
+
+  for (int i = 1; i < click_list.size(); ++i) {
+    cv::LineIterator it(ref, click_list[i-1], click_list[i]);
+    while (it.pos() != click_list[i]) {
+      cv::Point pos = it.pos();
+      particle.push_back(Particle(pos.x, pos.y, 3));
+      it++;
+    }
+  }
+
 
   // allocate and initalize force field
   field = new Force**[FIELDSIZE];
@@ -122,10 +157,10 @@ int main(int argc, char* argv[]) {
   // z=2 is the first index with nonzero
   // force field vectors in my test data
 
-  for (int i = 0; i < FIELDSIZE - 1; ++i) {
-    Particle p(i, 50, 2);
-    particle.push_back(p);
-  }
+  // for (int i = 0; i < FIELDSIZE - 1; ++i) {
+  //   Particle p(i, 50, 2);
+  //   particle.push_back(p);
+  // }
 
   std::cout << std::endl << "running particle simulation" << std::endl;
 
