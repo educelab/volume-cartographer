c8f8be63d406be390b5b21d65eb0cfeffe076724
Seth Parker <csparker247@gmail.com>
2015-04-16 14:16:28 -0400

    (segment) Resumable segmentations now work as expected. Removed distance threshold from available opts because values other than 1 can cause major problems with knowing where to start resumable segmentations.

--- apps/segment.cpp
+++ apps/segment.cpp
@@ -20,7 +20,7 @@ std::string outputName = "";
 
 // Options
 int gravity_scale = -1;
-int threshold = -1;
+int threshold = 1;
 double startIndex = -1.0;
 int stopOffset = -1;
 
@@ -28,7 +28,7 @@ int main(int argc, char* argv[]) {
   std::cout << "vc_segment" << std::endl;
   if (argc < 5) {
     std::cerr << "Usage:" << std::endl;
-    std::cerr << argv[0] << " {--gravity [1-10] --threshold [1-10] --startIndex [Z-Index #] --endAfter [value]} --seg [Seg ID #] --volpkg [volpkgpath]" << std::endl;
+    std::cerr << argv[0] << " {--gravity [1-10] --startIndex [Z-Index #] --stopOffset [value]} --seg [Seg ID #] --volpkg [volpkgpath]" << std::endl;
     exit(EXIT_FAILURE);
   }
 
@@ -40,21 +40,20 @@ int main(int argc, char* argv[]) {
     gravity_scale = 2;
   }
 
-  pcl::console::parse_argument (argc, argv, "--threshold", threshold);
-  if (threshold == -1) {
-    std::cout << "No Distance Threshold value given, defaulting to 1" << std::endl;
-    threshold = 1;
-  }
+  // NOTE: Distance thresholding causes problems for resumable segmentations 
+  //       because you currently need to know the previous step distance.
+  //       Removing until the pipeline is stronger.
+  // pcl::console::parse_argument (argc, argv, "--threshold", threshold);
+  // if (threshold == -1) {
+  //   std::cout << "No Distance Threshold value given, defaulting to 1" << std::endl;
+  //   threshold = 1;
+  // }
 
   pcl::console::parse_argument (argc, argv, "--startIndex", startIndex);
   
-  pcl::console::parse_argument (argc, argv, "--endAfter", stopOffset);
+  pcl::console::parse_argument (argc, argv, "--stopOffset", stopOffset);
 
   pcl::console::parse_argument (argc, argv, "--seg", segID);
-  if (segID == "") {
-    std::cerr << "ERROR: Incorrect/missing segmentation ID!" << std::endl;
-    exit(EXIT_FAILURE);
-  }
   
   pcl::console::parse_argument (argc, argv, "--volpkg", volpkgLocation);
   if (volpkgLocation == "") {
@@ -66,58 +65,66 @@ int main(int argc, char* argv[]) {
   VolumePkg volpkg(volpkgLocation);
   // To-Do: Check to make sure the Seg ID is actually in the volpkg
   volpkg.setActiveSegmentation(segID);
+  if (segID == "") {
+    std::cerr << "ERROR: Incorrect/missing segmentation ID!" << std::endl;
+    exit(EXIT_FAILURE);
+  }
 
 // Setup
   // Load the activeSegmentation's current cloud
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr masterCloud (new pcl::PointCloud<pcl::PointXYZRGB>);
   masterCloud = volpkg.openCloud();
-  int chainLength = masterCloud->width;
 
-  // Setup the cloud filter to generate our starting path for segmentation
+  // Get some info about the cloud, including chain length and z-index's represented by seg.
+  int chainLength = masterCloud->width;
+  int iterations = masterCloud->height;
+  pcl::PointXYZRGB min_p, max_p;
+  pcl::getMinMax3D (*masterCloud, min_p, max_p);
+  int minIndex = floor(masterCloud->points[0].x);
+  int maxIndex = floor(max_p.x);
+
+  // Setup the temp clouds
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr immutableCloud (new pcl::PointCloud<pcl::PointXYZRGB>);
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr segPath (new pcl::PointCloud<pcl::PointXYZRGB>);
 
   // If no start index is given, our starting path is all of the points already on the largest slice index
   if (startIndex == -1.0) {
-    pcl::PointXYZRGB min_p, max_p;
-    pcl::getMinMax3D (*masterCloud, min_p, max_p);
-
-    startIndex = floor(max_p.x);
+    startIndex = maxIndex;
     std::cout << "No starting index given, defaulting to Highest-Z: " << startIndex << std::endl;
   }
 
 
 // Prepare our clouds
-  // Conditions for being a point that is part of the starting path: startIndex <= pt.x < startIndex + 1
-  pcl::ConditionAnd<pcl::PointXYZRGB>::Ptr pathCond (new pcl::ConditionAnd<pcl::PointXYZRGB> ());
-  pathCond->addComparison (pcl::FieldComparison<pcl::PointXYZRGB>::ConstPtr (new pcl::FieldComparison<pcl::PointXYZRGB> ("x", pcl::ComparisonOps::GE, startIndex)));
-  pathCond->addComparison (pcl::FieldComparison<pcl::PointXYZRGB>::ConstPtr (new pcl::FieldComparison<pcl::PointXYZRGB> ("x", pcl::ComparisonOps::LT, startIndex + 1.0)));
-  // build the filter
-  pcl::ConditionalRemoval<pcl::PointXYZRGB> pathFilter;
-  pathFilter.setCondition(pathCond);
-  pathFilter.setInputCloud (masterCloud);  
-  // apply filter
-  pathFilter.filter (*segPath);
+  // Get the starting path pts.
+  // Find our starting row. NOTE: This currently assumes segmentation distance threshold has always been 1
+  int pathRow = startIndex - minIndex;
+  for (int i = 0; i < chainLength; ++i) {
+    pcl::PointXYZRGB temp_pt;
+    temp_pt = masterCloud->points[i+(pathRow*chainLength)];
+    if (temp_pt.x != -1) {
+        segPath->push_back(temp_pt);
+    } 
+  }
 
-  // starting paths must have the same number of points as the input width to maintain ordering
+  // Starting paths must have the same number of points as the input width to maintain ordering
   if ( segPath->width != chainLength ) {
-    std::cerr << std::endl << "ERROR: Starting chain length does not match expected chain length." << std::endl;
+    std::cerr << std::endl;
+    std::cerr << "ERROR: Starting chain length does not match expected chain length." << std::endl;
     std::cerr << "           Expected: " << chainLength << std::endl;
     std::cerr << "           Actual: " << segPath->width << std::endl;
     std::cerr << "       Consider using a lower starting index value." << std::endl << std::endl;
     exit(EXIT_FAILURE);
   }
 
-  // Conditions for being a point before the starting path: pt.x < startIndex
-  pcl::ConditionAnd<pcl::PointXYZRGB>::Ptr staticCond (new pcl::ConditionAnd<pcl::PointXYZRGB> ());
-  staticCond->addComparison (pcl::FieldComparison<pcl::PointXYZRGB>::ConstPtr (new pcl::FieldComparison<pcl::PointXYZRGB> ("x", pcl::ComparisonOps::LT, startIndex)));
-  // build the filter
-  pcl::ConditionalRemoval<pcl::PointXYZRGB> staticFilter;
-  staticFilter.setCondition(staticCond);
-  staticFilter.setInputCloud (masterCloud);
-  // apply filter
-  staticFilter.filter (*immutableCloud);
-
+  // Get the immutable points, i.e all pts before the starting path row
+  for (int i = 0; i < (pathRow * chainLength); ++i) {
+        pcl::PointXYZRGB temp_pt;
+        temp_pt = masterCloud->points[i];
+        immutableCloud->push_back(temp_pt);
+  }
+  immutableCloud->width = chainLength;
+  immutableCloud->height = immutableCloud->points.size()/chainLength;
+  immutableCloud->points.resize (immutableCloud->width * immutableCloud->height);
   
 // Run segmentation using path as our starting points
   pcl::PointCloud<pcl::PointXYZRGB> mutableCloud;
