3064a57097e49bf64b34278402a8bcedeae8531a
Abigail Coleman <abbyc713@gmail.com>
2015-06-26 11:43:56 -0400

    (resampling) Initial commit of resampling addition to MeshUtils.

--- texturing/meshUtils.cpp
+++ texturing/meshUtils.cpp
@@ -86,3 +86,136 @@ itk::Mesh<::itk::Vector<double, 3>, 3>::Pointer smoothNormals ( itk::Mesh<::itk:
         
     return outputMesh;
 }
+
+itk::Mesh<::itk::Vector<double, 3>, 3>::Pointer resample ( itk::Mesh<::itk::Vector<double, 3>, 3>::Pointer inputMesh,
+                                                           int NumberOfSamples, float Gradation ) {
+
+    typedef itk::Vector< double, 3 >  PixelType;  // A vector to hold the normals along with the points of each vertice in the mesh
+    const unsigned int Dimension = 3;   // Need a 3 Dimensional Mesh
+    
+    // declare Mesh object using template parameters 
+    typedef itk::Mesh< PixelType, Dimension >   MeshType;
+            
+    // declare pointer to new Mesh object to be returned
+    MeshType::Pointer  outputMesh = MeshType::New();
+
+    // export mesh to stl, stl needed to import into ACVD's vtkSurface class    
+    itk::STLMeshIOFactory::RegisterOneFactory();
+    typedef itk::MeshFileWriter< MeshType > WriterType;
+    WriterType::Pointer writer = WriterType::New();
+    writer->SetFileName( "temp.stl" );
+    writer->SetInput( inputMesh );
+    try
+    {
+        writer->Update();
+    }
+    catch( itk::ExceptionObject & excp )
+    {
+        std::cerr << excp << std::endl;
+    }
+    itk::STLMeshIO::Pointer meshIO = itk::STLMeshIO::New();
+    meshIO->Print( std::cout );
+   
+    // ACVD's vtkSurface class used for resampling
+    vtkSurface *Mesh = vtkSurface::New();
+    Mesh->CreateFromFile( "temp.stl" );
+    Mesh->GetCellData()->Initialize();
+    Mesh->GetPointData()->Initialize();
+
+    // initialize paramaters needed for ACVD resampling 
+    int SubsamplingThreshold = 10;
+    int QuadricsOptimizationLevel = 1;
+    char outputfile[500];
+    strcpy (outputfile, "simplification.ply");
+
+    vtkIsotropicDiscreteRemeshing *Remesh = vtkIsotropicDiscreteRemeshing::New();
+
+    Remesh->SetInput(Mesh);
+    Remesh->SetFileLoadSaveOption(0);
+    Remesh->SetNumberOfClusters(NumberOfSamples);
+    Remesh->SetConsoleOutput(2);
+    Remesh->SetSubsamplingThreshold(SubsamplingThreshold);
+    Remesh->GetMetric()->SetGradation(Gradation);
+    Remesh->Remesh();
+
+    // code from ACVD.cxx example, optimization of resampled mesh
+    if (QuadricsOptimizationLevel != 0) {
+        // Note : this is an adaptation of Siggraph 2000 Paper :
+        // Out-of-core simplification of large polygonal models
+        vtkIntArray *Clustering = Remesh->GetClustering();
+
+        char REALFILE[5000];
+        char FileBeforeProcessing[500];
+        strcpy (FileBeforeProcessing,"smooth_");
+        strcat (FileBeforeProcessing, outputfile);
+        strcpy (REALFILE, FileBeforeProcessing);
+        Remesh->GetOutput()->WriteToFile(REALFILE);
+
+        int Cluster,NumberOfMisclassedItems = 0;
+
+        double **ClustersQuadrics = new double*[NumberOfSamples];
+        for (int i = 0; i < NumberOfSamples; i++) {
+            ClustersQuadrics[i] = new double[9];
+            for (int j = 0; j < 9; j++) {
+                ClustersQuadrics[i][j] = 0;
+            }
+        }
+        vtkIdList *FList = vtkIdList::New();
+        for (int i = 0; i < Remesh->GetNumberOfItems (); i++) {
+            Cluster = Clustering->GetValue (i);
+            if ((Cluster >= 0)&& (Cluster < NumberOfSamples)) {
+                if (Remesh->GetClusteringType() == 0) {
+                    vtkQuadricTools::AddTriangleQuadric(
+                            ClustersQuadrics[Cluster], Remesh->GetInput(), i, false);
+                } else {
+                    Remesh->GetInput()->GetVertexNeighbourFaces(i, FList);
+                    for (int j = 0;j < FList->GetNumberOfIds(); j++)
+                        vtkQuadricTools::AddTriangleQuadric(
+                                ClustersQuadrics[Cluster], Remesh->GetInput(), FList->GetId(j), false);
+                }
+            } else {
+                NumberOfMisclassedItems++;
+            }
+        }
+        FList->Delete();
+
+        if (NumberOfMisclassedItems) {
+            cout << NumberOfMisclassedItems << " Items with wrong cluster association" << endl;
+        }
+
+        double P[3];
+        for (int i = 0; i < NumberOfSamples; i++) {
+            Remesh->GetOutput()->GetPoint (i, P);
+            vtkQuadricTools::ComputeRepresentativePoint(ClustersQuadrics[i], P, QuadricsOptimizationLevel);
+            Remesh->GetOutput()->SetPointCoordinates (i, P);
+            delete[] ClustersQuadrics[i];
+        }
+        delete [] ClustersQuadrics;
+
+        Mesh->GetPoints()->Modified ();
+
+        cout << "After Quadrics Post-processing : " << endl;
+        Remesh->GetOutput()->DisplayMeshProperties();
+
+    }
+
+    // save the output mesh to .ply format
+    char REALFILE[500];
+    strcpy (REALFILE, "");
+    strcat (REALFILE, outputfile);
+
+    Remesh->GetOutput()->WriteToFile(REALFILE);
+
+    Remesh->Delete();
+    Mesh->Delete();
+
+    // Read in resampled mesh and return it
+    // try to convert the ply to an ITK mesh
+    int meshWidth = -1;
+    int meshHeight = -1;
+    if ( !ply2itkmesh("simplification.ply", outputMesh, meshWidth, meshHeight) ) {
+        exit( -1 );
+    };
+
+    return outputMesh;
+}
--- texturing/meshUtils.h
+++ texturing/meshUtils.h
@@ -11,11 +11,19 @@
 #include <itkTriangleCell.h>
 #include "itkPointsLocator.h"
 #include "itkMapContainer.h"
+#include <itkMeshFileWriter.h>
+#include <itkSTLMeshIOFactory.h>
+#include <itkSTLMeshIO.h>
+
+#include "vtkIsotropicDiscreteRemeshing.h"
+#include "plyHelper.h"
 
 #ifndef VC_MESHUTILS_H
 #define VC_MESHUTILS_H
 
 itk::Mesh<::itk::Vector<double, 3>, 3>::Pointer smoothNormals ( itk::Mesh<::itk::Vector<double, 3>, 3>::Pointer inputMesh,
                                                                 double smoothingFactor );
+
+itk::Mesh<::itk::Vector<double, 3>, 3>::Pointer resample ( itk::Mesh<::itk::Vector<double, 3>, 3>::Pointer inputMesh );
 
 #endif // VC_MESHUTILS_H
