9b005157a5d11567443165208a422a025de2e2b8
Mike Roup <roup.michael@gmail.com>
2015-07-18 15:00:18 -0400

    herp derp

--- texturing/main.cpp
+++ texturing/main.cpp
@@ -20,6 +20,17 @@
 
 #define SAY(x) std::cout << x << std::endl
 
+#define SKIPCELL do {                           \
+    ++cellIterator;                             \
+    if (cellIterator == cellEnd)                \
+      goto hell;                                \
+                                                \
+    ++cellIterator;                             \
+    if (cellIterator == cellEnd)                \
+      goto hell;                                \
+    pass_counter++;                             \
+  } while (0)
+
 int main(int argc, char* argv[])
 {
   if ( argc < 6 ) {
@@ -63,7 +74,7 @@ int main(int argc, char* argv[])
 
   int aSampleDir = atoi( argv[ 6 ] ); // sampleDirection (0=omni, 1=positive, 2=negative)
   EDirectionOption aDirectionOption = ( EDirectionOption )aSampleDir;
-    
+
   // declare pointer to new Mesh object
   VC_MeshType::Pointer  mesh = VC_MeshType::New();
 
@@ -93,7 +104,7 @@ int main(int argc, char* argv[])
   // [u, v] == point's position in the output matrix
   unsigned long pointID, meshX, meshY;
   double u, v;
-    
+
   // Load the slices from the volumepkg
   std::vector< cv::Mat > aImgVol;
 
@@ -126,28 +137,6 @@ int main(int argc, char* argv[])
   }
   std::cout << std::endl;
 
-  // Generate per point uv coordinates specifically for the OBJ
-  // VC_UVMap uvMap;
-  // std::cerr << "Calculating UV coordinates for final mesh..." << std::endl;
-  // for ( VC_PointsInMeshIterator point = mesh->GetPoints()->Begin(); point != mesh->GetPoints()->End(); ++point ) {
-  //   pointID = point.Index();
-
-  //   // Calculate the point's [meshX, meshY] position based on its pointID
-  //   meshX = pointID % meshWidth;
-  //   meshY = (pointID - meshX) / meshWidth;
-
-  //   // Calculate the point's UV position
-  //   u =  (double) meshX / (double) meshWidth;
-  //   v =  (double) meshY / (double) meshHeight;
-
-  //   // OBJ UV coordinates start in the bottom-left of an image, but we're calculating them from the top-left
-  //   // Subtract v from 1.0 to account for this
-  //   cv::Vec2d uv( u, 1.0 - v );
-
-  //   // Add the uv coordinates into our map at the point index specified
-  //   uvMap.insert( {pointID, uv} );
-  // }
-
   // Initialize iterators
   VC_CellIterator  cellIterator = mesh->GetCells()->Begin();
   VC_CellIterator  cellEnd      = mesh->GetCells()->End();
@@ -155,148 +144,168 @@ int main(int argc, char* argv[])
   VC_CellType *    next_cell;
   VC_PointsInCellIterator pointsIterator;
 
-  double total_dist = 0;
-  for (int i = 0; i < (meshWidth / 2); ++i) {
-    VC_CellType* cell;
-    cell = cellIterator.Value();
-    pointsIterator = cell->PointIdsBegin();
+  // double total_dist = 0;
+  // for (int i = 0; i < (meshWidth / 2); ++i) {
+  //   VC_CellType* cell;
+  //   cell = cellIterator.Value();
+  //   pointsIterator = cell->PointIdsBegin();
 
-    unsigned long left_pointID = *pointsIterator;
-    VC_PointType p = mesh->GetPoint(left_pointID);
+  //   unsigned long left_pointID = *pointsIterator;
+  //   VC_PointType p = mesh->GetPoint(left_pointID);
 
-    cv::Vec3f left(p[0],p[1],p[2]);
+  //   cv::Vec3f left(p[0],p[1],p[2]);
 
-    ++cellIterator;
-    ++cellIterator;
-    next_cell = cellIterator.Value();        
-    pointsIterator = next_cell->PointIdsBegin();
-    unsigned long right_pointID = *pointsIterator;
-    VC_PointType next_p = mesh->GetPoint(right_pointID);
+  //   ++cellIterator;
+  //   ++cellIterator;
+  //   next_cell = cellIterator.Value();
+  //   pointsIterator = next_cell->PointIdsBegin();
+  //   unsigned long right_pointID = *pointsIterator;
+  //   VC_PointType next_p = mesh->GetPoint(right_pointID);
 
 
-    cv::Vec3f right(next_p[0],next_p[1],next_p[2]);
+  //   cv::Vec3f right(next_p[0],next_p[1],next_p[2]);
 
-    cv::Vec3f offset = (right - left);
-    total_dist += sqrt(offset.dot(offset));
-  }
+  //   cv::Vec3f offset = (right - left);
+  //   total_dist += sqrt(offset.dot(offset));
+  // }
 
-  int outputWidth = total_dist / step_size; // IMPORTANTE
-  SAY(outputWidth);
+  // int outputWidth = total_dist / step_size; // IMPORTANTE
 
-  cellIterator = mesh->GetCells()->Begin();
-  cv::Mat outputTexture = cv::Mat::zeros( textureH, outputWidth + 20, CV_16UC1 );  // maybe add buffer here
+  cv::Mat outputTexture = cv::Mat::zeros(textureH, textureW, CV_16UC1);  // maybe add buffer here
 
   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  cell = cellIterator.Value();
-  pointsIterator = cell->PointIdsBegin();
-  unsigned long left_pointID = *pointsIterator;
-  VC_PointType p = mesh->GetPoint(left_pointID);
-  cv::Vec3f current(p[0],p[1],p[2]);
-  
-  // Iterate over all of the cells to lay out the faces in the output texture
-  int row_counter = 0;
-  int width_counter = 0;
-  while( cellIterator != cellEnd ) {
-    goto heaven;
-  hell:
-    SAY("SEE YA");
-    break;
-  heaven:
-    
-    // Link the pointer to our current cell
-    if (cellIterator == cellEnd)
-      goto hell;
+  std::vector<std::vector<cv::Vec3f> > stuff;
+  for (int i = 0; i < meshHeight; ++i) {
+    std::vector<cv::Vec3f> row;
 
-    ++cellIterator;
-    if (cellIterator == cellEnd)
-      goto hell;
-    
-    ++cellIterator;
-    if (cellIterator == cellEnd)
-      goto hell;
-    next_cell = cellIterator.Value();        
-    pointsIterator = next_cell->PointIdsBegin();
-    unsigned long right_pointID = *pointsIterator;
-    VC_PointType next_p = mesh->GetPoint(right_pointID);
-    cv::Vec3f next(next_p[0],next_p[1],next_p[2]);
-
-    cv::Vec3f offset = (next - current);
-    double distance = sqrt(offset.dot(offset));
-
-
-    double remainder = step_size;
-    while (distance < remainder) {
-      remainder = remainder - distance;
-      current = next;
-      ++cellIterator;
+    for (int k = 0; k < meshWidth - 1; ++k) {
+      cell = cellIterator.Value();
+      pointsIterator = cell->PointIdsBegin();
+      unsigned long left_pointID = *pointsIterator;
+      VC_PointType p = mesh->GetPoint(left_pointID);
+      row.push_back(cv::Vec3f(p[0],p[1],p[2]));
+      cellIterator++;
       if (cellIterator == cellEnd)
-        goto hell;
-      ++cellIterator;
+        break;
+      cellIterator++;
       if (cellIterator == cellEnd)
-        goto hell;
-      next_cell = cellIterator.Value();        
-      pointsIterator = next_cell->PointIdsBegin();
-      right_pointID = *pointsIterator;
-      next_p = mesh->GetPoint(right_pointID);
-      next = cv::Vec3f(next_p[0],next_p[1],next_p[2]);
-
-      offset = (next - current);
-      distance = sqrt(offset.dot(offset));
+        break;
     }
-
-    cv::Vec3f CORRECT_POINT = offset + current;
-    current = next;
-        
-    std::cout << "Texturing face " << cellIterator.Index() << "/" << cellEnd.Index() << "\r" << std::flush;
-
-    VC_PixelType left_normal;
-    VC_PixelType right_normal;
-    mesh->GetPointData( left_pointID, &left_normal );
-    mesh->GetPointData( right_pointID, &right_normal );
-
-    //just average these, not the best way
-    cv::Vec3f NORMAL((left_normal[0] + right_normal[0]) / 2,
-                     (left_normal[1] + right_normal[1]) / 2,
-                     (left_normal[2] + right_normal[2]) / 2);
-
-    // Calculate the point's [meshX, meshY] position based on its pointID
-    // meshX = pointID % outputWitdh;
-    // meshY = (pointID - meshX) / outputWitdh;
-
-    // Calculate the point's pixel position in the output texture
-    // u = (double)outputWidth * (double)width_counter / (double)meshWidth;
-    // v = (double)textureH * (double)row_counter / (double)meshHeight;
-    u = width_counter;
-    v = row_counter;
-
-    // Fill in the output pixel with a value
-    // cv::Mat.at uses (row, column)
-    double value = textureWithMethod( CORRECT_POINT,
-                                      NORMAL,
-                                      aImgVol,
-                                      aFilterOption,
-                                      radius,
-                                      minorRadius,
-                                      0.5,
-                                      aDirectionOption);
-    outputTexture.at<unsigned short>((int)v, (int)u) = (unsigned short)value;
-
-    width_counter++;
-    if (width_counter == outputWidth) {
-      row_counter++;
-      width_counter = 0;
-    }
-
+    
+    stuff.push_back(row);
+    if (cellIterator == cellEnd)
+      break;
   }
-  std::cout << std::endl;
+  
+  for (int i = 0; i < stuff.size(); ++i) {
+    for (int x = 0; x < stuff[i].size(); ++x) {
+      double value = textureWithMethod( stuff[i][x],
+                                        cv::Vec3f(1,1,1),
+                                        aImgVol,
+                                        aFilterOption,
+                                        radius,
+                                        minorRadius,
+                                        0.5,
+                                        aDirectionOption);
+      outputTexture.at<unsigned short>(i, x) = (unsigned short)value;
+    }
+  }
+  
+vpkg.saveTextureData(outputTexture);
 
-  // volcart::io::objWriter objwriter("textured.obj", mesh, uvMap, outputTexture);
+exit(EXIT_FAILURE);
 
-  // objwriter.write();
+// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+//   cellIterator = mesh->GetCells()->Begin();
+//   while( cellIterator != cellEnd ) {
+//     goto heaven;
+//   hell:
+//     SAY("SEE YA");
+//     break;
+//   heaven:
 
-  vpkg.saveTextureData(outputTexture);
+//     if (cellIterator == cellEnd)                
+//       goto hell;                                
+//     cell = cellIterator.Value();
+//     pointsIterator = cell->PointIdsBegin();
+//     unsigned long left_pointID = *pointsIterator;
+//     VC_PointType p = mesh->GetPoint(left_pointID);
+//     left = cv::Vec3f(p[0],p[1],p[2]);
+//     pass_counter++;
 
-  return 0;
+//     SKIPCELL;
+//     next_cell = cellIterator.Value();
+//     pointsIterator = next_cell->PointIdsBegin();
+//     unsigned long right_pointID = *pointsIterator;
+//     VC_PointType next_p = mesh->GetPoint(right_pointID);
+//     right = cv::Vec3f(next_p[0],next_p[1],next_p[2]);
+
+//     cv::Vec3f offset = (right - left);
+//     double distance = sqrt(offset.dot(offset));
+
+//     // FOR WHEN WE'RE STILL IN THE SAME ROW
+//   restart:
+//     double delta = step_size;
+//     while (distance < delta) {
+//       delta = delta - distance;
+//       left = right;
+//       left_pointID = right_pointID;
+
+//       // ONE NEW POINT
+//       SKIPCELL;
+//       next_cell = cellIterator.Value();
+//       pointsIterator = next_cell->PointIdsBegin();
+//       right_pointID = *pointsIterator;
+//       next_p = mesh->GetPoint(right_pointID);
+//       right = cv::Vec3f(next_p[0],next_p[1],next_p[2]);
+
+//       // CALCULATE OFFSET AGAIN
+//       offset = (right - left);
+//       distance = sqrt(offset.dot(offset));
+//     }
+//     cv::normalize(offset,offset,delta);
+//     cv::Vec3f CORRECT_POINT = offset + left;
+//     left = CORRECT_POINT;
+
+//     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//     std::cout << "Texturing face " << cellIterator.Index() << "/" << cellEnd.Index() << "\r" << std::flush;
+
+//     VC_PixelType left_normal;
+//     VC_PixelType right_normal;
+//     mesh->GetPointData( left_pointID, &left_normal );
+//     mesh->GetPointData( right_pointID, &right_normal );
+
+//     //just average these, not the best way
+//     cv::Vec3f NORMAL((left_normal[0] + right_normal[0]) / 2,
+//                      (left_normal[1] + right_normal[1]) / 2,
+//                      (left_normal[2] + right_normal[2]) / 2);
+
+//     // Fill in the output pixel with a value
+//     // cv::Mat.at uses (row, column)
+//     double value = textureWithMethod( CORRECT_POINT,
+//                                       NORMAL,
+//                                       aImgVol,
+//                                       aFilterOption,
+//                                       radius,
+//                                       minorRadius,
+//                                       0.5,
+//                                       aDirectionOption);
+//     outputTexture.at<unsigned short>(row_counter, width_counter) = (unsigned short)value;
+
+//     width_counter++;
+//     if (width_counter == outputWidth || pass_counter == meshWidth) {
+//       row_counter++;
+//       pass_counter = 0;
+//       width_counter = 0;
+//     } else {
+//       goto restart;
+//     }
+//   }
+//   std::cout << std::endl;
+
+vpkg.saveTextureData(outputTexture);
+
+return 0;
 } // end main
-
