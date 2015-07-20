a26074dbba549beb75a81609fefc68f0338ac95f
Mike Roup <roup.michael@gmail.com>
2015-07-20 10:34:30 -0400

    (crunch time) cleanup + add comments

--- texturing/main.cpp
+++ texturing/main.cpp
@@ -18,8 +18,6 @@
 
 #include "UPointMapping.h"
 
-#define SAY(x) std::cout << x << std::endl
-
 #define SKIPCELL do {                           \
     ++cellIterator;                             \
     if (cellIterator == cellEnd)                \
@@ -144,14 +142,18 @@ int main(int argc, char* argv[])
   VC_CellType *    next_cell;
   VC_PointsInCellIterator pointsIterator;
 
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
+  // Get vertices and normals from mesh
+  // Vector of vectors used to organize data
+  // so we don't have to mess with itk
   std::vector<std::vector<cv::Vec3f> > points;
   std::vector<std::vector<cv::Vec3f> > normals;
   for (int i = 0; i < meshHeight; ++i) {
     std::vector<cv::Vec3f> point_row;
     std::vector<cv::Vec3f> normal_row;
 
+    // look at a row of the mesh
     for (int k = 0; k < meshWidth - 1; ++k) {
       cell = cellIterator.Value();
       pointsIterator = cell->PointIdsBegin();
@@ -163,6 +165,7 @@ int main(int argc, char* argv[])
       point_row.push_back(cv::Vec3f(p[0],p[1],p[2]));
       normal_row.push_back(cv::Vec3f(n[0],n[1],n[2]));
 
+      // We only look at every other cell to avoid duplication
       cellIterator++;
       if (cellIterator == cellEnd)
         break;
@@ -170,66 +173,78 @@ int main(int argc, char* argv[])
       if (cellIterator == cellEnd)
         break;
     }
-    
+
     points.push_back(point_row);
     normals.push_back(normal_row);
     if (cellIterator == cellEnd)
       break;
   }
 
-  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // calculate running length of line segments for each row
   std::vector<std::vector<double> > difference_array;
   for (int i = 0; i < points.size(); ++i) {
     std::vector<double> diff_row;
 
     for (int x = 0; x < points[i].size()-1; ++x) {
 
+      // find the distance between neighboring points
+      //  | b - a | = sqrt ((b - a) . (b - a))
       cv::Vec3f offset = points[i][x+1] - points[i][x];
       double len = sqrt(offset.dot(offset));
 
       diff_row.push_back(len);
 
+      // add the distance to total distance of the last point
+      // if we aren't looking at the first point
       if (x != 0) {
         diff_row[x] += diff_row[x-1];
       }
     }
-    
+
     difference_array.push_back(diff_row);
   }
-  
+
+  // total length of the first row of points
   double real_chain_length = difference_array[0][difference_array[0].size() - 1];
 
-  SAY(real_chain_length);
-  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-  
+  // create an output texture with a little wiggle room
   cv::Mat outputTexture = cv::Mat::zeros(textureH, (int)real_chain_length+20, CV_16UC1);
 
   for (int i = 0; i < points.size(); ++i) {
     for (int x = 0; x < (int)real_chain_length; ++x) {
+      // we want to make sure this will work with a step size
+      // of one before we get fancy
       double delta = x;
+      std::cout << "delta " << delta << std::endl;
+
+      // increase the index until the interpolated position is between
+      // two existing points (by their accumulated distance)
       int upper_bound = 0;
-      int lower_bound;
       while (delta > difference_array[i][upper_bound]) {
         ++upper_bound;
       }
-      lower_bound = upper_bound - 1;
+      int lower_bound = upper_bound - 1;
 
+      // find out how far the new point is past the low index
+      //  new_x - low_x
+      // ---------------
+      // high_x - low_x
+      double scale_factor = (delta - difference_array[i][lower_bound]) / (difference_array[i][upper_bound] - difference_array[i][lower_bound]);
+
+      // points from mesh that are around the interpolated point
       cv::Vec3f left = points[i][lower_bound];
       cv::Vec3f right = points[i][upper_bound];
-      
-      cv::Vec3f l2r = (right - left);
-      // double length_of_l2r = sqrt(l2r.dot(l2r));
-      double scale_factor = (delta - lower_bound) / (difference_array[i][upper_bound] - difference_array[i][lower_bound]);
-      std::cout << scale_factor << std::endl;
 
+      // find the new point
+      // t*(p1 - p0) + p0
+      cv::Vec3f CORRECT_POINT = scale_factor * (right - left) + left;
+
+      // normal vectors // we're just averaging the normals for now
       cv::Vec3f normal_left = normals[i][lower_bound];
       cv::Vec3f normal_right = normals[i][upper_bound];
-      
-
-      cv::Vec3f CORRECT_POINT = scale_factor * l2r + left;
       cv::Vec3f CORRECT_NORMAL = (normal_right + normal_left) / 2;
 
+      // texture the point like normal and add it to the output image
       double value = textureWithMethod( CORRECT_POINT,
                                         CORRECT_NORMAL,
                                         aImgVol,
@@ -238,10 +253,10 @@ int main(int argc, char* argv[])
                                         minorRadius,
                                         0.5,
                                         aDirectionOption);
-      outputTexture.at<unsigned short>(i, x) = (unsigned short)value;      
+      outputTexture.at<unsigned short>(i, x) = (unsigned short)value;
     }
   }
-  
+
   vpkg.saveTextureData(outputTexture);
 
   return 0;
