ceb61c9116174f48bbcf6b7c75dfe297fe66c981
Sean Karlage <sean.karlage@uky.edu>
2015-10-05 11:45:59 -0400

    Adding new chainmesh class to handle storing the positions of each chain

---
+++ segmentation/localResliceParticleSim/chainmesh.cpp
@@ -0,0 +1,28 @@
+//
+// Created by skarlage on 10/5/15.
+//
+
+#include "chainmesh.h"
+
+using namespace volcart::segmentation;
+
+ChainMesh::ChainMesh() : height_(0), width_(0) { }
+
+// NOTE: OpenCV does rows first, then columns, so need to flip the order of width/height when passing to Mat constructor
+ChainMesh::ChainMesh(uint32_t width, uint32_t height) : width_(width), height_(height) {
+    positions_ = cv::Mat(height, width, CV_32FC3);
+}
+
+// Pushes a chain back into the ChainMesh
+void ChainMesh::addChain(Chain row) {
+    for (auto i = 0; i < row.particleCount(); ++i) {
+        for (auto component = VC_INDEX_X; component <= VC_INDEX_Z; ++component) {
+            positions_.at(row.zIndex, i)(component) = row.at(i);
+        }
+    }
+}
+
+// Export the mesh as an ordered PCD
+pcl::PointCloud<pcl::PointXYZRGB> exportAsOrderedPCD() const {
+    // TODO: Implement
+}
---
+++ segmentation/localResliceParticleSim/chainmesh.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#ifndef VC_CHAINMESH_H
+#define VC_CHAINMESH_H
+
+#include <opencv2/core/mat.hpp>
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+
+#include "chain.h"
+
+namespace volcart {
+
+namespace segmentation {
+
+class ChainMesh {
+public:
+    ChainMesh();
+
+    ChainMesh(uint32_t width, uint32_t height);
+
+    pcl::PointCloud<pcl::PointXYZRGB> exportAsOrderedPCD() const;
+
+    void addChain(Chain c);
+
+    cv::Mat positions() const { return positions_; }
+
+    uint32_t width() const { return width_; }
+
+    uint32_t height() const { return height_; }
+
+private:
+    cv::Mat positions_;
+    uint32_t width_;
+    uint32_t height_;
+
+};
+
+}
+
+}
+
+#endif //VC_CHAINMESH_H
