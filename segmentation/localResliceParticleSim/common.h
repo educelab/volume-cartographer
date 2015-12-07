eb3b7b37db891b1abacc864bd91e98bf0c5f90f4
Sean Karlage <sean.karlage@uky.edu>
2015-12-07 17:58:25 -0500

    Added cout for pairs

--- segmentation/localResliceParticleSim/common.h
+++ segmentation/localResliceParticleSim/common.h
@@ -43,10 +43,21 @@ enum Direction {
 }
 
 // Helpful for printing out vector. Only for debug.
+template <typename T1, typename T2>
+std::ostream& operator<<(std::ostream& s, std::pair<T1, T2> p)
+{
+    return s << "(" << std::get<0>(p) << ", " << std::get<1>(p) << ")";
+}
+
 template <typename T>
 std::ostream& operator<<(std::ostream& s, std::vector<T> v)
 {
     s << "[";
+    if (v.size() == 0) {
+        return s << "]";
+    } else if (v.size() == 1) {
+        return s << v[0] << "]";
+    }
     // Need - 2 because v.end() points to one past the end of v.
     std::for_each(v.begin(), v.end() - 2, [&s](const T& t) { s << t << ", "; });
     return s << *(v.end() - 1) << "]";
