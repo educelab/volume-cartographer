0ad58f4456b6f6d980a8cee5e3ef24356787f1af
Sean Karlage <sean.karlage@uky.edu>
2015-12-07 12:45:05 -0500

    Fixed vector sizing issues

--- segmentation/localResliceParticleSim/localResliceParticleSim.cpp
+++ segmentation/localResliceParticleSim/localResliceParticleSim.cpp
@@ -18,7 +18,7 @@ LocalResliceSegmentation::segmentLayer(const bool showVisualization,
                                        const int32_t startIndex,
                                        const int32_t endIndex,
                                        const int32_t stepNumLayers,
-                                       const double derivativeTolerance,
+                                       const double  derivativeTolerance,
                                        const int32_t keepNumMaxima,
                                        const int32_t numRandomTries)
 {
@@ -48,46 +48,48 @@ LocalResliceSegmentation::segmentLayer(const bool showVisualization,
          sliceIndex += stepNumLayers) {
 
         // First chain object
-        std::unique_ptr<Chain> currentChain(new Chain(pkg_, currentPos, sliceIndex));
-        mesh.addChain(*currentChain);
+        Chain currentChain(pkg_, currentPos, sliceIndex);
+        mesh.addChain(currentChain);
 
         // Get current derivatives
-        int32_t N = currentChain->size();
-        std::vector<double> currentDerivs(N);
+        int32_t N = currentChain.size();
+        std::vector<double> currentDerivs;
         currentDerivs.reserve(N);
+        std::cout << currentChain.positions() << std::endl;
         for (int32_t i = 0; i < N; ++i) {
-            currentDerivs[i] = fivePointStencil(i, currentChain->positions());
+            currentDerivs.push_back(fivePointStencil(i, currentChain.positions()));
         }
 
 		std::cout << "slice: " << sliceIndex << std::endl;
 
         if (showVisualization) {
-            currentChain->draw();
+            currentChain.draw();
             cv::waitKey(0);
         }
 
         std::vector<VoxelVec> choiceSpace =
-            currentChain->stepAll(stepNumLayers, keepNumMaxima);
+            currentChain.stepAll(stepNumLayers, keepNumMaxima);
 
         // Get the first set of positions (all the 'best' positions independent
         // of neighborhood constraints)
         VoxelVec bestPositions;
         bestPositions.reserve(N);
-        for (int32_t i = 0; i < N; ++i) {
-            bestPositions.push_back(choiceSpace[i][0]);
+        for (const auto& v : choiceSpace) {
+            bestPositions.push_back(v[0]);
         }
 
         // And evaluate it
-        std::vector<double> bestIndepPosDeriv;
-        bestIndepPosDeriv.reserve(N);
+        std::vector<double> combinationDerivs;
+        combinationDerivs.reserve(N);
         for (int32_t i = 0; i < N; ++i) {
-            bestIndepPosDeriv.push_back(fivePointStencil(i, bestPositions));
+            combinationDerivs.push_back(fivePointStencil(i, bestPositions));
         }
         double minDerivativeDiff =
-            l2_difference_norm(bestIndepPosDeriv, currentDerivs);
+            l2_difference_norm(combinationDerivs, currentDerivs);
 
         std::cout << "Trying " << numRandomTries << " positions" << std::endl;
         for (int32_t i = 0; i < numRandomTries; ++i) {
+            combinationDerivs.clear();
 
             // 1. Generate random positions vector from the indices in this
             VoxelVec positions;
@@ -98,8 +100,6 @@ LocalResliceSegmentation::segmentLayer(const bool showVisualization,
             }
 
             // 2. Generate derivatives from positions
-            std::vector<double> combinationDerivs;
-            combinationDerivs.reserve(N);
             for (int32_t i = 0; i < N; ++i) {
                 combinationDerivs.push_back(fivePointStencil(i, positions));
             }
@@ -109,7 +109,7 @@ LocalResliceSegmentation::segmentLayer(const bool showVisualization,
             double norm = l2_difference_norm(combinationDerivs, currentDerivs);
             if (norm - minDerivativeDiff < derivativeTolerance) {
                 minDerivativeDiff = norm;
-                bestPositions = std::move(positions);
+                bestPositions = positions;
             }
         }
         std::cout << "minDerivDiff: " << minDerivativeDiff << std::endl;
@@ -130,8 +130,9 @@ double LocalResliceSegmentation::fivePointStencil(
             [](double sum, cv::Vec3d p) { return sum + p(VC_INDEX_X); }) / ps.size();
 
     // Fit curve to new predicted ps.
-    FittedCurve<> f;
-    decltype(f)::PointVectorType newPoints(ps.size());
+    FittedCurve<double, 4> f;
+    FittedCurve<double, 4>::PointVector newPoints;
+    newPoints.reserve(ps.size());
     for (const auto& p : ps) {
         newPoints.emplace_back(p(VC_INDEX_X), p(VC_INDEX_Y));
     }
