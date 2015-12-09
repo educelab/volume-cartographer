ef5e74ef1cb12ea22ab1fbe0482243f80063f6de
Abigail Coleman <abbyc713@gmail.com>
2015-12-09 10:28:23 -0500

    (cloth)Remove current simulations to begin new Four Corners Method.

--- texturing/cloth.cpp
+++ texturing/cloth.cpp
@@ -15,15 +15,6 @@
 // bullet converter
 #include "itk2bullet.h"
 
-struct NodeTarget {
-    btVector3 t_pos;
-    btScalar  t_stepsize;
-};
-
-static void softBodyTickCallback(btDynamicsWorld *world, btScalar timeStep);
-std::vector<btSoftBody::Node*> pinnedPoints;
-std::vector<NodeTarget> targetPoints;
-
 int main(int argc, char* argv[]) {
     if ( argc < 5 ) {
         std::cout << "Usage: vc_texture2 volpkg seg-id iterations" << std::endl;
@@ -71,8 +62,7 @@ int main(int argc, char* argv[]) {
                                                                           collisionConfiguration,
                                                                           softBodySolver);
 
-    dynamicsWorld->setGravity(btVector3(0, -5, 0));
-    dynamicsWorld->setInternalTickCallback(softBodyTickCallback, dynamicsWorld, true);
+    dynamicsWorld->setGravity(btVector3(0, 0, 0));
 
     // convert itk mesh to bullet mesh (vertices and triangle arrays)
     std::cerr << "volcart::cloth::message: Converting mesh to softBody" << std::endl;
@@ -81,14 +71,6 @@ int main(int argc, char* argv[]) {
 
     psb->getWorldInfo()->m_gravity = dynamicsWorld->getGravity(); // Have to explicitly make softbody gravity match world gravity
     dynamicsWorld->addSoftBody(psb);
-
-    // Add a collision plane to drop the mesh onto
-    btCollisionShape* groundShape = new btStaticPlaneShape(btVector3(0, 1, 0), 1);
-
-    btDefaultMotionState* groundMotionState = new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, -1, 0)));
-    btRigidBody::btRigidBodyConstructionInfo groundRigidBodyCI(0, groundMotionState, groundShape, btVector3(0, 0, 0));
-    btRigidBody* plane = new btRigidBody(groundRigidBodyCI);
-    dynamicsWorld->addRigidBody(plane);
 
     // Constraints for the mesh as a soft body
     // These needed to be tested to find optimal values.
@@ -101,78 +83,6 @@ int main(int argc, char* argv[]) {
     // set the damping coefficient of the soft body [0,1]
     psb->m_cfg.kDP = 0.95;
 
-    // Set the top row of vertices such that they wont move/fall
-    // Currently assumes that the first point has the same z-value as the rest of the starting chain
-    int min_z = (int) std::floor(mesh->GetPoint(0)[2]);
-    int min_y = (int) std::floor(mesh->GetPoint(0)[1]);
-    int max_y = (int) std::ceil(mesh->GetPoint(0)[1]);
-    std::cerr << "volcart::cloth::message: Pinning points at Z: " << min_z << std::endl;
-    for(unsigned long i = 0; i < psb->m_nodes.size(); ++i) {
-        if( (int)psb->m_nodes[i].m_x.z() <= min_z) {
-            psb->setMass(i, 0);
-            btSoftBody::Node* node_ptr = &psb->m_nodes[i];
-            pinnedPoints.push_back(node_ptr);
-
-            if( (int)psb->m_nodes[i].m_x.y() > max_y )
-                max_y = (int)psb->m_nodes[i].m_x.y();
-            if ( (int)psb->m_nodes[i].m_x.y() < min_y )
-                min_y = (int)psb->m_nodes[i].m_x.y();
-        }
-    }
-
-    // rotate mesh 90 degrees around the y axis
-    std::cerr << "volcart::cloth::message: Rotating mesh" << std::endl;
-    psb->rotate(btQuaternion(0,SIMD_PI/2,0));
-
-    // Create target positions for our pinned points
-    for( auto p_id = pinnedPoints.begin(); p_id < pinnedPoints.end(); ++p_id ) {
-        NodeTarget n_target;
-        btVector3 target_pos;
-        btScalar distance;
-        btScalar t_x, t_y, t_z;
-
-        if ( p_id == pinnedPoints.begin() ) {
-            target_pos = (*p_id)->m_x;
-            target_pos.setZ((max_y + min_y) / 2);
-        }
-        else {
-            // Aligns the pinned points parallel with the x plane
-            distance = (*p_id)->m_x.distance((*std::prev(p_id))->m_x); //wtf
-            t_x = targetPoints.back().t_pos.getX() + distance;
-            t_y = (*p_id)->m_x.getY();
-            t_z = (max_y + min_y) / 2;
-            target_pos = btVector3(t_x, t_y, t_z);
-        }
-
-        n_target.t_pos = target_pos;
-        n_target.t_stepsize = (*p_id)->m_x.distance(target_pos) / 60; // Will take minimum 60 iterations to reach target
-        targetPoints.push_back(n_target);
-    }
-
-    // step simulation
-    std::cerr << "volcart::cloth::message: Beginning simulation" << std::endl;
-    for (int i = 0; i < NUM_OF_ITERATIONS; ++i) {
-        std::cerr << "volcart::cloth::message: Step " << i + 1 << "/" << NUM_OF_ITERATIONS << "\r" << std::flush;
-        dynamicsWorld->stepSimulation(1/60.f);
-        psb->solveConstraints();
-    }
-
-    std::cerr << std::endl;
-
-    // drop mesh onto collision plane
-    psb->setTotalMass(10, true );
-    for (int i = 0; i < NUM_OF_ITERATIONS; ++i) {
-        std::cerr << "volcart::cloth::message: Drop step " << i + 1 << "/" << NUM_OF_ITERATIONS << "\r" << std::flush;
-        dynamicsWorld->stepSimulation(1/60.f);
-        psb->solveConstraints();
-    }
-
-
-    std::cerr << std::endl;
-
-    // rotate mesh back to original orientation
-    psb->rotate(btQuaternion(0,-SIMD_PI/2,0));
-
     // Convert soft body to itk mesh
     volcart::meshing::bullet2itk::bullet2itk(mesh, psb);
 
@@ -181,10 +91,6 @@ int main(int argc, char* argv[]) {
     objwriter.write();
 
     // bullet clean up
-    dynamicsWorld->removeRigidBody(plane);
-    delete plane->getMotionState();
-    delete plane;
-    delete groundShape;
     dynamicsWorld->removeSoftBody(psb);
     delete psb;
     delete dynamicsWorld;
@@ -196,12 +102,3 @@ int main(int argc, char* argv[]) {
 
     return 0;
 } // end main
-
-void softBodyTickCallback(btDynamicsWorld *world, btScalar timeStep) {
-    for( size_t p_id = 0; p_id < pinnedPoints.size(); ++p_id ) {
-        if ( pinnedPoints[p_id]->m_x == targetPoints[p_id].t_pos ) continue;
-        btVector3 delta = (targetPoints[p_id].t_pos - pinnedPoints[p_id]->m_x).normalized() * targetPoints[p_id].t_stepsize;
-        pinnedPoints[p_id]->m_x += delta;
-        pinnedPoints[p_id]->m_v += delta/timeStep;
-    }
-};
