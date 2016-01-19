b8663a83fc4118ef9ae0db5d6859de2ea11e4b58
Sean Karlage <sean.karlage@uky.edu>
2016-01-19 18:08:36 -0500

    Finished code that will read STs from surface_normal files and calculate eigen{value,vector}s

--- examples/VerifyStructureTensor.cpp
+++ examples/VerifyStructureTensor.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include <tuple>
+#include <opencv2/opencv.hpp>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
 #include "volumepkg.h"
+
+using EigenValues = cv::Vec3d;
+using EigenVectors = cv::Matx33d;
+
+const StructureTensor zeroST = StructureTensor(0,0,0,0,0,0,0,0,0);
+
+std::pair<EigenValues, EigenVectors> getEigenValuesVectors(StructureTensor t);
+int32_t getMaxEigenValueIndex(const EigenValues val);
 
 int main(int argc, char** argv)
 {
@@ -16,6 +28,56 @@ int main(int argc, char** argv)
     int32_t z = std::stoi(std::string(argv[4]));
 
     VolumePkg v(path);
+    volcart::Volume vol = v.volume();
+    assert(z >= 2 && z < v.getNumberOfSlices() - 2 && "z index must not be first or last two points\n");
 
-    std::cout << v.volume().getStructureTensor(x, y, z) << "\n";
+    auto pathString = vol.getNormalPathAtIndex(z).string();
+    auto cloud = new pcl::PointCloud<pcl::PointXYZRGBNormal>();
+    auto ret = pcl::io::loadPCDFile(pathString, *cloud);
+    if (ret == -1) {
+        std::cerr << "couldn't load " << pathString << "\n";
+        std::exit(1);
+    }
+
+    // Old algo
+    // Deal with ZXY --> XYZ bullshit
+    int32_t cols = v.getSliceWidth();
+    auto pt = cloud->points[y * cols + x];
+    std::cout << "\nOld Algo\n";
+    std::cout << "estimated normal at index " << pt.y << ", " << pt.z << ", " << pt.x << "\n";
+    double correctedNormal[3] = { pt.normal[1], pt.normal[2], pt.normal[0] };
+    std::cout << "[" << correctedNormal[0] << ", " << correctedNormal[1] << "," << correctedNormal[2] << "]\n";
+
+    // New algo
+    // Print out eigenvector corresponding to max eigenvalue
+    std::cout << "\nNew Algo\n";
+    auto st = vol.getStructureTensor(x, y, z);
+    if (st == zeroST) {
+        std::cout << "Structure Tensor was zero\n";
+        std::exit(0);
+    }
+    std::cout << "structure tensor\n" << st << "\n";
+    auto eig = getEigenValuesVectors(st);
+    std::cout << "eigenvalues\n" << eig.first << "\n" << "eigenvectors\n" << eig.second << "\n";
+    int32_t maxIdx = getMaxEigenValueIndex(eig.first);
+    std::cout << "Max Eigenvalue index: " << maxIdx << "\n";
+    std::cout << "Corresponding eigenvector\n" << eig.second.row(maxIdx) << "\n";
+}
+
+std::pair<EigenValues, EigenVectors> getEigenValuesVectors(const StructureTensor t)
+{
+    EigenValues val;
+    EigenVectors vec;
+    cv::eigen(t, val, vec);
+    return std::make_pair(val, vec);
+}
+
+int32_t getMaxEigenValueIndex(const EigenValues val)
+{
+    std::vector<double> vals = { val(0), val(1), val(2) };
+    for (double& v : vals) {
+        v = std::fabs(v);
+    }
+    auto it = std::max_element(vals.begin(), vals.end());
+    return it - vals.begin();
 }
