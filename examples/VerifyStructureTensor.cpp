b16803d2c1c06df2c9536a80b57755d61fa27681
Sean Karlage <sean.karlage@uky.edu>
2016-01-25 15:49:29 -0500

    Changed verification code to take a slice as input and take some metrics on similarity of StructureTensors across whole slice

--- examples/VerifyStructureTensor.cpp
+++ examples/VerifyStructureTensor.cpp
@@ -1,79 +1,110 @@
 #include <iostream>
 #include <tuple>
+#include <array>
+#include <cstdio>
 #include <opencv2/opencv.hpp>
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
 #include "volumepkg.h"
 
-using EigenValues = cv::Vec3d;
-using EigenVectors = cv::Matx33d;
+using EigenVector = cv::Vec3d;
 
-const StructureTensor zeroST = StructureTensor(0, 0, 0, 0, 0, 0, 0, 0, 0);
-
-std::pair<EigenValues, EigenVectors> getEigenValuesVectors(StructureTensor t);
-int32_t getMaxEigenValueIndex(const EigenValues val);
+EigenVector oldAlgoNormalAtIndex(
+    const pcl::PointCloud<pcl::PointXYZRGBNormal>& cloud, const int32_t width,
+    const int32_t x, const int32_t y);
+pcl::PointCloud<pcl::PointXYZRGBNormal> cloudFromSlice(VolumePkg& v,
+                                                       const int32_t z);
 
 int main(int argc, char** argv)
 {
-    if (argc < 5) {
+    if (argc < 3) {
         std::cerr << "Usage:\n";
-        std::cerr << "    " << argv[0] << " [volpkg] [x] [y] [z]\n";
+        std::cerr << "    " << argv[0] << " volpkg z-idx [-verbose]\n";
         std::exit(1);
     }
 
     auto path = std::string(argv[1]);
-    std::cout << path << std::endl;
-    int32_t x = std::stoi(std::string(argv[2]));
-    int32_t y = std::stoi(std::string(argv[3]));
-    int32_t z = std::stoi(std::string(argv[4]));
+    int32_t arg_z = std::stoi(std::string(argv[2]));
+    bool verbose = false;
+    if (argc > 3) {
+        auto s = std::string(argv[3]);
+        if (s == "-verbose" || s == "-v") {
+            verbose = true;
+        }
+    }
 
     VolumePkg v(path);
-    volcart::Volume vol = v.volume();
-    assert(z >= 2 && z < v.getNumberOfSlices() - 2 &&
+    const volcart::Volume vol = v.volume();
+    assert(arg_z >= 2 && arg_z < v.getNumberOfSlices() - 2 &&
            "z index must not be first or last two points\n");
 
-    auto pathString = vol.getNormalPathAtIndex(z).string();
-    auto cloud = new pcl::PointCloud<pcl::PointXYZRGBNormal>();
-    auto ret = pcl::io::loadPCDFile(pathString, *cloud);
-    if (ret == -1) {
+    // Comparison of two eigenvectors for closeness
+    auto eigenvectorsAreClose = [](const EigenVector v1, const EigenVector v2,
+                                   const double eps) {
+        return cv::norm(v2 - v1) < eps;
+    };
+
+    // Epsilon for comparison
+    const double eps = 0.01;
+
+    // Comparison over whole slice
+    auto cloud = cloudFromSlice(v, arg_z);
+    int64_t totalCount = 0;
+    int64_t sameCount = 0;
+    int64_t zeroCount = 0;
+    for (int32_t y = 0; y < v.getSliceHeight(); ++y) {
+        for (int32_t x = 0; x < v.getSliceWidth(); ++x) {
+            // Get old eigenvalue
+            auto oldEigvec =
+                oldAlgoNormalAtIndex(cloud, v.getSliceWidth(), x, y);
+
+            // Get new eigenvalue
+            try {
+                auto pairs = vol.eigenPairsAtIndex(x, y, arg_z);
+                auto newEigvec = pairs[0].second;
+                if (eigenvectorsAreClose(oldEigvec, newEigvec, eps)) {
+                    sameCount++;
+                } else {
+                    if (verbose) {
+                        std::cout << "(" << x << ", " << y << ") " << oldEigvec
+                                  << ", " << newEigvec << "  "
+                                  << cv::norm(oldEigvec - newEigvec) << "\n";
+                    }
+                }
+            } catch (const volcart::ZeroStructureTensorException& ex) {
+                zeroCount++;
+            }
+            totalCount++;
+        }
+    }
+
+    // Report
+    int64_t validCount = totalCount - zeroCount;
+    std::printf("zeroCount: %ld (%f%%)\n", zeroCount,
+                double(zeroCount) / totalCount);
+    std::printf("sameCount: %ld (%f%%)\n", sameCount,
+                double(sameCount) / validCount);
+    std::printf("diffCount: %ld (%f%%)\n", validCount - sameCount,
+                double(validCount - sameCount) / validCount);
+}
+
+pcl::PointCloud<pcl::PointXYZRGBNormal> cloudFromSlice(VolumePkg& v,
+                                                       const int32_t z)
+{
+    auto pathString = v.volume().getNormalPathAtIndex(z).string();
+    auto cloud = pcl::PointCloud<pcl::PointXYZRGBNormal>();
+    if (pcl::io::loadPCDFile(pathString, cloud) == -1) {
         std::cerr << "couldn't load " << pathString << "\n";
         std::exit(1);
     }
-
-    // Old algo
-    // Deal with ZXY --> XYZ bullshit
-    int32_t cols = v.getSliceWidth();
-    auto pt = cloud->points[y * cols + x];
-    std::cout << "\nOld Algo\n";
-    std::cout << "estimated normal at index " << pt.y << ", " << pt.z << ", "
-              << pt.x << "\n";
-    double correctedNormal[3] = {pt.normal[1], pt.normal[2], pt.normal[0]};
-    std::cout << "[" << correctedNormal[0] << ", " << correctedNormal[1] << ","
-              << correctedNormal[2] << "]\n";
-
-    // New algo
-    // Print out eigenvector corresponding to max eigenvalue
-    std::cout << "\nNew Algo\n";
-    try {
-        auto pairs = vol.eigenPairsAtIndex(x, y, z);
-        std::cout << "eigenvalues\n" << pairs.first << "\n"
-                  << "eigenvectors\n" << pairs.second << "\n";
-        int32_t maxIdx = getMaxEigenValueIndex(pairs.first);
-        std::cout << "Max Eigenvalue index: " << maxIdx << "\n";
-        std::cout << "Corresponding eigenvector\n" << pairs.second.row(maxIdx)
-                  << "\n";
-    } catch (volcart::ZeroStructureTensorException& ex) {
-        std::cerr << ex.what() << std::endl;
-        std::exit(1);
-    }
+    return cloud;
 }
 
-int32_t getMaxEigenValueIndex(const EigenValues val)
+EigenVector oldAlgoNormalAtIndex(
+    const pcl::PointCloud<pcl::PointXYZRGBNormal>& cloud, const int32_t width,
+    const int32_t x, const int32_t y)
 {
-    std::vector<double> vals = {val(0), val(1), val(2)};
-    for (double& v : vals) {
-        v = std::fabs(v);
-    }
-    return std::distance(vals.begin(),
-                         std::max_element(vals.begin(), vals.end()));
+    // Deal with ZXY --> XYZ bullshit
+    auto pt = cloud.points[y * width + x];
+    return {pt.normal_y, pt.normal_z, pt.normal_x};
 }
