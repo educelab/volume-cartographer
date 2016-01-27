b3183eeef3c2b3c7970de9281b6a478c164245ae
Sean Karlage <sean.karlage@uky.edu>
2016-01-27 17:32:57 -0500

    Implemented HDF5 file I/O for raw data

--- examples/MakeNeuralNetTrainingData.cpp
+++ examples/MakeNeuralNetTrainingData.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 #include <random>
+#include <sstream>
 #include <tuple>
 #include <cmath>
 #include <boost/filesystem.hpp>
 #include <opencv2/opencv.hpp>
+#include <H5Cpp.h>
 #include "volumepkg.h"
 
 namespace fs = boost::filesystem;
 
 struct CartesianCoord {
     int32_t x, y, z;
+    CartesianCoord() = default;
     CartesianCoord(int32_t x, int32_t y, int32_t z) : x(x), y(y), z(z) {}
+    operator cv::Point3i() const { return {x, y, z}; }
 };
 std::ostream& operator<<(std::ostream& s, const CartesianCoord& c)
 {
@@ -19,6 +23,7 @@ std::ostream& operator<<(std::ostream& s, const CartesianCoord& c)
 
 struct PolarCoord {
     double r, theta;
+    PolarCoord() = default;
     PolarCoord(double r, double theta) : r(r), theta(theta) {}
 };
 std::ostream& operator<<(std::ostream& s, const PolarCoord& p)
@@ -28,6 +33,12 @@ std::ostream& operator<<(std::ostream& s, const PolarCoord& p)
 
 CartesianCoord polarToCartesian(const PolarCoord coord, const int32_t z);
 void draw(const volcart::Volume& vol, const CartesianCoord c);
+template <typename DType>
+void writeVolumeToH5File(const std::string& filename,
+                         const volcart::Tensor3D<DType>& volume);
+void writeStructureTensorEigToH5File(const std::string& filename,
+                                     const StructureTensor& st,
+                                     const EigenPairs& pairs);
 
 // Center of the dataset on XY-plane
 int32_t g_centerX;
@@ -62,24 +73,37 @@ int main(int argc, char** argv)
     std::uniform_int_distribution<int32_t> zDist(zmin, zmax + 1);
 
     // Start generating
+    CartesianCoord c;
     while (true) {
         double theta = thetaDist(e);
         double r = rDist(e);
         int32_t z = zDist(e);
         std::cout << "r: " << r << ", theta: " << theta << ", z: " << z
                   << std::endl;
-        CartesianCoord c = polarToCartesian({r, theta}, z);
+        c = polarToCartesian({r, theta}, z);
 
         // Volume bounds checking
         if (c.x < 0 || c.x > volpkg.getSliceWidth() || c.y < 0 ||
             c.y > volpkg.getSliceHeight()) {
             continue;
+        } else {
+            break;
         }
-
-        std::cout << c << std::endl;
-
-        draw(vol, c);
     }
+
+    // Get volume, write to hdf5 file
+    int32_t radius = 3;
+    auto volume = vol.getVoxelNeighborsCubic<uint16_t>({c.x, c.y, c.z}, radius);
+    std::stringstream ss;
+    ss << c.x << "_" << c.y << "_" << c.z << "_size" << 2 * radius + 1;
+    const auto volumeFileName = ss.str() + "_volume.h5";
+    const auto stFileName = ss.str() + "_st_eig.h5";
+    writeVolumeToH5File(volumeFileName, volume);
+
+    // Get ST & eigenvalues/vectors
+    auto st = vol.structureTensorAtIndex(c.x, c.y, c.z, radius);
+    auto pairs = vol.eigenPairsAtIndex(c.x, c.y, c.z, radius);
+    writeStructureTensorEigToH5File(stFileName, st, pairs);
 }
 
 // Change polar coordinate to cartesian, taking into account offset center value
@@ -87,6 +111,98 @@ CartesianCoord polarToCartesian(const PolarCoord p, const int32_t z)
 {
     return CartesianCoord(g_centerX + std::round(p.r * std::cos(p.theta)),
                           g_centerY + std::round(p.r * std::sin(p.theta)), z);
+}
+
+template <typename DType>
+void writeVolumeToH5File(const std::string& filename,
+                         const volcart::Tensor3D<DType>& volume)
+{
+    constexpr int32_t rank = 4;
+    hsize_t dimsf[] = {1, static_cast<hsize_t>(volume.dz()),
+                       static_cast<hsize_t>(volume.dy()),
+                       static_cast<hsize_t>(volume.dx())};
+    constexpr int32_t radius = 3;
+    constexpr int32_t side = radius * 2 + 1;
+    DType v[1][side][side][side];
+    std::cout << "dx: " << volume.dx() << "\ndy: " << volume.dy()
+              << "\ndz: " << volume.dz() << "\n";
+    for (int32_t z = 0; z < volume.dz(); ++z) {
+        for (int32_t y = 0; y < volume.dy(); ++y) {
+            for (int32_t x = 0; x < volume.dx(); ++x) {
+                v[0][z][y][x] = volume(x, y, z);
+            }
+        }
+    }
+    /*
+    DType v[1][7][6][5];
+    std::cout << "dx: " << volume.dx() << "\ndy: " << volume.dy()
+              << "\ndz: " << volume.dz() << "\n";
+    for (int32_t z = 0; z < 7; ++z) {
+        for (int32_t y = 0; y < 6; ++y) {
+            for (int32_t x = 0; x < 5; ++x) {
+                v[0][z][y][x] = 1;
+            }
+        }
+    }
+    */
+
+    try {
+        H5::H5File f(filename, H5F_ACC_TRUNC);
+        H5::DataSpace dspace(rank, dimsf);
+        auto dset = f.createDataSet("data", H5::PredType::STD_U16LE, dspace);
+        dset.write(v, H5::PredType::STD_U16LE);
+    } catch (const H5::Exception& ex) {
+        std::cerr << ex.getDetailMsg() << std::endl;
+    }
+}
+
+void writeStructureTensorEigToH5File(const std::string& filename,
+                                     const StructureTensor& st,
+                                     const EigenPairs& pairs)
+{
+    // Dimensions for H5 data
+    hsize_t stDims[] = {3, 3};
+    int32_t stRank = 2;
+    hsize_t eigvecDims[] = {3, 3};
+    int32_t eigvecRank = 2;
+    hsize_t eigvalDims[] = {3};
+    int32_t eigvalRank = 1;
+
+    // Raw buffers for data
+    double stBuf[3][3] = {{st(0, 0), st(0, 1), st(0, 2)},
+                          {st(1, 0), st(1, 1), st(1, 2)},
+                          {st(2, 0), st(2, 1), st(2, 2)}};
+    cv::Vec3d ev0 = pairs[0].second;
+    cv::Vec3d ev1 = pairs[1].second;
+    cv::Vec3d ev2 = pairs[2].second;
+    double eigvecBuf[3][3] = {{ev0(0), ev0(1), ev0(2)},
+                              {ev1(0), ev1(1), ev1(2)},
+                              {ev2(0), ev2(1), ev2(2)}};
+    double eigvalBuf[3] = {pairs[0].first, pairs[1].first, pairs[2].first};
+
+    try {
+        H5::H5File f(filename, H5F_ACC_TRUNC);
+
+        // Write structure tensor
+        H5::DataSpace stSpace(stRank, stDims);
+        auto stDSet = f.createDataSet("StructureTensor",
+                                      H5::PredType::NATIVE_FLOAT, stSpace);
+        stDSet.write(stBuf, H5::PredType::NATIVE_FLOAT);
+
+        // Write eigenvectors
+        H5::DataSpace eigvecSpace(eigvecRank, eigvecDims);
+        auto eigvecDSet = f.createDataSet(
+            "Eigenvectors", H5::PredType::NATIVE_FLOAT, eigvecSpace);
+        eigvecDSet.write(eigvecBuf, H5::PredType::NATIVE_FLOAT);
+
+        // Write eigenvalues
+        H5::DataSpace eigvalSpace(eigvalRank, eigvalDims);
+        auto eigvalDSet = f.createDataSet(
+            "Eigenvalues", H5::PredType::NATIVE_FLOAT, eigvalSpace);
+        eigvalDSet.write(eigvalBuf, H5::PredType::NATIVE_FLOAT);
+    } catch (const H5::Exception& ex) {
+        std::cerr << ex.getDetailMsg() << std::endl;
+    }
 }
 
 // Draw point on slice
