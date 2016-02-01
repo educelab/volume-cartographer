9d2019c2d3f29bac2bbf606fc912a2cfc9823ac2
Sean Karlage <skarlage@get9.io>
2016-02-01 13:29:23 -0500

    [WIP] Cleaned up algorithm a little bit; no longer monte carlo

--- segmentation/localResliceParticleSim/localResliceParticleSim.cpp
+++ segmentation/localResliceParticleSim/localResliceParticleSim.cpp
@@ -7,105 +7,137 @@
 #include "localResliceParticleSim.h"
 #include "common.h"
 
-
 using namespace volcart::segmentation;
 
-LocalResliceSegmentation::LocalResliceSegmentation(VolumePkg& pkg) :
-        pkg_(pkg), startIndex_(0), endIndex_(0) { }
+using ScalarVector = std::vector<double>;
+std::vector<double> deriv(const VoxelVec& vec);
 
-pcl::PointCloud<pcl::PointXYZRGB>
-LocalResliceSegmentation::segmentLayer(const bool showVisualization,
-                                       const int32_t startIndex,
-                                       const int32_t endIndex,
-                                       const int32_t stepNumLayers,
-                                       const double  derivativeTolerance,
-                                       const int32_t keepNumMaxima,
-                                       const int32_t numRandomTries)
+ScalarVector subtractAbs(const ScalarVector& lhs, const ScalarVector& rhs)
 {
-    // Account for zero-indexing and slices lost in calculating normal vector
-    startIndex_ = startIndex;
-    endIndex_ = pkg_.getNumberOfSlices() - 3;
-    if (endIndex != kDefaultEndIndex) {
-        endIndex_ = endIndex;
+    assert(lhs.size() == rhs.size() && "lhs must be the same size as rhs");
+    ScalarVector res(lhs.size(), 0.0);
+    for (uint32_t i = 0; i < lhs.size(); ++i) {
+        res[i] = std::fabs(lhs[i] - rhs[i]);
     }
+    return res;
+}
 
-    // Seed random number generator
-    std::random_device rd;
-    std::default_random_engine mt(rd());
+double normL2(const ScalarVector& lhs, const ScalarVector& rhs)
+{
+    assert(lhs.size() == rhs.size() && "lhs must be the same size as rhs");
+    double res = 0.0;
+    for (uint32_t i = 0; i < lhs.size(); ++i) {
+        res += (lhs[i] - rhs[i]) * (lhs[i] - rhs[i]);
+    }
+    return std::sqrt(res);
+}
 
-    // ChainMesh that holds the segment
+VoxelVec downsample(const VoxelVec& voxels, int32_t zIndex,
+                    double keepPerc = 0.40)
+{
+    FittedCurve<double> curve(voxels);
+    int32_t newSize = std::round(voxels.size() * keepPerc);
+    curve.resample(newSize);
+    auto resampled = curve.resampledPoints();
+    VoxelVec vResampled;
+    vResampled.reserve(newSize);
+    for (auto v : resampled) {
+        vResampled.emplace_back(v(0), v(1), zIndex);
+    }
+    return vResampled;
+}
+
+LocalResliceSegmentation::LocalResliceSegmentation(VolumePkg& pkg) : pkg_(pkg)
+{
+}
+
+pcl::PointCloud<pcl::PointXYZRGB> LocalResliceSegmentation::segmentLayer(
+    const bool showVisualization, const int32_t startIndex,
+    const int32_t endIndex, const int32_t stepNumLayers,
+    const double derivativeTolerance, const int32_t keepNumMaxima,
+    const int32_t numRandomTries)
+{
+    // Starting positions
     VoxelVec currentPos;
-    auto path = pkg_.openCloud(); 
+    auto path = pkg_.openCloud();
     for (const auto& p : *path) {
         currentPos.emplace_back(p.x, p.y, p.z);
     }
-    int32_t N = currentPos.size();
-    ChainMesh mesh(N, endIndex_ - startIndex_);
-    mesh.addPositions(currentPos);
+    currentPos = downsample(currentPos, startIndex, 0.40);
+    std::cout << "currentPos size: " << currentPos.size() << std::endl;
 
-    for (int32_t sliceIndex = startIndex_; sliceIndex <= endIndex_;
-         sliceIndex += stepNumLayers) {
+    // ChainMesh that will hold all positions
+    ChainMesh mesh(currentPos.size(), endIndex - startIndex);
 
+    for (int32_t zIndex = startIndex; zIndex <= endIndex;
+         zIndex += stepNumLayers) {
         // First chain object
-        Chain chain(pkg_, currentPos, sliceIndex);
+        Chain chain(pkg_, currentPos, zIndex);
 
-        // Get current derivatives
-        int32_t N = chain.size();
-        std::vector<double> initDerivs = chain.curve().deriv();
-
-		std::cout << "slice: " << sliceIndex << std::endl;
-
+        std::cout << "slice: " << zIndex << std::endl;
         if (showVisualization) {
             chain.draw();
             cv::waitKey(0);
         }
 
-        std::vector<VoxelVec> choiceSpace =
-            chain.stepAll(stepNumLayers, keepNumMaxima);
+        auto choiceSpace = chain.stepAll(stepNumLayers, keepNumMaxima);
 
         // Get the first set of positions (all the 'best' positions independent
         // of neighborhood constraints)
-        VoxelVec bestPositions;
-        bestPositions.reserve(N);
-        for (const auto& v : choiceSpace) {
-            bestPositions.push_back(v[0]);
+        VoxelVec bestPos;
+        bestPos.reserve(currentPos.size());
+        for (auto& v : choiceSpace) {
+            bestPos.push_back(v.front());
+            v.pop_front();
         }
 
-        // And evaluate it
-        std::vector<double> combinationDerivs = chain.curve().deriv();
-        double minDerivativeDiff =
-            l2_difference_norm(combinationDerivs, initDerivs);
+        ScalarVector initDerivs = chain.curve().deriv();
+        ScalarVector bestPosDerivs = deriv(bestPos);
+        double minDerivativeDiff = normL2(bestPosDerivs, initDerivs);
 
+        // Greedy algorithm to iteratively re-step the particle that introduces
+        // the largest difference in the derivative
         for (int32_t i = 0; i < numRandomTries; ++i) {
-            combinationDerivs.clear();
+            std::cout << "i: " << i << std::endl;
+            std::cout << "mindiff: " << minDerivativeDiff << "\n";
+            // 1. Calculate the index of the maximum derivative difference
+            ScalarVector absDiff = subtractAbs(bestPosDerivs, initDerivs);
+            int32_t maxDiffIdx =
+                std::distance(absDiff.begin(),
+                              std::max_element(absDiff.begin(), absDiff.end()));
 
-            // 1. Generate random positions vector from the indices in this
-            VoxelVec positions;
-            positions.reserve(N);
-            for (const auto& v : choiceSpace) {
-                std::uniform_int_distribution<int32_t> dist(0, v.size() - 1);
-                positions.push_back(v[dist(mt)]);
+            // 2. Get the next candidate voxel position for that index
+            std::cout << "maxDiffIdx: " << maxDiffIdx << std::endl;
+            VoxelVec combinationPos(bestPos.begin(), bestPos.end());
+            if (choiceSpace[maxDiffIdx].size() > 0) {
+                combinationPos[maxDiffIdx] = choiceSpace[maxDiffIdx].front();
+                choiceSpace[maxDiffIdx].pop_front();
+            } else {
+                std::cerr << "ran out of candidate positions\n";
+                break;
             }
-            //std::cout << "positions: " << positions << std::endl;
 
-            // 2. Generate derivatives from positions
-			combinationDerivs = chain.curve().deriv();
-            //std::cout << "combination derivs: " << combinationDerivs << std::endl;
-    
-            // 2. Calculate l2 norm of this and previous positions and set
-            //    min accordingly
-            double norm = l2_difference_norm(combinationDerivs, initDerivs);
-            //std::cout << "norm: " << norm << std::endl;
-            if (norm - minDerivativeDiff < derivativeTolerance) {
-                minDerivativeDiff = norm;
-                bestPositions = positions;
+            // 3. Re-evaluate derivatives of best positions and find absdiff
+            ScalarVector combinationPosDerivs = deriv(combinationPos);
+            std::cout << "combination: "
+                      << normL2(combinationPosDerivs, initDerivs) << std::endl;
+            if (normL2(combinationPosDerivs, initDerivs) < minDerivativeDiff) {
+                bestPos = combinationPos;
+                bestPosDerivs = combinationPosDerivs;
+                minDerivativeDiff = normL2(combinationPosDerivs, initDerivs);
             }
-            //std::cout << std::endl;
         }
-        std::cout << "minDerivDiff: " << minDerivativeDiff << std::endl;
-        mesh.addPositions(bestPositions);
-        currentPos = bestPositions;
+
+        // At the end, bestPos contains the best voxel positions
+        mesh.addPositions(currentPos);
+        currentPos = bestPos;
     }
 
     return mesh.exportAsPointCloud();
+}
+
+std::vector<double> deriv(const VoxelVec& vec)
+{
+    FittedCurve<double> curve(vec);
+    return curve.deriv();
 }
--- segmentation/localResliceParticleSim/localResliceParticleSim.h
+++ segmentation/localResliceParticleSim/localResliceParticleSim.h
@@ -15,57 +15,34 @@
 #include "chainmesh.h"
 #include "common.h"
 
-namespace volcart {
-
-namespace segmentation {
-
-class LocalResliceSegmentation {
+namespace volcart
+{
+namespace segmentation
+{
+class LocalResliceSegmentation
+{
 public:
     LocalResliceSegmentation(VolumePkg& pkg);
 
+    void segmentLayer(const bool visualize, const VoxelVec& initPath,
+                      const int32_t startIndex, const int32_t endIndex,
+                      const int32_t stepNumLayers);
+
     pcl::PointCloud<pcl::PointXYZRGB> segmentLayer(
-            const bool    showVisualization=kDefaultShowVisualization,
-            const int32_t startIndex=kDefaultStartIndex,
-            const int32_t endIndex=kDefaultEndIndex,
-            const int32_t stepNumLayers=kDefaultStepNumLayers,
-            const double  derivativeTolerance=kDefaultDerivativeTolerance,
-            const int32_t keepNumMaxima=kDefaultKeepNumMaxima,
-            const int32_t numRandomTries=kDefaultNumRandomTries);
+        const bool showVisualization, const int32_t startIndex,
+        const int32_t endIndex, const int32_t stepNumLayers,
+        const double derivativeTolerance = kDefaultDerivativeTolerance,
+        const int32_t keepNumMaxima = kDefaultKeepNumMaxima,
+        const int32_t numRandomTries = kDefaultNumRandomTries);
 
 private:
     VolumePkg& pkg_;
-    int32_t startIndex_;
-    int32_t endIndex_;
 
-    double fivePointStencil(const uint32_t center, const VoxelVec& ps) const;
-
-    constexpr static bool    kDefaultShowVisualization   = false;
-    constexpr static int32_t kDefaultStartIndex          = 0;
-    constexpr static int32_t kDefaultEndIndex            = -1;
-    constexpr static int32_t kDefaultStepNumLayers       = 2;
-    constexpr static double  kDefaultDerivativeTolerance = 1e-5;
-    constexpr static int32_t kDefaultKeepNumMaxima       = 5;
-    constexpr static int32_t kDefaultNumRandomTries      = 100;
-
+    constexpr static double kDefaultDerivativeTolerance = 1e-2;
+    constexpr static int32_t kDefaultKeepNumMaxima = 5;
+    constexpr static int32_t kDefaultNumRandomTries = 10;
 };
-
-// Some utility functions used in the algorithm that don't belong in common.h
-// Returns the L2 norm of lhs - rhs
-template <typename Scalar>
-Scalar l2_difference_norm(const std::vector<Scalar>& lhs,
-                          const std::vector<Scalar>& rhs)
-{
-    assert(lhs.size() == rhs.size() && "lhs and rhs must be the same size");
-    Scalar out = 0;
-    size_t N = lhs.size();
-    for (size_t i = 0; i < N; ++i) {
-        out += (lhs[i] - rhs[i]) * (lhs[i] - rhs[i]);
-    }
-    return std::sqrt(out);
 }
-
-}
-
 }
 
 #endif
