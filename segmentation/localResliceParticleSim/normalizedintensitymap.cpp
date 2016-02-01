c55a0efa76aca99b3e1d71aa0f74b77b24789fde
Sean Karlage <skarlage@get9.io>
2016-02-01 13:28:57 -0500

    Ran clang-format, changed window to be drawn with opengl

--- segmentation/localResliceParticleSim/normalizedintensitymap.cpp
+++ segmentation/localResliceParticleSim/normalizedintensitymap.cpp
@@ -1,7 +1,6 @@
 #include "normalizedintensitymap.h"
 #include "common.h"
 
-
 using namespace volcart::segmentation;
 
 NormalizedIntensityMap::NormalizedIntensityMap(cv::Mat r)
@@ -17,10 +16,14 @@ void NormalizedIntensityMap::draw(const int32_t displayWidth,
     // Build intensity map
     cv::Mat mapImage(displayWidth, displayHeight, CV_8UC3, cv::Scalar(0, 0, 0));
     for (int32_t i = 1; i < _intensities.cols; ++i) {
-        auto p1 = cv::Point(binWidth * (i-1),
-				displayHeight - cvRound(displayHeight * _intensities.at<double>(i-1)));
-        auto p2 = cv::Point(binWidth * (i),
-				displayHeight - cvRound(displayHeight * _intensities.at<double>(i)));
+        auto p1 = cv::Point(
+            binWidth * (i - 1),
+            displayHeight -
+                cvRound(displayHeight * _intensities.at<double>(i - 1)));
+        auto p2 =
+            cv::Point(binWidth * (i),
+                      displayHeight -
+                          cvRound(displayHeight * _intensities.at<double>(i)));
         cv::line(mapImage, p1, p2, cv::Scalar(0, 255, 0));
     }
 
@@ -37,13 +40,14 @@ void NormalizedIntensityMap::draw(const int32_t displayWidth,
         }
     }
     cv::line(mapImage, cv::Point(binWidth * maxima[minIdx].first, 0),
-		cv::Point(binWidth * maxima[minIdx].first, mapImage.rows), BGR_BLUE);
+             cv::Point(binWidth * maxima[minIdx].first, mapImage.rows),
+             BGR_BLUE);
 
     // Vertical line at particle's current x position
     cv::line(mapImage, cv::Point(binWidth * centerX, 0),
-		cv::Point(binWidth * centerX, mapImage.rows), BGR_YELLOW);
+             cv::Point(binWidth * centerX, mapImage.rows), BGR_YELLOW);
 
-    cv::namedWindow("Intensity Map", cv::WINDOW_NORMAL);
+    cv::namedWindow("Intensity Map", cv::WINDOW_OPENGL);
     cv::imshow("Intensity Map", mapImage);
 }
 
@@ -54,15 +58,19 @@ IndexDistPairVec NormalizedIntensityMap::findMaxima() const
     cv::Mat sobelDerivatives;
     cv::Sobel(_intensities, sobelDerivatives, CV_64FC1, 1, 0);
 
-    // Get indices of positive -> negative transitions, store in pairs (index, intensity)
+    // Get indices of positive -> negative transitions, store in pairs (index,
+    // intensity)
     auto crossings = IndexDistPairVec();
     for (auto i = 0; i < sobelDerivatives.cols - 1; ++i) {
-        if (sobelDerivatives.at<double>(i) * sobelDerivatives.at<double>(i+1) <= 0 &&
-            sobelDerivatives.at<double>(i) > sobelDerivatives.at<double>(i+1)) {
-            if (_intensities.at<double>(i) > _intensities.at<double>(i+1)) {
+        if (sobelDerivatives.at<double>(i) *
+                    sobelDerivatives.at<double>(i + 1) <=
+                0 &&
+            sobelDerivatives.at<double>(i) >
+                sobelDerivatives.at<double>(i + 1)) {
+            if (_intensities.at<double>(i) > _intensities.at<double>(i + 1)) {
                 crossings.emplace_back(i, _intensities.at<double>(i));
             } else {
-                crossings.emplace_back(i+1, _intensities.at<double>(i+1));
+                crossings.emplace_back(i + 1, _intensities.at<double>(i + 1));
             }
         }
     }
