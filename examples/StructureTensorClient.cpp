f256be1e7635428a7085f8b2bea3e5f74d61f56a
Sean Karlage <skarlage@get9.io>
2016-02-06 18:04:07 -0500

    Remove zero ST exception handling

--- examples/StructureTensorClient.cpp
+++ examples/StructureTensorClient.cpp
@@ -9,8 +9,8 @@ void draw(const volcart::Volume& v, const int32_t zSlice, const EigenVector vec,
 int main(int argc, char** argv)
 {
     if (argc < 5) {
-        std::cerr << "Usage:\n";
-        std::cerr << "    " << argv[0] << " volpkg x y z [radius]\n";
+        std::cerr << "Usage:" << std::endl;
+        std::cerr << "    " << argv[0] << " volpkg x y z [radius]" << std::endl;
         std::exit(1);
     }
 
@@ -24,32 +24,29 @@ int main(int argc, char** argv)
     const int32_t x = std::stoi(argv[2]);
     const int32_t y = std::stoi(argv[3]);
     const int32_t z = std::stoi(argv[4]);
-    std::cout << "{" << x << ", " << y << ", " << z << "} @ " << radius << "\n";
+    std::cout << "{" << x << ", " << y << ", " << z << "} @ " << radius
+              << std::endl;
 
-    std::cout << "structure tensor:\n"
-              << v.structureTensorAtIndex(x, y, z, radius) << "\n";
-    try {
-        auto pairs = v.eigenPairsAtIndex(x, y, z, radius);
-        std::cout << "eigenvalues/eigenvectors\n";
-        std::for_each(pairs.begin(), pairs.end(),
-                      [](const std::pair<EigenValue, EigenVector>& p) {
-                          std::cout << p.first << ":  " << p.second << "\n";
-                      });
-        draw(v, z, pairs[0].second, {x, y});
-    } catch (const volcart::ZeroStructureTensorException& ex) {
-        std::cout << ex.what() << std::endl;
-    }
+    StructureTensor st = v.structureTensorAtIndex(x, y, z, radius);
+    std::cout << "structure tensor:" << std::endl << st << std::endl;
+    auto pairs = v.eigenPairsAtIndex(x, y, z, radius);
+    std::cout << "eigenvalues/eigenvectors" << std::endl;
+    std::for_each(pairs.begin(), pairs.end(),
+                  [](const std::pair<EigenValue, EigenVector>& p) {
+                      std::cout << p.first << ": " << p.second << std::endl;
+                  });
+    draw(v, z, pairs[0].second, {x, y});
 }
 
 void draw(const volcart::Volume& v, const int32_t zSlice, const EigenVector vec,
           const cv::Point p1)
 {
-    auto slice = v.getSliceData(zSlice);
+    auto slice = v.getSliceDataCopy(zSlice);
     slice /= 255.0;
     slice.convertTo(slice, CV_8UC3);
     cv::cvtColor(slice, slice, CV_GRAY2BGR);
 
-    constexpr int32_t scale = 20;
+    constexpr int32_t scale = 10;
     const cv::Point p2{cvRound(p1.x + vec(0) * scale),
                        cvRound(p1.y + vec(1) * scale)};
     cv::line(slice, p1, p2, cv::Scalar(0, 0, 0xFF), 2);
