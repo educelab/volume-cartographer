943117117d1c38298533e5a9e52a4d70e921c69e
Seth Parker <csparker247@gmail.com>
2016-02-09 12:59:35 -0500

    Fix stupid case-senesitive filename weirdness

--- segmentation/structureTensorParticleSim/structureTensorParticleSim.cpp
+++ segmentation/structureTensorParticleSim/structureTensorParticleSim.cpp
@@ -18,19 +18,14 @@
 namespace volcart {
     namespace segmentation {
         pcl::PointCloud <pcl::PointXYZRGB> structureTensorParticleSim(pcl::PointCloud<pcl::PointXYZRGB>::Ptr segPath, VolumePkg volpkg, double gravity_scale, int threshold, int endOffset) {
-            // Load the surface normal field from volpkg
-            // Field object handles which files will be loaded and when
-            Field normal_field(&volpkg);
             // Organize the points into a connected chain
-            Chain particle_chain(segPath, &volpkg, gravity_scale, threshold, endOffset);
+            Chain particle_chain(segPath, volpkg, gravity_scale, threshold, endOffset);
 
             // The only stop condition is that each particle has reached the target index.
             // Otherwise the loop will stop after an arbitrary number of iterations.
             for (int iteration = 0; particle_chain.isMoving() && iteration < 25000; ++iteration) {
                 // Make the chain take one iteration forward
-                particle_chain.step(normal_field);
-                // Unload unused normal vectors to free memory
-                normal_field.clean();
+                particle_chain.step();
             }
 
             return particle_chain.orderedPCD();
