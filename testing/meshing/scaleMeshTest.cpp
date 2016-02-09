1bef892d961fe0a8c9a39c52e47a53b56d0b3ee8
Ryan Taber <ryan@Viss-iMac-2.local>
2016-02-09 14:53:02 -0500

    updated scaleMeshTest nomenclature

--- testing/meshing/scaleMeshTest.cpp
+++ testing/meshing/scaleMeshTest.cpp
@@ -24,16 +24,16 @@
  *  (plane, cube, cone, sphere, arch) which initialize objects for the following    *
  *  test cases:                                                                     *
  *                                                                                  *
- *  1. planeTest (planeFix)                                                         *
- *  2. cubeTest (cubeFix)                                                           *
- *  3. archTest (archFix)                                                           *
- *  4. sphereTest (sphereFix)                                                       *
- *  5. coneTest (coneFix)                                                           *
- *  6. inputUnchangedTest (planeFix)                                                *   
+ *  1. ScaledPlaneTest (PlaneFixture)                                               *
+ *  2. ScaledCubeTest (CubeFixture)                                                 *
+ *  3. ScaledArchTest (ArchFixture)                                                 *
+ *  4. ScaledSphereTest (SphereFixture)                                             *
+ *  5. ScaledConeTest (ConeFixture)                                                 *
+ *  6. ConfirmInputPlaneMeshIsUnchangedAfterScalingTest (PlaneFixture)              *
  *                                                                                  *
  * Input:                                                                           *
- *     No required inputs for this sample test. All test objects are created        *
- *     internally.                                                                  *
+ *     No required external inputs for this sample test. All test objects are       *
+ *     created internally by the various test fixtures.                             *
  *                                                                                  *
  * Test-Specific Output:                                                            *
  *     Specific test output only given on failure of any tests. Otherwise, general  *
@@ -43,264 +43,265 @@
  *     See the /testing/meshing wiki for more information on this test              *
  * **********************************************************************************/
 
+
 /*
  * Individual test fixtures for each of the common shapes
  * Test cases call appropriate fixture
  */
 
-struct planeFix {
+struct PlaneFixture {
 
-    planeFix() {
+    PlaneFixture() {
 
-        inputMesh = plane.itkMesh();
+        _in_PlaneMesh = _Plane.itkMesh();
+
+        _ScaleFactor = 3;
 
         std::cerr << "setting up planar mesh for scaling" << std::endl;
     }
 
-    ~planeFix(){ std::cerr << "cleaning up plane objects" << std::endl; }
+    ~PlaneFixture(){ std::cerr << "cleaning up plane objects" << std::endl; }
 
-    VC_MeshType::Pointer inputMesh;
-    VC_MeshType::Pointer outputMesh = VC_MeshType::New();
-    volcart::shapes::Plane plane;
+    VC_MeshType::Pointer _in_PlaneMesh;
+    VC_MeshType::Pointer _out_PlaneMesh = VC_MeshType::New();
+    volcart::shapes::Plane _Plane;
+    double _ScaleFactor;
 };
 
-struct cubeFix {
+struct CubeFixture {
 
-    cubeFix() {
+    CubeFixture() {
 
-        inputMesh = cube.itkMesh();
+        _in_CubeMesh = _Cube.itkMesh();
+
+        _ScaleFactor = 3;
 
         std::cerr << "setting up cube mesh for scaling" << std::endl;
     }
 
-    ~cubeFix(){ std::cerr << "cleaning up cube objects" << std::endl; }
+    ~CubeFixture(){ std::cerr << "cleaning up cube objects" << std::endl; }
 
-    VC_MeshType::Pointer inputMesh;
-    VC_MeshType::Pointer outputMesh = VC_MeshType::New();
-    volcart::shapes::Cube cube;
+    VC_MeshType::Pointer _in_CubeMesh;
+    VC_MeshType::Pointer _out_CubeMesh = VC_MeshType::New();
+    volcart::shapes::Cube _Cube;
+    double _ScaleFactor;
 };
 
-struct archFix {
+struct ArchFixture {
 
-    archFix() {
+    ArchFixture() {
 
-        inputMesh = arch.itkMesh();
+        _in_ArchMesh = _Arch.itkMesh();
+
+        _ScaleFactor = 3;
 
         std::cerr << "setting up arch mesh for scaling" << std::endl;
     }
 
-    ~archFix(){ std::cerr << "cleaning up arch mesh objects" << std::endl; }
+    ~ArchFixture(){ std::cerr << "cleaning up arch mesh objects" << std::endl; }
 
-    VC_MeshType::Pointer inputMesh;
-    VC_MeshType::Pointer outputMesh = VC_MeshType::New();
-    volcart::shapes::Arch arch;
+    VC_MeshType::Pointer _in_ArchMesh;
+    VC_MeshType::Pointer _out_ArchMesh = VC_MeshType::New();
+    volcart::shapes::Arch _Arch;
+    double _ScaleFactor;
 };
 
-struct sphereFix {
+struct SphereFixture {
 
-    sphereFix() {
+    SphereFixture() {
 
-        inputMesh = sphere.itkMesh();
+        _in_SphereMesh = _Sphere.itkMesh();
+
+        _ScaleFactor = 3;
 
         std::cerr << "setting up spherical mesh for scaling" << std::endl;
     }
 
-    ~sphereFix(){ std::cerr << "cleaning up spherical mesh for scaling" << std::endl; }
+    ~SphereFixture(){ std::cerr << "cleaning up spherical mesh for scaling" << std::endl; }
 
-    VC_MeshType::Pointer inputMesh;
-    VC_MeshType::Pointer outputMesh = VC_MeshType::New();
-    volcart::shapes::Sphere sphere;
+    VC_MeshType::Pointer _in_SphereMesh;
+    VC_MeshType::Pointer _out_SphereMesh = VC_MeshType::New();
+    volcart::shapes::Sphere _Sphere;
+    double _ScaleFactor;
 };
 
-struct coneFix {
+struct ConeFixture {
 
-    coneFix() {
+    ConeFixture() {
 
-        inputMesh = cone.itkMesh();
+        _in_ConeMesh = _Cone.itkMesh();
+
+        _ScaleFactor = 3;
 
         std::cerr << "setting up cone mesh for scaling" << std::endl;
     }
 
-    ~coneFix(){ std::cerr << "cleaning up cone mesh for scaling" << std::endl; }
+    ~ConeFixture(){ std::cerr << "cleaning up cone mesh for scaling" << std::endl; }
 
-    VC_MeshType::Pointer inputMesh;
-    VC_MeshType::Pointer outputMesh = VC_MeshType::New();
-    volcart::shapes::Cone cone;
+    VC_MeshType::Pointer _in_ConeMesh;
+    VC_MeshType::Pointer _out_ConeMesh = VC_MeshType::New();
+    volcart::shapes::Cone _Cone;
+    double _ScaleFactor;
 };
 
 
-BOOST_FIXTURE_TEST_CASE(planeTest, planeFix){
+BOOST_FIXTURE_TEST_CASE(ScaledPlaneTest, PlaneFixture){
 
-    //scaling factor <,>,== 0 all used here
-    double factor = 3;
 
     //loop to cover positive, negative and zero value of factor
-    while (factor > -1.5){
+    while (_ScaleFactor > -1.5){
 
-        //call scaleMesh        TODO: outputMesh coming back NULL from scaleMesh()
-        volcart::meshing::scaleMesh(inputMesh, outputMesh, factor);
+        volcart::meshing::scaleMesh(_in_PlaneMesh, _out_PlaneMesh, _ScaleFactor);
 
-        BOOST_CHECK_EQUAL(inputMesh->GetNumberOfPoints(), outputMesh->GetNumberOfPoints());
+        BOOST_CHECK_EQUAL(_in_PlaneMesh->GetNumberOfPoints(), _out_PlaneMesh->GetNumberOfPoints());
 
 
-        for (size_t p; p < outputMesh->GetNumberOfPoints(); ++p){
+        for (size_t point; point < _out_PlaneMesh->GetNumberOfPoints(); ++point){
 
             //check each of the points in the input and output meshes to confirm
             //ratio matches scale factor
 
-            BOOST_CHECK_EQUAL(inputMesh->GetPoint(p)[0] * factor, outputMesh->GetPoint(p)[0]);
-            BOOST_CHECK_EQUAL(inputMesh->GetPoint(p)[1] * factor, outputMesh->GetPoint(p)[1]);
-            BOOST_CHECK_EQUAL(inputMesh->GetPoint(p)[2] * factor, outputMesh->GetPoint(p)[2]);
+            BOOST_CHECK_EQUAL(_in_PlaneMesh->GetPoint(point)[0] * _ScaleFactor, _out_PlaneMesh->GetPoint(point)[0]);
+            BOOST_CHECK_EQUAL(_in_PlaneMesh->GetPoint(point)[1] * _ScaleFactor, _out_PlaneMesh->GetPoint(point)[1]);
+            BOOST_CHECK_EQUAL(_in_PlaneMesh->GetPoint(point)[2] * _ScaleFactor, _out_PlaneMesh->GetPoint(point)[2]);
 
         }
 
-        factor -= 0.5;
+        _ScaleFactor -= 0.5;
     }
 
 }
 
-BOOST_FIXTURE_TEST_CASE(cubeTest, cubeFix){
+BOOST_FIXTURE_TEST_CASE(ScaledCubeTest, CubeFixture){
 
-    double factor = 3;
+    while (_ScaleFactor > -1.5){
 
-    while (factor > -1.5){
+        volcart::meshing::scaleMesh(_in_CubeMesh, _out_CubeMesh, _ScaleFactor);
 
-        volcart::meshing::scaleMesh(inputMesh, outputMesh, factor);
-
-        BOOST_CHECK_EQUAL(inputMesh->GetNumberOfPoints(), outputMesh->GetNumberOfPoints());
+        BOOST_CHECK_EQUAL(_in_CubeMesh->GetNumberOfPoints(), _out_CubeMesh->GetNumberOfPoints());
 
 
-        for (size_t p; p < outputMesh->GetNumberOfPoints(); ++p){
+        for (size_t point; point < _out_CubeMesh->GetNumberOfPoints(); ++point){
 
-            BOOST_CHECK_EQUAL(inputMesh->GetPoint(p)[0] * factor, outputMesh->GetPoint(p)[0]);
-            BOOST_CHECK_EQUAL(inputMesh->GetPoint(p)[1] * factor, outputMesh->GetPoint(p)[1]);
-            BOOST_CHECK_EQUAL(inputMesh->GetPoint(p)[2] * factor, outputMesh->GetPoint(p)[2]);
+            BOOST_CHECK_EQUAL(_in_CubeMesh->GetPoint(point)[0] * _ScaleFactor, _out_CubeMesh->GetPoint(point)[0]);
+            BOOST_CHECK_EQUAL(_in_CubeMesh->GetPoint(point)[1] * _ScaleFactor, _out_CubeMesh->GetPoint(point)[1]);
+            BOOST_CHECK_EQUAL(_in_CubeMesh->GetPoint(point)[2] * _ScaleFactor, _out_CubeMesh->GetPoint(point)[2]);
         }
 
-        factor -= 0.5;
+        _ScaleFactor -= 0.5;
     }
 }
 
-BOOST_FIXTURE_TEST_CASE(archTest, archFix){
+BOOST_FIXTURE_TEST_CASE(ScaledArchTest, ArchFixture){
 
-    double factor = 3;
+    while (_ScaleFactor > -1.5){
 
-    while (factor > -1.5){
+        volcart::meshing::scaleMesh(_in_ArchMesh, _out_ArchMesh, _ScaleFactor);
 
-        volcart::meshing::scaleMesh(inputMesh, outputMesh, factor);
+        BOOST_CHECK_EQUAL(_in_ArchMesh->GetNumberOfPoints(), _out_ArchMesh->GetNumberOfPoints());
 
-        volcart::io::objWriter writer;
+        for (size_t point; point < _out_ArchMesh->GetNumberOfPoints(); ++point){
 
-        BOOST_CHECK_EQUAL(inputMesh->GetNumberOfPoints(), outputMesh->GetNumberOfPoints());
-
-        for (size_t p; p < outputMesh->GetNumberOfPoints(); ++p){
-
-            BOOST_CHECK_EQUAL(inputMesh->GetPoint(p)[0] * factor, outputMesh->GetPoint(p)[0]);
-            BOOST_CHECK_EQUAL(inputMesh->GetPoint(p)[1] * factor, outputMesh->GetPoint(p)[1]);
-            BOOST_CHECK_EQUAL(inputMesh->GetPoint(p)[2] * factor, outputMesh->GetPoint(p)[2]);
+            BOOST_CHECK_EQUAL(_in_ArchMesh->GetPoint(point)[0] * _ScaleFactor, _out_ArchMesh->GetPoint(point)[0]);
+            BOOST_CHECK_EQUAL(_in_ArchMesh->GetPoint(point)[1] * _ScaleFactor, _out_ArchMesh->GetPoint(point)[1]);
+            BOOST_CHECK_EQUAL(_in_ArchMesh->GetPoint(point)[2] * _ScaleFactor, _out_ArchMesh->GetPoint(point)[2]);
         }
 
-        factor -= 0.5;
+        _ScaleFactor -= 0.5;
     }
 }
 
-BOOST_FIXTURE_TEST_CASE(sphereTest, sphereFix){
+BOOST_FIXTURE_TEST_CASE(ScaledSphereTest, SphereFixture){
 
-    double factor = 3;
+    while (_ScaleFactor > -1.5){
 
-    while (factor > -1.5){
+        volcart::meshing::scaleMesh(_in_SphereMesh, _out_SphereMesh, _ScaleFactor);
 
-        volcart::meshing::scaleMesh(inputMesh, outputMesh, factor);
+        BOOST_CHECK_EQUAL(_in_SphereMesh->GetNumberOfPoints(), _out_SphereMesh->GetNumberOfPoints());
 
-        BOOST_CHECK_EQUAL(inputMesh->GetNumberOfPoints(), outputMesh->GetNumberOfPoints());
+        for (size_t point; point < _out_SphereMesh->GetNumberOfPoints(); ++point){
 
-        for (size_t p; p < outputMesh->GetNumberOfPoints(); ++p){
-
-            BOOST_CHECK_EQUAL(inputMesh->GetPoint(p)[0] * factor, outputMesh->GetPoint(p)[0]);
-            BOOST_CHECK_EQUAL(inputMesh->GetPoint(p)[1] * factor, outputMesh->GetPoint(p)[1]);
-            BOOST_CHECK_EQUAL(inputMesh->GetPoint(p)[2] * factor, outputMesh->GetPoint(p)[2]);
+            BOOST_CHECK_EQUAL(_in_SphereMesh->GetPoint(point)[0] * _ScaleFactor, _out_SphereMesh->GetPoint(point)[0]);
+            BOOST_CHECK_EQUAL(_in_SphereMesh->GetPoint(point)[1] * _ScaleFactor, _out_SphereMesh->GetPoint(point)[1]);
+            BOOST_CHECK_EQUAL(_in_SphereMesh->GetPoint(point)[2] * _ScaleFactor, _out_SphereMesh->GetPoint(point)[2]);
         }
 
-        factor -= 0.5;
+        _ScaleFactor -= 0.5;
     }
 }
 
-BOOST_FIXTURE_TEST_CASE(coneTest, coneFix){
+BOOST_FIXTURE_TEST_CASE(ScaledConeTest, ConeFixture){
 
-    double factor = 3;
+    while (_ScaleFactor > -1.5){
 
-    while (factor > -1.5){
+        volcart::meshing::scaleMesh(_in_ConeMesh, _out_ConeMesh, _ScaleFactor);
 
-        volcart::meshing::scaleMesh(inputMesh, outputMesh, factor);
+        BOOST_CHECK_EQUAL(_in_ConeMesh->GetNumberOfPoints(), _out_ConeMesh->GetNumberOfPoints());
 
-        BOOST_CHECK_EQUAL(inputMesh->GetNumberOfPoints(), outputMesh->GetNumberOfPoints());
+        for (size_t point; point < _out_ConeMesh->GetNumberOfPoints(); ++point){
 
-        for (size_t p; p < outputMesh->GetNumberOfPoints(); ++p){
-
-            BOOST_CHECK_EQUAL(inputMesh->GetPoint(p)[0] * factor, outputMesh->GetPoint(p)[0]);
-            BOOST_CHECK_EQUAL(inputMesh->GetPoint(p)[1] * factor, outputMesh->GetPoint(p)[1]);
-            BOOST_CHECK_EQUAL(inputMesh->GetPoint(p)[2] * factor, outputMesh->GetPoint(p)[2]);
+            BOOST_CHECK_EQUAL(_in_ConeMesh->GetPoint(point)[0] * _ScaleFactor, _out_ConeMesh->GetPoint(point)[0]);
+            BOOST_CHECK_EQUAL(_in_ConeMesh->GetPoint(point)[1] * _ScaleFactor, _out_ConeMesh->GetPoint(point)[1]);
+            BOOST_CHECK_EQUAL(_in_ConeMesh->GetPoint(point)[2] * _ScaleFactor, _out_ConeMesh->GetPoint(point)[2]);
         }
 
-        factor -= 0.5;
+        _ScaleFactor -= 0.5;
     }
 }
 
-BOOST_FIXTURE_TEST_CASE(inputUnchangedTest, planeFix){
+BOOST_FIXTURE_TEST_CASE(ConfirmInputPlaneMeshIsUnchangedAfterScalingTest, PlaneFixture){
 
-    //scaling factor arbitrary
-    double factor = 25;
+    //reassign scale factor to a larger value
+    _ScaleFactor = 25;
 
     //call scaleMesh
-    volcart::meshing::scaleMesh(inputMesh, outputMesh, factor);
+    volcart::meshing::scaleMesh(_in_PlaneMesh, _out_PlaneMesh, _ScaleFactor);
 
     //init new plane mesh
-    VC_MeshType::Pointer planeMesh = plane.itkMesh();
+    VC_MeshType::Pointer NewPlaneMesh = _Plane.itkMesh();
 
-    //compare inputMesh and planeMesh to confirm inputMesh goes through
+    //compare _in_PlaneMesh and NewPlaneMesh to confirm _in_PlaneMesh goes through
     //scaleMesh unchanged
 
-    BOOST_CHECK_EQUAL(inputMesh->GetNumberOfPoints(), planeMesh->GetNumberOfPoints());
-    BOOST_CHECK_EQUAL(inputMesh->GetNumberOfCells(), planeMesh->GetNumberOfCells());
+    BOOST_CHECK_EQUAL(_in_PlaneMesh->GetNumberOfPoints(), NewPlaneMesh->GetNumberOfPoints());
+    BOOST_CHECK_EQUAL(_in_PlaneMesh->GetNumberOfCells(), NewPlaneMesh->GetNumberOfCells());
 
-    for (size_t p; p < inputMesh->GetNumberOfPoints(); ++p){
+    for (size_t point; point < _in_PlaneMesh->GetNumberOfPoints(); ++point){
 
-        BOOST_CHECK_EQUAL(inputMesh->GetPoint(p)[0], planeMesh->GetPoint(p)[0]);
-        BOOST_CHECK_EQUAL(inputMesh->GetPoint(p)[1], planeMesh->GetPoint(p)[1]);
-        BOOST_CHECK_EQUAL(inputMesh->GetPoint(p)[2], planeMesh->GetPoint(p)[2]);
+        BOOST_CHECK_EQUAL(_in_PlaneMesh->GetPoint(point)[0], NewPlaneMesh->GetPoint(point)[0]);
+        BOOST_CHECK_EQUAL(_in_PlaneMesh->GetPoint(point)[1], NewPlaneMesh->GetPoint(point)[1]);
+        BOOST_CHECK_EQUAL(_in_PlaneMesh->GetPoint(point)[2], NewPlaneMesh->GetPoint(point)[2]);
 
     }
 
     // Compare Cells
-    VC_CellIterator inputMeshCell = inputMesh->GetCells()->Begin();
-    VC_CellIterator planeMeshCell = planeMesh->GetCells()->Begin();
+    VC_CellIterator in_PlaneMeshCell = _in_PlaneMesh->GetCells()->Begin();
+    VC_CellIterator NewPlaneMeshCell = NewPlaneMesh->GetCells()->Begin();
 
     int c = 0;
-    while (inputMeshCell != inputMesh->GetCells()->End()) {
+    while (in_PlaneMeshCell != _in_PlaneMesh->GetCells()->End()) {
 
         //Initialize Iterators for Points in a Cell
-        VC_PointsInCellIterator inputMeshPoint = inputMeshCell.Value()->PointIdsBegin();
-        VC_PointsInCellIterator planeMeshPoint = planeMeshCell.Value()->PointIdsBegin();
+        VC_PointsInCellIterator in_PlaneMeshPointId = in_PlaneMeshCell.Value()->PointIdsBegin();
+        VC_PointsInCellIterator NewPlaneMeshPointId = NewPlaneMeshCell.Value()->PointIdsBegin();
 
         int counter = 0;
         //while we have points in the cell
-        while ( inputMeshPoint != inputMeshCell.Value()->PointIdsEnd() ) {
+        while ( in_PlaneMeshPointId != in_PlaneMeshCell.Value()->PointIdsEnd() ) {
 
             //Now to check the points within the cells
             if (counter == 0)
-                BOOST_CHECK_EQUAL(*inputMeshPoint, *planeMeshPoint);
+                BOOST_CHECK_EQUAL(*in_PlaneMeshPointId, *NewPlaneMeshPointId);
             else if(counter == 1)
-                BOOST_CHECK_EQUAL(*inputMeshPoint, *planeMeshPoint);
+                BOOST_CHECK_EQUAL(*in_PlaneMeshPointId, *NewPlaneMeshPointId);
             else if (counter == 2)
-                BOOST_CHECK_EQUAL(*inputMeshPoint, *planeMeshPoint);
+                BOOST_CHECK_EQUAL(*in_PlaneMeshPointId, *NewPlaneMeshPointId);
 
             //increment points
-            ++inputMeshPoint; ++planeMeshPoint; ++counter;
-
+            ++in_PlaneMeshPointId; ++NewPlaneMeshPointId; ++counter;
         }
 
         //increment cells
-        ++inputMeshCell; ++planeMeshCell;
+        ++in_PlaneMeshCell; ++NewPlaneMeshCell;
     }
-
 }
