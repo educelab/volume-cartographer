ce96c8acd9cdb897aaa728a806d8d48a0a7b3b2f
Ryan Taber <ryan@Viss-iMac-2.local>
2016-02-17 14:54:18 -0500

    adding in greedyProjectionMeshingTest updates

--- examples/greedyProjectionMeshingExample.cpp
+++ examples/greedyProjectionMeshingExample.cpp
@@ -23,103 +23,51 @@
 #include "greedyProjectionMeshing.h"
 
 using namespace volcart::meshing;
-void compareMeshes (const ::pcl::PolygonMesh &output, const ::pcl::PolygonMesh &old_mesh);
 
 int main(int argc, char* argv[]) {
 
-    /* If arguments changed from the set values in greedyProjectionMeshing.h
-    if ( argc != 5 ) {
-        std::cout << "Incorrect arguments" << endl;
-        return 1;
-    }
-    unsigned max = argv[2];
-    double radius = argv[3];
-    double radiusMultiplier = argv[4];
-    */
+    //init shapes
+    volcart::shapes::Plane Plane;
+    volcart::shapes::Cube Cube;
+    volcart::shapes::Arch Arch;
+    volcart::shapes::Sphere Sphere;
+    volcart::shapes::Cone Cone;
 
-    // Create a mesh
-    volcart::shapes::Plane mesh;
-    pcl::PointCloud<pcl::PointNormal> cloud_PointNormal = mesh.pointCloudNormal();
+    //convert shaped meshes to point cloud
+    pcl::PointCloud<pcl::PointNormal> in_PlanePointNormalCloud = Plane.pointCloudNormal();
+    pcl::PointCloud<pcl::PointNormal> in_CubePointNormalCloud = Cube.pointCloudNormal();
+    pcl::PointCloud<pcl::PointNormal> in_ArchPointNormalCloud = Arch.pointCloudNormal();
+    pcl::PointCloud<pcl::PointNormal> in_SpherePointNormalCloud = Sphere.pointCloudNormal();
+    pcl::PointCloud<pcl::PointNormal> in_ConePointNormalCloud = Cone.pointCloudNormal();
 
-    // Create pointer for the mesh to pass to greedyProjectionMeshing
-    pcl::PointCloud<pcl::PointNormal>::Ptr input( new pcl::PointCloud<pcl::PointNormal>);
-    *input = cloud_PointNormal;
+    // Create ptr to point clouds and fill --> needed for the call to greedyProjectionMeshing()
+    pcl::PointCloud<pcl::PointNormal>::Ptr in_PlanePointNormalCloudPtr( new pcl::PointCloud<pcl::PointNormal>);
+    pcl::PointCloud<pcl::PointNormal>::Ptr in_CubePointNormalCloudPtr( new pcl::PointCloud<pcl::PointNormal>);
+    pcl::PointCloud<pcl::PointNormal>::Ptr in_ArchPointNormalCloudPtr( new pcl::PointCloud<pcl::PointNormal>);
+    pcl::PointCloud<pcl::PointNormal>::Ptr in_SpherePointNormalCloudPtr( new pcl::PointCloud<pcl::PointNormal>);
+    pcl::PointCloud<pcl::PointNormal>::Ptr in_ConePointNormalCloudPtr( new pcl::PointCloud<pcl::PointNormal>);
 
-    // Call function from namespace in header file
-    std::cout << "Being greedy..." << std::endl;
-    pcl::PolygonMesh output = greedyProjectionMeshing(input, 100, 2.0, 2.5);
+    *in_PlanePointNormalCloudPtr = in_PlanePointNormalCloud;
+    *in_CubePointNormalCloudPtr = in_CubePointNormalCloud;
+    *in_ArchPointNormalCloudPtr = in_ArchPointNormalCloud;
+    *in_SpherePointNormalCloudPtr = in_SpherePointNormalCloud;
+    *in_ConePointNormalCloudPtr = in_ConePointNormalCloud;
 
-    // Write mesh to file
-    pcl::io::saveOBJFile ( "greedyExample.obj", output);
+    // Call function from
+    pcl::PolygonMesh out_PlanePolyMesh = greedyProjectionMeshing(in_PlanePointNormalCloudPtr, 100, 2.0, 2.5);
+    pcl::PolygonMesh out_CubePolyMesh = greedyProjectionMeshing(in_CubePointNormalCloudPtr, 100, 2.0, 2.5);
+    pcl::PolygonMesh out_ArchPolyMesh = greedyProjectionMeshing(in_ArchPointNormalCloudPtr, 100, 2.0, 2.5);
+    pcl::PolygonMesh out_SpherePolyMesh = greedyProjectionMeshing(in_SpherePointNormalCloudPtr, 100, 2.0, 2.5);
+    pcl::PolygonMesh out_ConePolyMesh = greedyProjectionMeshing(in_ConePointNormalCloudPtr, 100, 2.0, 2.5);
 
-    std::cout << "File saved as greedyExample.obj" << std::endl;
 
-    // Load in old mesh for comparison
-    pcl::PolygonMesh old_mesh ;
-    pcl::io::loadOBJFile("greedyExampleChanged.obj", old_mesh );
+    // Write meshes to file
+    pcl::io::saveOBJFile ( "PlaneGreedyProjectionMeshing.obj", out_PlanePolyMesh);
+    pcl::io::saveOBJFile ( "CubeGreedyProjectionMeshing.obj", out_CubePolyMesh);
+    pcl::io::saveOBJFile ( "ArchGreedyProjectionMeshing.obj", out_ArchPolyMesh);
+    pcl::io::saveOBJFile ( "SphereGreedyProjectionMeshing.obj", out_SpherePolyMesh);
+    pcl::io::saveOBJFile ( "ConeGreedyProjectionMeshing.obj", out_ConePolyMesh);
 
-    // Call comparison function
-    compareMeshes(output, old_mesh);
-
-   return 0;
+    return EXIT_SUCCESS;
 }
 
-// Comparison function to check for accurate results, compare each point and each cell/face
-void compareMeshes (const ::pcl::PolygonMesh &output, const ::pcl::PolygonMesh &old_mesh) {
-
-    // Convert each mesh to obtain the correct points/data
-    pcl::PointCloud<pcl::PointNormal> convOutputCloud;
-    pcl::fromPCLPointCloud2(output.cloud, convOutputCloud);
-
-    pcl::PointCloud<pcl::PointNormal> convOldCloud;
-    pcl::fromPCLPointCloud2(output.cloud, convOldCloud);
-
-    // Check size of data in both meshes
-    if (convOutputCloud.points.size() != convOldCloud.points.size()){
-        std::cout << "Points vectors are different sizes, therefore they're not equal. " << std::endl;
-        return;
-    }
-
-    // Otherwise, compare points
-    else {
-        // Check points in cloud
-        for (int i = 0; i < convOutputCloud.points.size(); i++) {
-
-            // 4th thing in data array is a type, so don't use it
-            for (int m = 0; m < 3; m++ ) {
-
-                if (convOutputCloud.points[i].data[m] != convOldCloud.points[i].data[m]) {
-                    std::cout << "Cloud points do not match at points[" << i << "].data[" << m << "]" << std::endl;
-                    std::cout << convOutputCloud.points[i].data[0] << convOutputCloud.points[i].data[1] <<
-                    convOutputCloud.points[i].data[2] << convOutputCloud.points[i].data[3]
-                    << " || " << convOldCloud.points[i].data[0] << convOldCloud.points[i].data[1] <<
-                    convOldCloud.points[i].data[2] << convOldCloud.points[i].data[3] << std::endl;
-
-                }
-            }
-        }
-    }
-
-    // Compare size of polygon vectors
-    if (output.polygons.size() != old_mesh.polygons.size()) {
-        std::cout << "Polygon vectors are different sizes, therefore they're not equal." << endl;
-        return;
-    }
-
-    // Check faces
-    // Iterate through each of the vertices of both new and old mesh and compare
-    for (int i = 0; i < output.polygons.size(); i++) {
-
-        for (int j = 0; j < output.polygons[i].vertices.size(); j++) {
-
-            if (output.polygons[i].vertices[j] != old_mesh.polygons[i].vertices[j]) {
-                std::cout << "Difference at polygon " << i << " at vertex " << j << endl;
-                return;
-            }
-        }
-    }
-
-    std::cout << std::endl << "The meshes are the same!" << std::endl;
-
-    return;
-}
--- testing/meshing/greedyProjectionMeshingTest.cpp
+++ testing/meshing/greedyProjectionMeshingTest.cpp
@@ -8,7 +8,7 @@
 #define BOOST_TEST_MODULE greedyProjectionMeshing
 
 #include <boost/test/unit_test.hpp>
-//#include <boost/test/included/unit_test.hpp>
+
 #include <boost/test/unit_test_log.hpp>
 #include "vc_defines.h"
 #include "shapes.h"
@@ -34,6 +34,13 @@
  *         correct mesh based on a previously created, correct mesh. This is performed  *
  *         via the compareMeshes test case.                                             *
  *                                                                                      *
+ *  Test Cases:                                                                         *
+ *  1. CompareSavedAndFixturePlaneGreedyProjections                                     *
+ *  2. CompareSavedAndFixtureCubeGreedyProjections                                      *
+ *  3. CompareSavedAndFixtureArchGreedyProjections                                      *
+ *  4. CompareSavedAndFixtureSphereGreedyProjections                                    *
+ *  5. CompareSavedAndFixtureConeGreedyProjections                                      *
+ *                                                                                      *
  *  Input:                                                                              *
  *      - OBJ file to be loaded in as a PCL::PolygonMesh                                *
  *                                                                                      *
@@ -54,88 +61,259 @@
 //   If errors occur, output them. Otherwise give success message.
 
 
-/*
- * This builds objects for the test cases below that reference
- * the fixture as their second argument
- */
+struct PlaneGreedyProjectionFixture {
 
-struct Fix {
+    PlaneGreedyProjectionFixture() {
 
-    Fix() {
-
-        std::cerr << "Setting up greedyProjectionMeshingTest objects." << std::endl;
+        std::cerr << "Setting up greedyProjectionMeshing Plane objects." << std::endl;
 
         // Create a mesh
-        pcl::PointCloud<pcl::PointNormal> cloud_PointNormal = mesh.pointCloudNormal();
+         _in_PlanePointNormalCloud = _Plane.pointCloudNormal();
 
         // Create pointer for the mesh to pass to greedyProjectionMeshing
-        pcl::PointCloud<pcl::PointNormal>::Ptr input( new pcl::PointCloud<pcl::PointNormal>);
-        *input = cloud_PointNormal;
-
-        // Call function from namespace in header file
-        new_mesh = volcart::meshing::greedyProjectionMeshing(input, 100, 2.0, 2.5);
-
-        //uncomment to enable inline obj file generation
-        // Write mesh to file
-        // pcl::io::saveOBJFile ( "greedyExample.obj", output);
-        //std::cout << "File saved as greedyExample.obj" << std::endl;
-
-
+        pcl::PointCloud<pcl::PointNormal>::Ptr _in_PlanePointNormalCloudPtr( new pcl::PointCloud<pcl::PointNormal>);
+        *_in_PlanePointNormalCloudPtr = _in_PlanePointNormalCloud;
+        
+        // Call greedyProjectionMeshing() and store results in PolygonMesh
+        _out_FixturePlanePolygonMesh = volcart::meshing::greedyProjectionMeshing(_in_PlanePointNormalCloudPtr, 100, 2.0, 2.5);
+        
         // Load in old mesh for comparison in the compareMeshes test case below
-        pcl::io::loadOBJFile("greedyExample.obj", saved_mesh);
+        pcl::io::loadOBJFile("PlaneGreedyProjectionMeshing.obj", _in_SavedPlanePolygonMesh);
 
+        pcl::fromPCLPointCloud2(_out_FixturePlanePolygonMesh.cloud, _out_FixturePlanePointCloud);
+        pcl::fromPCLPointCloud2(_in_SavedPlanePolygonMesh.cloud, _out_SavedPlanePointCloud);    
     }
 
-    ~Fix(){ std::cerr << "Cleaning up greedyProjectionMeshing objects" << std::endl; }
+    ~PlaneGreedyProjectionFixture(){ std::cerr << "Cleaning up greedyProjectionMeshing Plane objects" << std::endl; }
 
-    volcart::shapes::Plane mesh;
-    pcl::PolygonMesh saved_mesh;
-    pcl::PolygonMesh new_mesh;
+    volcart::shapes::Plane _Plane;
+    pcl::PointCloud<pcl::PointNormal> _in_PlanePointNormalCloud;
+    pcl::PolygonMesh _out_FixturePlanePolygonMesh, _in_SavedPlanePolygonMesh;
+    
+    // point clouds to be filled for comparison in test case
+    pcl::PointCloud<pcl::PointNormal> _out_FixturePlanePointCloud, _out_SavedPlanePointCloud;
+        
 };
 
+struct CubeGreedyProjectionFixture {
 
-// Comparison function to check for accurate results, compare each point and each cell/face
-BOOST_FIXTURE_TEST_CASE(compareMeshes, Fix)
+    CubeGreedyProjectionFixture() {
+
+        std::cerr << "Setting up greedyProjectionMeshing Cube objects." << std::endl;
+
+        _in_CubePointNormalCloud = _Cube.pointCloudNormal();
+        pcl::PointCloud<pcl::PointNormal>::Ptr _in_CubePointNormalCloudPtr( new pcl::PointCloud<pcl::PointNormal>);
+        *_in_CubePointNormalCloudPtr = _in_CubePointNormalCloud;
+        _out_FixtureCubePolygonMesh = volcart::meshing::greedyProjectionMeshing(_in_CubePointNormalCloudPtr, 100, 2.0, 2.5);
+        pcl::io::loadOBJFile("CubeGreedyProjectionMeshing.obj", _in_SavedCubePolygonMesh);
+        pcl::fromPCLPointCloud2(_out_FixtureCubePolygonMesh.cloud, _out_FixtureCubePointCloud);
+        pcl::fromPCLPointCloud2(_in_SavedCubePolygonMesh.cloud, _out_SavedCubePointCloud);
+    }
+
+    ~CubeGreedyProjectionFixture(){ std::cerr << "Cleaning up greedyProjectionMeshing Cube objects" << std::endl; }
+
+    volcart::shapes::Cube _Cube;
+    pcl::PointCloud<pcl::PointNormal> _in_CubePointNormalCloud;
+    pcl::PolygonMesh _out_FixtureCubePolygonMesh, _in_SavedCubePolygonMesh;
+    pcl::PointCloud<pcl::PointNormal> _out_FixtureCubePointCloud, _out_SavedCubePointCloud;
+
+};
+
+struct ArchGreedyProjectionFixture {
+
+    ArchGreedyProjectionFixture() {
+
+        std::cerr << "Setting up greedyProjectionMeshing Arch objects." << std::endl;
+
+        _in_ArchPointNormalCloud = _Arch.pointCloudNormal();
+        pcl::PointCloud<pcl::PointNormal>::Ptr _in_ArchPointNormalCloudPtr( new pcl::PointCloud<pcl::PointNormal>);
+        *_in_ArchPointNormalCloudPtr = _in_ArchPointNormalCloud;
+        _out_FixtureArchPolygonMesh = volcart::meshing::greedyProjectionMeshing(_in_ArchPointNormalCloudPtr, 100, 2.0, 2.5);
+        pcl::io::loadOBJFile("ArchGreedyProjectionMeshing.obj", _in_SavedArchPolygonMesh);
+        pcl::fromPCLPointCloud2(_out_FixtureArchPolygonMesh.cloud, _out_FixtureArchPointCloud);
+        pcl::fromPCLPointCloud2(_in_SavedArchPolygonMesh.cloud, _out_SavedArchPointCloud);
+    }
+
+    ~ArchGreedyProjectionFixture(){ std::cerr << "Cleaning up greedyProjectionMeshing Arch objects" << std::endl; }
+
+    volcart::shapes::Arch _Arch;
+    pcl::PointCloud<pcl::PointNormal> _in_ArchPointNormalCloud;
+    pcl::PolygonMesh _out_FixtureArchPolygonMesh, _in_SavedArchPolygonMesh;
+    pcl::PointCloud<pcl::PointNormal> _out_FixtureArchPointCloud, _out_SavedArchPointCloud;
+
+};
+
+struct SphereGreedyProjectionFixture {
+
+    SphereGreedyProjectionFixture() {
+
+        std::cerr << "Setting up greedyProjectionMeshing Sphere objects." << std::endl;
+
+        _in_SpherePointNormalCloud = _Sphere.pointCloudNormal();
+        pcl::PointCloud<pcl::PointNormal>::Ptr _in_SpherePointNormalCloudPtr( new pcl::PointCloud<pcl::PointNormal>);
+        *_in_SpherePointNormalCloudPtr = _in_SpherePointNormalCloud;
+        _out_FixtureSpherePolygonMesh = volcart::meshing::greedyProjectionMeshing(_in_SpherePointNormalCloudPtr, 100, 2.0, 2.5);
+        pcl::io::loadOBJFile("SphereGreedyProjectionMeshing.obj", _in_SavedSpherePolygonMesh);
+        pcl::fromPCLPointCloud2(_out_FixtureSpherePolygonMesh.cloud, _out_FixtureSpherePointCloud);
+        pcl::fromPCLPointCloud2(_in_SavedSpherePolygonMesh.cloud, _out_SavedSpherePointCloud);
+    }
+
+    ~SphereGreedyProjectionFixture(){ std::cerr << "Cleaning up greedyProjectionMeshing Sphere objects" << std::endl; }
+
+    volcart::shapes::Sphere _Sphere;
+    pcl::PointCloud<pcl::PointNormal> _in_SpherePointNormalCloud;
+    pcl::PolygonMesh _out_FixtureSpherePolygonMesh, _in_SavedSpherePolygonMesh;
+    pcl::PointCloud<pcl::PointNormal> _out_FixtureSpherePointCloud, _out_SavedSpherePointCloud;
+
+};
+
+struct ConeGreedyProjectionFixture {
+
+    ConeGreedyProjectionFixture() {
+
+        std::cerr << "Setting up greedyProjectionMeshing Cone objects." << std::endl;
+
+        _in_ConePointNormalCloud = _Cone.pointCloudNormal();
+        pcl::PointCloud<pcl::PointNormal>::Ptr _in_ConePointNormalCloudPtr( new pcl::PointCloud<pcl::PointNormal>);
+        *_in_ConePointNormalCloudPtr = _in_ConePointNormalCloud;
+        _out_FixtureConePolygonMesh = volcart::meshing::greedyProjectionMeshing(_in_ConePointNormalCloudPtr, 100, 2.0, 2.5);
+        pcl::io::loadOBJFile("ConeGreedyProjectionMeshing.obj", _in_SavedConePolygonMesh);
+        pcl::fromPCLPointCloud2(_out_FixtureConePolygonMesh.cloud, _out_FixtureConePointCloud);
+        pcl::fromPCLPointCloud2(_in_SavedConePolygonMesh.cloud, _out_SavedConePointCloud);
+
+    }
+
+    ~ConeGreedyProjectionFixture(){ std::cerr << "Cleaning up greedyProjectionMeshing Cone objects" << std::endl; }
+
+    volcart::shapes::Cone _Cone;
+    pcl::PointCloud<pcl::PointNormal> _in_ConePointNormalCloud;
+    pcl::PolygonMesh _out_FixtureConePolygonMesh, _in_SavedConePolygonMesh;
+    pcl::PointCloud<pcl::PointNormal> _out_FixtureConePointCloud, _out_SavedConePointCloud;
+
+};
+
+BOOST_FIXTURE_TEST_CASE(CompareSavedAndFixturePlaneGreedyProjections, PlaneGreedyProjectionFixture)
 {
+    // Check number of points and cells in each of the PolygonMesh objects for equality
+    BOOST_CHECK_EQUAL (_out_FixturePlanePointCloud.points.size(), _out_SavedPlanePointCloud.points.size());
+    BOOST_CHECK_EQUAL (_out_FixturePlanePolygonMesh.polygons.size(), _in_SavedPlanePolygonMesh.polygons.size());
 
-    // Convert each mesh to obtain the correct points/data
-    pcl::PointCloud<pcl::PointNormal> convOutputCloud;
-    pcl::fromPCLPointCloud2(new_mesh.cloud, convOutputCloud);
+    // Points
+    for (int p = 0; p < _out_FixturePlanePointCloud.points.size(); p++) {
+        for (int d = 0; d < 3; d++ ) {
 
-    pcl::PointCloud<pcl::PointNormal> convOldCloud;
-    pcl::fromPCLPointCloud2(saved_mesh.cloud, convOldCloud);
-
-    // Compare size of points in each of the PolygonMesh objects
-    BOOST_CHECK_EQUAL (convOutputCloud.points.size(), convOldCloud.points.size());
-
-
-    // Check points in cloud
-    for (int i = 0; i < convOutputCloud.points.size(); i++) {
-
-        // 4th element in data array is a type, so don't use it
-        for (int m = 0; m < 3; m++ ) {
-
-            BOOST_CHECK_EQUAL (convOutputCloud.points[i].data[m], convOldCloud.points[i].data[m]) ;
+            BOOST_CHECK_EQUAL (_out_FixturePlanePointCloud.points[p].data[d], 
+                                                                          _out_SavedPlanePointCloud.points[p].data[d]);
         }
     }
+   
+    // Cells
+    for (int c_id = 0; c_id < _out_FixturePlanePolygonMesh.polygons.size(); c_id++) {
+        for (int pnt = 0; pnt < _out_FixturePlanePolygonMesh.polygons[c_id].vertices.size(); pnt++) {
 
-
-    // Compare size of polygons in each of the PolygonMesh objects
-    BOOST_CHECK_EQUAL (new_mesh.polygons.size(), saved_mesh.polygons.size());
-
-    // Checking faces for equality
-    // Compare each of the vertices of both new and saved meshes
-    for (int i = 0; i < new_mesh.polygons.size(); i++) {
-
-        for (int j = 0; j < new_mesh.polygons[i].vertices.size(); j++) {
-
-            BOOST_CHECK_EQUAL (new_mesh.polygons[i].vertices[j] , saved_mesh.polygons[i].vertices[j]);
+            BOOST_CHECK_EQUAL (_out_FixturePlanePolygonMesh.polygons[c_id].vertices[pnt],
+                                                               _in_SavedPlanePolygonMesh.polygons[c_id].vertices[pnt]);
         }
     }
-
-    std::cout << std::endl << "The meshes are the same!" << std::endl;
-
-    return;
 }
 
+BOOST_FIXTURE_TEST_CASE(CompareSavedAndFixtureCubeGreedyProjections, CubeGreedyProjectionFixture)
+{
+    // Check number of points and cells in each of the PolygonMesh objects for equality
+    BOOST_CHECK_EQUAL (_out_FixtureCubePointCloud.points.size(), _out_SavedCubePointCloud.points.size());
+    BOOST_CHECK_EQUAL (_out_FixtureCubePolygonMesh.polygons.size(), _in_SavedCubePolygonMesh.polygons.size());
+
+    // Points
+    for (int p = 0; p < _out_FixtureCubePointCloud.points.size(); p++) {
+        for (int d = 0; d < 3; d++ ) {
+
+            BOOST_CHECK_EQUAL (_out_FixtureCubePointCloud.points[p].data[d],
+                                                                         _out_SavedCubePointCloud.points[p].data[d]);
+        }
+    }
+
+    // Cells
+    for (int c_id = 0; c_id < _out_FixtureCubePolygonMesh.polygons.size(); c_id++) {
+        for (int pnt = 0; pnt < _out_FixtureCubePolygonMesh.polygons[c_id].vertices.size(); pnt++) {
+
+            BOOST_CHECK_EQUAL (_out_FixtureCubePolygonMesh.polygons[c_id].vertices[pnt],
+                                                               _in_SavedCubePolygonMesh.polygons[c_id].vertices[pnt]);
+        }
+    }
+}
+
+BOOST_FIXTURE_TEST_CASE(CompareSavedAndFixtureArchGreedyProjections, ArchGreedyProjectionFixture)
+{
+    // Check number of points and cells in each of the PolygonMesh objects for equality
+    BOOST_CHECK_EQUAL (_out_FixtureArchPointCloud.points.size(), _out_SavedArchPointCloud.points.size());
+    BOOST_CHECK_EQUAL (_out_FixtureArchPolygonMesh.polygons.size(), _in_SavedArchPolygonMesh.polygons.size());
+
+    // Points
+    for (int p = 0; p < _out_FixtureArchPointCloud.points.size(); p++) {
+        for (int d = 0; d < 3; d++ ) {
+
+            BOOST_CHECK_EQUAL (_out_FixtureArchPointCloud.points[p].data[d],
+                                                                           _out_SavedArchPointCloud.points[p].data[d]);
+        }
+    }
+
+    // Cells
+    for (int c_id = 0; c_id < _out_FixtureArchPolygonMesh.polygons.size(); c_id++) {
+        for (int pnt = 0; pnt < _out_FixtureArchPolygonMesh.polygons[c_id].vertices.size(); pnt++) {
+
+            BOOST_CHECK_EQUAL (_out_FixtureArchPolygonMesh.polygons[c_id].vertices[pnt],
+                                                               _in_SavedArchPolygonMesh.polygons[c_id].vertices[pnt]);
+        }
+    }
+}
+
+BOOST_FIXTURE_TEST_CASE(CompareSavedAndFixtureSphereGreedyProjections, SphereGreedyProjectionFixture)
+{
+    // Check number of points and cells in each of the PolygonMesh objects for equality
+    BOOST_CHECK_EQUAL (_out_FixtureSpherePointCloud.points.size(), _out_SavedSpherePointCloud.points.size());
+    BOOST_CHECK_EQUAL (_out_FixtureSpherePolygonMesh.polygons.size(), _in_SavedSpherePolygonMesh.polygons.size());
+
+    // Points
+    for (int p = 0; p < _out_FixtureSpherePointCloud.points.size(); p++) {
+        for (int d = 0; d < 3; d++ ) {
+
+            BOOST_CHECK_EQUAL (_out_FixtureSpherePointCloud.points[p].data[d],
+                                                                        _out_SavedSpherePointCloud.points[p].data[d]);
+        }
+    }
+
+    // Cells
+    for (int c_id = 0; c_id < _out_FixtureSpherePolygonMesh.polygons.size(); c_id++) {
+        for (int pnt = 0; pnt < _out_FixtureSpherePolygonMesh.polygons[c_id].vertices.size(); pnt++) {
+
+            BOOST_CHECK_EQUAL (_out_FixtureSpherePolygonMesh.polygons[c_id].vertices[pnt],
+                                                              _in_SavedSpherePolygonMesh.polygons[c_id].vertices[pnt]);
+        }
+    }
+}
+
+BOOST_FIXTURE_TEST_CASE(CompareSavedAndFixtureConeGreedyProjections, ConeGreedyProjectionFixture)
+{
+    // Check number of points and cells in each of the PolygonMesh objects for equality
+    BOOST_CHECK_EQUAL (_out_FixtureConePointCloud.points.size(), _out_SavedConePointCloud.points.size());
+    BOOST_CHECK_EQUAL (_out_FixtureConePolygonMesh.polygons.size(), _in_SavedConePolygonMesh.polygons.size());
+
+    // Points
+    for (int p = 0; p < _out_FixtureConePointCloud.points.size(); p++) {
+        for (int d = 0; d < 3; d++ ) {
+
+            BOOST_CHECK_EQUAL (_out_FixtureConePointCloud.points[p].data[d],
+                                                                          _out_SavedConePointCloud.points[p].data[d]);
+        }
+    }
+
+    // Cells
+    for (int c_id = 0; c_id < _out_FixtureConePolygonMesh.polygons.size(); c_id++) {
+        for (int pnt = 0; pnt < _out_FixtureConePolygonMesh.polygons[c_id].vertices.size(); pnt++) {
+
+            BOOST_CHECK_EQUAL (_out_FixtureConePolygonMesh.polygons[c_id].vertices[pnt],
+                                                               _in_SavedConePolygonMesh.polygons[c_id].vertices[pnt]);
+        }
+    }
+}
 
