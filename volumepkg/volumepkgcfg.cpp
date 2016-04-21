dc4aac9672ec1b81caa394ce4158d5bc755c1a34
Sean Karlage <sean.karlage@uky.edu>
2016-04-21 14:46:46 -0400

    Make const-correct + take advantage of C++11 niceness

--- volumepkg/volumepkgcfg.cpp
+++ volumepkg/volumepkgcfg.cpp
@@ -1,46 +1,36 @@
 #include "volumepkgcfg.h"
 
 ///// CONSTRUCTORS /////
-VolumePkgCfg::VolumePkgCfg() {
+VolumePkgCfg::VolumePkgCfg()
+{
     picojson::object obj;
     obj["version"] = picojson::value(VOLPKG_VERSION);
     _json = picojson::value(obj);
 }
 
 // Construct a new json config using a Dictionary as a template
-VolumePkgCfg::VolumePkgCfg(volcart::Dictionary dict, double version) {
-
+VolumePkgCfg::VolumePkgCfg(const volcart::Dictionary& dict, double version)
+{
     // Populate the cfg with keys from the dict
     picojson::object obj;
     volcart::Dictionary::const_iterator entry = dict.begin();
-    while ( entry != dict.end() ) {
-
-        // Version must be populated correctly for volumepkg setMetadata() functions to work
-        if ( entry->first == "version" ) {
+    for (const auto& entry : dict) {
+        if (entry.first == "version") {
             obj["version"] = picojson::value(version);
-
-            ++entry;
             continue;
         }
 
         // Default values
-        std::string type = entry->second;
-        if ( type == "int" ) {
+        if (entry.second == "int") {
             int initValue = 0;
-            obj[entry->first] = picojson::value(double(initValue));
+            obj[entry.first] = picojson::value(double(initValue));
+        } else if (entry.second == "double") {
+            double initValue = 0;
+            obj[entry.first] = picojson::value(initValue);
+        } else {
+            std::string initValue{""};
+            obj[entry.first] = picojson::value(initValue);
         }
-        else
-        if ( type == "double" ) {
-            double initValue = 0.0;
-            obj[entry->first] = picojson::value(initValue);
-        }
-        else {
-            std::string initValue = "";
-            obj[entry->first] = picojson::value(initValue);
-        }
-
-        ++entry;
     }
-
     _json = picojson::value(obj);
 };
