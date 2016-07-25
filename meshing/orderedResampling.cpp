1e44688569d61572dd3e74d70aa01dae9435ef4c
Hannah Hatch <hjha225@g.uky.edu>
2016-07-25 11:23:05 -0400

    Added files that will contain code

---
+++ meshing/orderedResampling.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Hannah Hatch on 7/25/16.
+//
+
+#include "orderedResampling.h"
---
+++ meshing/orderedResampling.h
@@ -0,0 +1,17 @@
+//
+// Created by Hannah Hatch on 7/25/16.
+//
+
+#ifndef VC_ORDEREDRESAMPLING_H
+#define VC_ORDEREDRESAMPLING_H
+
+namespace volcart {
+    namespace meshing {
+        class orderedResampling {
+
+        }; //orderedResampling
+    } //meshing
+} //volcart
+
+
+#endif //VC_ORDEREDRESAMPLING_H
