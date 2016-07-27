d53087314372162ffc8d7f943b949276c5ba32ef
Seth Parker <csparker247@gmail.com>
2016-07-27 13:39:50 -0400

    (testing) Generalize abf test variable names

--- testing/texturing/abfTest.cpp
+++ testing/texturing/abfTest.cpp
@@ -58,15 +58,15 @@ struct CreatePlaneABFUVFixture{
         std::cerr << "Creating Plane UV map using ABF..." << std::endl;
 
         //Get ITK Mesh
-        _in_PlaneITKMesh = _Plane.itkMesh();
+        _in_Mesh = _Plane.itkMesh();
 
         //Create uvMap from mesh
-        volcart::texturing::abf abf(_in_PlaneITKMesh);
+        volcart::texturing::abf abf(_in_Mesh);
         abf.compute();
-        _out_PlaneITKMesh = abf.getMesh();
+        _out_Mesh = abf.getMesh();
 
         // Load pre-generated output from file
-        volcart::testing::ParsingHelpers::parseObjFile("abf_Plane.obj", _SavedPlanePoints, _SavedPlaneCells);
+        volcart::testing::ParsingHelpers::parseObjFile("abf_Plane.obj", _SavedPoints, _SavedCells);
 
     }
 
@@ -74,11 +74,11 @@ struct CreatePlaneABFUVFixture{
 
     //declare Plane mesh and width and height
     volcart::shapes::Plane _Plane;
-    VC_MeshType::Pointer _in_PlaneITKMesh;
-    VC_MeshType::Pointer _out_PlaneITKMesh;
+    VC_MeshType::Pointer _in_Mesh;
+    VC_MeshType::Pointer _out_Mesh;
 
-    std::vector<VC_Vertex> _SavedPlanePoints;
-    std::vector<VC_Cell>   _SavedPlaneCells;
+    std::vector<VC_Vertex> _SavedPoints;
+    std::vector<VC_Cell>   _SavedCells;
 };
 
 struct CreatePlaneABFLSCMOnlyUVFixture{
@@ -88,16 +88,16 @@ struct CreatePlaneABFLSCMOnlyUVFixture{
         std::cerr << "Creating Plane UV map using ABF (LSCM only)..." << std::endl;
 
         //Get ITK Mesh
-        _in_PlaneITKMesh = _Plane.itkMesh();
+        _in_Mesh = _Plane.itkMesh();
 
         //Create uvMap from mesh
-        volcart::texturing::abf abf(_in_PlaneITKMesh);
+        volcart::texturing::abf abf(_in_Mesh);
         abf.setUseABF(false);
         abf.compute();
-        _out_PlaneITKMesh = abf.getMesh();
+        _out_Mesh = abf.getMesh();
 
         // Load pre-generated output from file
-        volcart::testing::ParsingHelpers::parseObjFile("abf_Plane_LSCMOnly.obj", _SavedPlanePoints, _SavedPlaneCells);
+        volcart::testing::ParsingHelpers::parseObjFile("abf_Plane_LSCMOnly.obj", _SavedPoints, _SavedCells);
 
     }
 
@@ -105,11 +105,11 @@ struct CreatePlaneABFLSCMOnlyUVFixture{
 
     //declare Plane mesh and width and height
     volcart::shapes::Plane _Plane;
-    VC_MeshType::Pointer _in_PlaneITKMesh;
-    VC_MeshType::Pointer _out_PlaneITKMesh;
+    VC_MeshType::Pointer _in_Mesh;
+    VC_MeshType::Pointer _out_Mesh;
 
-    std::vector<VC_Vertex> _SavedPlanePoints;
-    std::vector<VC_Cell>   _SavedPlaneCells;
+    std::vector<VC_Vertex> _SavedPoints;
+    std::vector<VC_Cell>   _SavedCells;
 };
 
 struct CreateArchABFUVFixture{
@@ -119,26 +119,26 @@ struct CreateArchABFUVFixture{
         std::cerr << "Creating Arch UV map using ABF..." << std::endl;
 
         //get ITK Mesh
-        _in_ArchITKMesh = _Arch.itkMesh();
+        _in_Mesh = _Arch.itkMesh();
 
         //Create uvMap from mesh
-        volcart::texturing::abf abf(_in_ArchITKMesh);
+        volcart::texturing::abf abf(_in_Mesh);
         abf.compute();
-        _out_ArchITKMesh = abf.getMesh();
+        _out_Mesh = abf.getMesh();
 
         // Load pre-generated output from file
-        volcart::testing::ParsingHelpers::parseObjFile("abf_Arch.obj", _SavedArchPoints, _SavedArchCells);
+        volcart::testing::ParsingHelpers::parseObjFile("abf_Arch.obj", _SavedPoints, _SavedCells);
     }
 
     ~CreateArchABFUVFixture(){std::cerr << "Destroying Arch ABF UV map..." << std::endl;}
 
     //declare Arch mesh
     volcart::shapes::Arch _Arch;
-    VC_MeshType::Pointer _in_ArchITKMesh;
-    VC_MeshType::Pointer _out_ArchITKMesh;
+    VC_MeshType::Pointer _in_Mesh;
+    VC_MeshType::Pointer _out_Mesh;
 
-    std::vector<VC_Vertex> _SavedArchPoints;
-    std::vector<VC_Cell>   _SavedArchCells;
+    std::vector<VC_Vertex> _SavedPoints;
+    std::vector<VC_Cell>   _SavedCells;
 };
 
 struct CreateArchABFLSCMOnlyUVFixture{
@@ -148,27 +148,27 @@ struct CreateArchABFLSCMOnlyUVFixture{
         std::cerr << "Creating Arch UV map using ABF (LSCM only)..." << std::endl;
 
         //get ITK Mesh
-        _in_ArchITKMesh = _Arch.itkMesh();
+        _in_Mesh = _Arch.itkMesh();
 
         //Create uvMap from mesh
-        volcart::texturing::abf abf(_in_ArchITKMesh);
+        volcart::texturing::abf abf(_in_Mesh);
         abf.setUseABF(false);
         abf.compute();
-        _out_ArchITKMesh = abf.getMesh();
+        _out_Mesh = abf.getMesh();
 
         // Load pre-generated output from file
-        volcart::testing::ParsingHelpers::parseObjFile("abf_Arch_LSCMOnly.obj", _SavedArchPoints, _SavedArchCells);
+        volcart::testing::ParsingHelpers::parseObjFile("abf_Arch_LSCMOnly.obj", _SavedPoints, _SavedCells);
     }
 
     ~CreateArchABFLSCMOnlyUVFixture(){std::cerr << "Destroying Arch ABF (LSCM only) UV map..." << std::endl;}
 
     //declare Arch mesh
     volcart::shapes::Arch _Arch;
-    VC_MeshType::Pointer _in_ArchITKMesh;
-    VC_MeshType::Pointer _out_ArchITKMesh;
+    VC_MeshType::Pointer _in_Mesh;
+    VC_MeshType::Pointer _out_Mesh;
 
-    std::vector<VC_Vertex> _SavedArchPoints;
-    std::vector<VC_Cell>   _SavedArchCells;
+    std::vector<VC_Vertex> _SavedPoints;
+    std::vector<VC_Cell>   _SavedCells;
 };
 
 /*
@@ -180,25 +180,25 @@ struct CreateArchABFLSCMOnlyUVFixture{
 BOOST_FIXTURE_TEST_CASE(PlaneABFUVTest, CreatePlaneABFUVFixture){
 
     //check size of uvMap and number of points in mesh
-    BOOST_CHECK_EQUAL(_out_PlaneITKMesh->GetNumberOfPoints(), _in_PlaneITKMesh->GetNumberOfPoints());
-    BOOST_CHECK_EQUAL(_out_PlaneITKMesh->GetNumberOfPoints(), _SavedPlanePoints.size());
+    BOOST_CHECK_EQUAL(_out_Mesh->GetNumberOfPoints(), _in_Mesh->GetNumberOfPoints());
+    BOOST_CHECK_EQUAL(_out_Mesh->GetNumberOfPoints(), _SavedPoints.size());
 
     //check uvmap against original mesh input pointIDs
     double value_test, value_truth;
-    for (size_t point = 0; point < _SavedPlanePoints.size(); ++point){
+    for (size_t point = 0; point < _SavedPoints.size(); ++point){
 
-        value_test  = _out_PlaneITKMesh->GetPoint(point)[0];
-        value_truth = _SavedPlanePoints[point].x;
+        value_test  = _out_Mesh->GetPoint(point)[0];
+        value_truth = _SavedPoints[point].x;
         if( (std::abs(value_test) > MINIMUM_VALUE) && (std::abs(value_truth) > MINIMUM_VALUE) )
             BOOST_CHECK_CLOSE_FRACTION( value_test, value_truth, TOLERANCE);
 
-        value_test  = _out_PlaneITKMesh->GetPoint(point)[1];
-        value_truth = _SavedPlanePoints[point].y;
+        value_test  = _out_Mesh->GetPoint(point)[1];
+        value_truth = _SavedPoints[point].y;
         if( (std::abs(value_test) > MINIMUM_VALUE) && (std::abs(value_truth) > MINIMUM_VALUE) )
             BOOST_CHECK_CLOSE_FRACTION( value_test, value_truth, TOLERANCE);
 
-        value_test  = _out_PlaneITKMesh->GetPoint(point)[2];
-        value_truth = _SavedPlanePoints[point].z;
+        value_test  = _out_Mesh->GetPoint(point)[2];
+        value_truth = _SavedPoints[point].z;
         if( (std::abs(value_test) > MINIMUM_VALUE) && (std::abs(value_truth) > MINIMUM_VALUE) )
             BOOST_CHECK_CLOSE_FRACTION( value_test, value_truth, TOLERANCE);
     }
@@ -208,25 +208,25 @@ BOOST_FIXTURE_TEST_CASE(PlaneABFUVTest, CreatePlaneABFUVFixture){
 BOOST_FIXTURE_TEST_CASE(PlaneABFLSCMOnlyUVTest, CreatePlaneABFLSCMOnlyUVFixture){
 
     //check size of uvMap and number of points in mesh
-    BOOST_CHECK_EQUAL(_out_PlaneITKMesh->GetNumberOfPoints(), _in_PlaneITKMesh->GetNumberOfPoints());
-    BOOST_CHECK_EQUAL(_out_PlaneITKMesh->GetNumberOfPoints(), _SavedPlanePoints.size());
+    BOOST_CHECK_EQUAL(_out_Mesh->GetNumberOfPoints(), _in_Mesh->GetNumberOfPoints());
+    BOOST_CHECK_EQUAL(_out_Mesh->GetNumberOfPoints(), _SavedPoints.size());
 
     //check uvmap against original mesh input pointIDs
     double value_test, value_truth;
-    for (size_t point = 0; point < _SavedPlanePoints.size(); ++point){
+    for (size_t point = 0; point < _SavedPoints.size(); ++point){
 
-        value_test  = _out_PlaneITKMesh->GetPoint(point)[0];
-        value_truth = _SavedPlanePoints[point].x;
+        value_test  = _out_Mesh->GetPoint(point)[0];
+        value_truth = _SavedPoints[point].x;
         if( (std::abs(value_test) > MINIMUM_VALUE) && (std::abs(value_truth) > MINIMUM_VALUE) )
             BOOST_CHECK_CLOSE_FRACTION( value_test, value_truth, TOLERANCE);
 
-        value_test  = _out_PlaneITKMesh->GetPoint(point)[1];
-        value_truth = _SavedPlanePoints[point].y;
+        value_test  = _out_Mesh->GetPoint(point)[1];
+        value_truth = _SavedPoints[point].y;
         if( (std::abs(value_test) > MINIMUM_VALUE) && (std::abs(value_truth) > MINIMUM_VALUE) )
             BOOST_CHECK_CLOSE_FRACTION( value_test, value_truth, TOLERANCE);
 
-        value_test  = _out_PlaneITKMesh->GetPoint(point)[2];
-        value_truth = _SavedPlanePoints[point].z;
+        value_test  = _out_Mesh->GetPoint(point)[2];
+        value_truth = _SavedPoints[point].z;
         if( (std::abs(value_test) > MINIMUM_VALUE) && (std::abs(value_truth) > MINIMUM_VALUE) )
             BOOST_CHECK_CLOSE_FRACTION( value_test, value_truth, TOLERANCE);
     }
@@ -236,25 +236,25 @@ BOOST_FIXTURE_TEST_CASE(PlaneABFLSCMOnlyUVTest, CreatePlaneABFLSCMOnlyUVFixture)
 BOOST_FIXTURE_TEST_CASE(ArchABFUVTest, CreateArchABFUVFixture){
 
     //check size of uvMap and number of points in mesh
-    BOOST_CHECK_EQUAL(_out_ArchITKMesh->GetNumberOfPoints(), _in_ArchITKMesh->GetNumberOfPoints());
-    BOOST_CHECK_EQUAL(_out_ArchITKMesh->GetNumberOfPoints(), _SavedArchPoints.size());
+    BOOST_CHECK_EQUAL(_out_Mesh->GetNumberOfPoints(), _in_Mesh->GetNumberOfPoints());
+    BOOST_CHECK_EQUAL(_out_Mesh->GetNumberOfPoints(), _SavedPoints.size());
 
     //check uvmap against original mesh input pointIDs
     double value_test, value_truth;
-    for (size_t point = 0; point < _SavedArchPoints.size(); ++point){
+    for (size_t point = 0; point < _SavedPoints.size(); ++point){
 
-        value_test  = _out_ArchITKMesh->GetPoint(point)[0];
-        value_truth = _SavedArchPoints[point].x;
+        value_test  = _out_Mesh->GetPoint(point)[0];
+        value_truth = _SavedPoints[point].x;
         if( (std::abs(value_test) > MINIMUM_VALUE) && (std::abs(value_truth) > MINIMUM_VALUE) )
             BOOST_CHECK_CLOSE_FRACTION( value_test, value_truth, TOLERANCE);
 
-        value_test  = _out_ArchITKMesh->GetPoint(point)[1];
-        value_truth = _SavedArchPoints[point].y;
+        value_test  = _out_Mesh->GetPoint(point)[1];
+        value_truth = _SavedPoints[point].y;
         if( (std::abs(value_test) > MINIMUM_VALUE) && (std::abs(value_truth) > MINIMUM_VALUE) )
             BOOST_CHECK_CLOSE_FRACTION( value_test, value_truth, TOLERANCE);
 
-        value_test  = _out_ArchITKMesh->GetPoint(point)[2];
-        value_truth = _SavedArchPoints[point].z;
+        value_test  = _out_Mesh->GetPoint(point)[2];
+        value_truth = _SavedPoints[point].z;
         if( (std::abs(value_test) > MINIMUM_VALUE) && (std::abs(value_truth) > MINIMUM_VALUE) )
             BOOST_CHECK_CLOSE_FRACTION( value_test, value_truth, TOLERANCE);
     }
@@ -264,25 +264,25 @@ BOOST_FIXTURE_TEST_CASE(ArchABFUVTest, CreateArchABFUVFixture){
 BOOST_FIXTURE_TEST_CASE(ArchABFLSCMOnlyUVTest, CreateArchABFLSCMOnlyUVFixture){
 
     //check size of uvMap and number of points in mesh
-    BOOST_CHECK_EQUAL(_out_ArchITKMesh->GetNumberOfPoints(), _in_ArchITKMesh->GetNumberOfPoints());
-    BOOST_CHECK_EQUAL(_out_ArchITKMesh->GetNumberOfPoints(), _SavedArchPoints.size());
+    BOOST_CHECK_EQUAL(_out_Mesh->GetNumberOfPoints(), _in_Mesh->GetNumberOfPoints());
+    BOOST_CHECK_EQUAL(_out_Mesh->GetNumberOfPoints(), _SavedPoints.size());
 
     //check uvmap against original mesh input pointIDs
     double value_test, value_truth;
-    for (size_t point = 0; point < _SavedArchPoints.size(); ++point){
+    for (size_t point = 0; point < _SavedPoints.size(); ++point){
 
-        value_test  = _out_ArchITKMesh->GetPoint(point)[0];
-        value_truth = _SavedArchPoints[point].x;
+        value_test  = _out_Mesh->GetPoint(point)[0];
+        value_truth = _SavedPoints[point].x;
         if( (std::abs(value_test) > MINIMUM_VALUE) && (std::abs(value_truth) > MINIMUM_VALUE) )
             BOOST_CHECK_CLOSE_FRACTION( value_test, value_truth, TOLERANCE);
 
-        value_test  = _out_ArchITKMesh->GetPoint(point)[1];
-        value_truth = _SavedArchPoints[point].y;
+        value_test  = _out_Mesh->GetPoint(point)[1];
+        value_truth = _SavedPoints[point].y;
         if( (std::abs(value_test) > MINIMUM_VALUE) && (std::abs(value_truth) > MINIMUM_VALUE) )
             BOOST_CHECK_CLOSE_FRACTION( value_test, value_truth, TOLERANCE);
 
-        value_test  = _out_ArchITKMesh->GetPoint(point)[2];
-        value_truth = _SavedArchPoints[point].z;
+        value_test  = _out_Mesh->GetPoint(point)[2];
+        value_truth = _SavedPoints[point].z;
         if( (std::abs(value_test) > MINIMUM_VALUE) && (std::abs(value_truth) > MINIMUM_VALUE) )
             BOOST_CHECK_CLOSE_FRACTION( value_test, value_truth, TOLERANCE);
     }
