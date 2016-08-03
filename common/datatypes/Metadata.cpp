5113da21b67319a691ea8e01785a91ad090f2891
Sean Karlage <skarlage@get9.io>
2016-08-03 00:56:58 -0700

    Change exception type to ifstream::failure (more appropriate than runtime_error)

--- common/datatypes/Metadata.cpp
+++ common/datatypes/Metadata.cpp
@@ -12,11 +12,13 @@ Metadata::Metadata(const fs::path& file_location) : _path(file_location)
     std::ifstream json_file(file_location.string());
     if (!json_file.is_open()) {
         std::cout << "Json File " << file_location << " not found" << std::endl;
+        throw std::ifstream::failure("could not find json file");
     }
 
     json_file >> _json;
     if (json_file.fail()) {
         std::cerr << "could not read json file " << file_location << std::endl;
+        throw std::ifstream::failure("could not read json file");
     }
 }
 
