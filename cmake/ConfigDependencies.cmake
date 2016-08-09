b3778215a323d3a92af44e81f21846c81a286b7d
Seth Parker <csparker247@gmail.com>
2016-08-09 16:59:04 -0400

    Move the dependency options into ConfigDependencies. Formatting cleanup.

--- cmake/ConfigDependencies.cmake
+++ cmake/ConfigDependencies.cmake
@@ -1,9 +1,15 @@
-# Where to look for deps
+### VC Prebuilt Libs ###
+# Built using vc-deps
+option(VC_PREBUILT_LIBS "Link against prebuilt dependencies" off)
 if (VC_PREBUILT_LIBS)
     set(CMAKE_PREFIX_PATH ${PROJECT_SOURCE_DIR}/vc-deps/deps)
 endif()
 
-# Boost
+########
+# Core #
+########
+
+### Boost ###
 set(VC_BOOST_COMPONENTS
     system
     filesystem
@@ -16,34 +22,42 @@ if (VC_PREBUILT_LIBS)
     set(Boost_USE_STATIC_LIBS on)
 endif()
 
-# Qt stuff
+### Qt5 ###
 find_package(Qt5 QUIET REQUIRED COMPONENTS Widgets Gui Core)
 
-# ITK
+### ITK ###
 find_package(ITK 4.10 QUIET REQUIRED)
 include(${ITK_USE_FILE})
 
-# VTK
+### VTK ###
 find_package(VTK QUIET REQUIRED)
 include(${VTK_USE_FILE})
-set(VTK_LIBRARIES_TMP ${VTK_LIBRARIES})
+set(VTK_LIBRARIES_TMP ${VTK_LIBRARIES}) # Save these for later
 
-# Eigen (make it into a target)
+### Eigen ###
+# Make it into a target
 find_package(Eigen3 QUIET REQUIRED)
 add_library(eigen3 INTERFACE IMPORTED)
 set_target_properties(eigen3 PROPERTIES
     INTERFACE_INCLUDE_DIRECTORIES "${EIGEN3_INCLUDE_DIR}"
 )
 
-# Rest
+### PCL ###
 set(PCL_STATIC on)
 find_package(PCL 1.7 REQUIRED QUIET)
 
-# XXX Put VTK libraries back because PCL silently overwrites them
+# Put VTK libraries back because PCL silently overwrites them
 set(VTK_LIBRARIES ${VTK_LIBRARIES_TMP})
 
+### OpenCV ###
 find_package(OpenCV REQUIRED)
 
+############
+# Optional #
+############
+
+### ACVD ###
+option(VC_USE_ACVD "Use ACVD library" off)
 if (VC_USE_ACVD)
     find_package(ACVD QUIET REQUIRED)
     add_library(acvd INTERFACE IMPORTED)
@@ -54,6 +68,9 @@ if (VC_USE_ACVD)
         INTERFACE_LINK_LIBRARIES "${ACVD_LIBRARIES};${VTK_LIBRARIES}"
     )
 endif()
+
+### Bullet Physics ###
+option(VC_USE_BULLET "Use Bullet Physics library" off)
 if (VC_USE_BULLET)
     find_package(Bullet QUIET REQUIRED)
     add_library(bullet INTERFACE IMPORTED)
@@ -62,6 +79,9 @@ if (VC_USE_BULLET)
         INTERFACE_LINK_LIBRARIES "${BULLET_LIBRARIES}"
     )
 endif()
+
+### libigl ###
+option(VC_USE_LIBIGL "Use libigl" off)
 if (VC_USE_LIBIGL)
     find_package(LIBIGL QUIET REQUIRED)
     add_library(libigl INTERFACE IMPORTED)
