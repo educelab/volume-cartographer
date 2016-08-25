6f9e066c453b3834a994ac68407bbe8f56bfcd47
Sean Karlage <skarlage@get9.io>
2016-08-24 22:54:57 -0700

    Testing badly-formatted file

---
+++ examples/src/BadFormat.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int argc, char** argv) {
+    int a = 0;
+        std::cout << a
+            << std::endl;
+    std::cout << "this is bad format!!!" << std::endl;
+                    return 0;
+}
