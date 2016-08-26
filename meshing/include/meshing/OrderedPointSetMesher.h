8fd1728742e3ca0ae6031aef695d24f6c635895f
Seth Parker <csparker247@gmail.com>
2016-08-26 18:22:57 -0400

    Try this clang-format thing out.

--- meshing/include/meshing/OrderedPointSetMesher.h
+++ meshing/include/meshing/OrderedPointSetMesher.h
@@ -4,33 +4,36 @@
 
 #pragma once
 
-#include "common/vc_defines.h"
-#include "common/types/PointSet.h"
 #include "common/types/Point.h"
+#include "common/types/PointSet.h"
+#include "common/vc_defines.h"
 
-namespace volcart{
-    namespace meshing{
+namespace volcart
+{
+namespace meshing
+{
 
-        class OrderedPointSetMesher {
-        public:
-            //Initalizers
-            OrderedPointSetMesher();
-            OrderedPointSetMesher(PointSet<Point3d> points);
+class OrderedPointSetMesher
+{
+public:
+    // Initalizers
+    OrderedPointSetMesher();
+    OrderedPointSetMesher(PointSet<Point3d> points);
 
-            // I/O
-            void setPointSet(PointSet<Point3d> points);
-            VC_MeshType::Pointer getOutputMesh();
+    // I/O
+    void setPointSet(PointSet<Point3d> points);
+    VC_MeshType::Pointer getOutputMesh();
 
-            // Processing
-            void compute();
+    // Processing
+    void compute();
 
-        private:
-            PointSet<Point3d> input_;
-            VC_MeshType::Pointer output_;
+private:
+    PointSet<Point3d> input_;
+    VC_MeshType::Pointer output_;
 
-            //Used to add a cell to the itk mesh
-            void addCell_(unsigned long a, unsigned long b, unsigned long c);
-        };
+    // Used to add a cell to the itk mesh
+    void addCell_(size_t a, size_t b, size_t c);
+};
 
-    }
-}
+}  // meshing
+}  // volcart
--- meshing/src/OrderedPointSetMesher.cpp
+++ meshing/src/OrderedPointSetMesher.cpp
@@ -8,24 +8,24 @@
 
 using namespace volcart::meshing;
 
-OrderedPointSetMesher::OrderedPointSetMesher(PointSet<Point3d> points){
+OrderedPointSetMesher::OrderedPointSetMesher(PointSet<Point3d> points)
+{
     input_ = points;
 }
 
-VC_MeshType::Pointer OrderedPointSetMesher::getOutputMesh(){
-    return output_;
-}
+VC_MeshType::Pointer OrderedPointSetMesher::getOutputMesh() { return output_; }
 
-void OrderedPointSetMesher::setPointSet(PointSet<Point3d> points){
+void OrderedPointSetMesher::setPointSet(PointSet<Point3d> points)
+{
     input_ = points;
 }
 
 void OrderedPointSetMesher::compute()
 {
     // Verify before computation
-    if(input_.empty())
+    if (input_.empty())
         throw std::invalid_argument("Attempted to mesh empty point set.");
-    if(!input_.isOrdered())
+    if (!input_.isOrdered())
         throw std::invalid_argument("Attempted to mesh unordered point set.");
 
     // Create a clean output mesh
@@ -33,9 +33,8 @@ void OrderedPointSetMesher::compute()
 
     // Transfer the vertex info
     VC_PointType temp_pt;
-    unsigned long cnt = 0;
-    for (auto & i : input_ )
-    {
+    size_t cnt = 0;
+    for (auto& i : input_) {
         temp_pt[0] = i[0];
         temp_pt[1] = i[1];
         temp_pt[2] = i[2];
@@ -45,27 +44,29 @@ void OrderedPointSetMesher::compute()
     }
 
     // Creates 2 cells per iteration and adds them to the mesh
-    unsigned long p0, p1, p2, p3;
-    for(auto i = 0; i < input_.height() - 1; i++){
-        for(auto j = 0; j < input_.width() - 1; j++){
+    size_t p0, p1, p2, p3;
+    for (auto i = 0; i < input_.height() - 1; i++) {
+        for (auto j = 0; j < input_.width() - 1; j++) {
 
             // Get the indices for the faces adjacent along the "hypotenuse"
-            p0 = i *  input_.width() + j;
+            p0 = i * input_.width() + j;
             p1 = p0 + 1;
             p2 = p1 + input_.width();
             p3 = p2 - 1;
 
-            if( p0 >= output_->GetNumberOfPoints() ||
+            if (p0 >= output_->GetNumberOfPoints() ||
                 p1 >= output_->GetNumberOfPoints() ||
                 p2 >= output_->GetNumberOfPoints() ||
-                p3 >= output_->GetNumberOfPoints() ) {
-                throw std::out_of_range("Predicted vertex index for face generation out of range of point set.");
+                p3 >= output_->GetNumberOfPoints()) {
+                throw std::out_of_range(
+                    "Predicted vertex index for face generation out of range "
+                    "of point set.");
             }
 
             addCell_(p1, p2, p3);
             addCell_(p0, p1, p3);
-        }// j loop
-    }// i loop
+        }  // j loop
+    }      // i loop
 
     // Sets the normals for the points and faces
     volcart::meshing::CalculateNormals calcNorm(output_);
@@ -73,7 +74,7 @@ void OrderedPointSetMesher::compute()
     output_ = calcNorm.getMesh();
 }
 
-void OrderedPointSetMesher::addCell_( unsigned long a, unsigned long b, unsigned long c )
+void OrderedPointSetMesher::addCell_(size_t a, size_t b, size_t c)
 {
     VC_CellType::CellAutoPointer current_C;
 
@@ -83,5 +84,5 @@ void OrderedPointSetMesher::addCell_( unsigned long a, unsigned long b, unsigned
     current_C->SetPointId(1, b);
     current_C->SetPointId(2, c);
 
-    output_->SetCell( output_->GetNumberOfCells(), current_C );
+    output_->SetCell(output_->GetNumberOfCells(), current_C);
 }
