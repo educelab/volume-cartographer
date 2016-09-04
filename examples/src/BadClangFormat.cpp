c583e14107fab5e7f5f23253705916267f6f80f2
Sean Karlage <sean.karlage@uky.edu>
2016-09-03 21:43:35 -0700

    Adding bad clang-format file without hooks

--- examples/src/BadClangFormat.cpp
+++ examples/src/BadClangFormat.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
-int main(int argc, char** argv)
+int main(          int argc, char** argv)
 {
-    std::cout << "hello, world!" << std::endl;
-    return 0;
+    std::cout <<          "hello, world!" << std::endl;
+    return             0;
 }
