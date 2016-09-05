86e4e2fbe21815c90d96ad5da0c14de1b677f02c
Sean Karlage <skarlage@get9.io>
2016-09-05 14:42:28 -0700

    Commit bad format file

--- examples/src/BadClangFormat.cpp
+++ examples/src/BadClangFormat.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
-int main(int argc, char** argv)
+int main(        int argc, char** argv)
 {
-    std::cout << "hello, world!" << std::endl;
+     std::cout << "hello, world!" << std::endl;
     return 0;
 }
