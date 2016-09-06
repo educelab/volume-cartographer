b65f6bf20ae070ef33e96b46a9839bab064fd1e4
Sean Karlage <skarlage@get9.io>
2016-09-06 00:19:31 -0700

    Fix call to lint_file

--- scripts/clang-format.py
+++ scripts/clang-format.py
@@ -201,5 +201,5 @@ if __name__ == '__main__':
     files = list(filter(lambda f: re.search(ext_re, f), files))
 
     # Validate each with clang-format
-    clean = all(lint_file(f) for f in files)
+    clean = all(lint_file(cf, f) for f in files)
     sys.exit(0) if clean else sys.exit(1)
