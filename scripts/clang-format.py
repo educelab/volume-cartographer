fc697ca498973dc9bc4530ffd5112a649ffe69b6
Sean Karlage <skarlage@get9.io>
2016-09-06 00:17:45 -0700

    Better version of clang-format that fetches cf from llvm

--- scripts/clang-format.py
+++ scripts/clang-format.py
@@ -3,25 +3,87 @@
 import argparse
 import difflib
 import logging
+import os
+import platform
 import re
+import shutil
+import string
 import subprocess
 import sys
+import tarfile
+import tempfile
+import urllib.request
 
+# The version we care about
 CLANG_FORMAT_VERSION = '3.9.0'
 CLANG_FORMAT_VERSION_SHORT = '3.9'
 
+# Name of clang-format as a binary
+CLANG_FORMAT_PROGNAME = 'clang-format'
+
+# URL location of the 'cached' copy of clang-format to download
+# for users which do not have clang-format installed
+CLANG_FORMAT_HTTP_LINUX_CACHE = 'http://llvm.org/releases/3.9.0/clang+llvm-3.9.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz'
+
+CLANG_FORMAT_HTTP_DARWIN_CACHE = 'http://llvm.org/releases/3.9.0/clang+llvm-3.9.0-x86_64-apple-darwin.tar.xz'
+
+# Path in the tarball to the clang-format binary
+CLANG_FORMAT_SOURCE_TAR_BASE = string.Template(
+    'clang+llvm-$version-$tar_path/bin/' + CLANG_FORMAT_PROGNAME
+)
+
+# Path to extract clang-format to in source tree
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+CLANG_FORMAT_EXTRACT_DIR = os.path.join(BASE_DIR, 'bin')
+
+
+def fetch_clang_format(url):
+    dest = tempfile.gettempdir()
+    tmp_tar = os.path.join(dest, 'temp.tar.xz')
+    logging.info(
+        'Downloading clang-format {} from {}, saving to {}'.
+        format(CLANG_FORMAT_VERSION, url, tmp_tar)
+    )
+    urllib.request.urlretrieve(url, tmp_tar)
+    return tmp_tar
+
+
+def extract_clang_format(tarpath, final_cf_path):
+    # Make the cf dir if it doesn't already exist
+    if not os.path.isdir(CLANG_FORMAT_EXTRACT_DIR):
+        os.mkdir(CLANG_FORMAT_EXTRACT_DIR)
+
+    # Extract from tarfile to the final dir
+    with tarfile.open(tarpath, 'r:xz') as t:
+        logging.info(
+            'Extracting {} from {}'.
+            format(CLANG_FORMAT_PROGNAME, tarpath)
+        )
+        path_in_tar = list(
+            filter(lambda n: n.endswith('/clang-format'), t.getnames())
+        )[0]
+        fullpath = os.path.join(CLANG_FORMAT_EXTRACT_DIR, CLANG_FORMAT_PROGNAME)
+        extract_to = os.path.join(tempfile.gettempdir())
+        t.extract(path_in_tar, path=extract_to)
+
+        # Move to final location
+        shutil.move(os.path.join(tempfile.gettempdir(), path_in_tar), fullpath)
+        logging.info('Extracted {} to {}'.format(path_in_tar, fullpath))
+
+        return fullpath
+
 
 def callo(cmd):
-    if isinstance(cmd, list):
-        cmd = ' '.join(cmd)
-    return subprocess.getoutput(cmd).strip()
+    if isinstance(cmd, str):
+        cmd = cmd.split()
+    return subprocess.check_output(cmd).decode('utf-8').strip()
 
 
 # Only return rc, not output
 def call(cmd):
-    if isinstance(cmd, str):
-        cmd = cmd.split()
-    return subprocess.call(cmd)
+    if isinstance(cmd, list):
+        cmd = ' '.join(cmd)
+    return subprocess.getstatusoutput(cmd)
 
 
 # Get all changed files (even non-source ones)
@@ -39,19 +101,34 @@ def find_clang_format(argpath):
     if argpath:
         return argpath
 
+    # Check for the previously-extracted clang-format and make sure it's
+    # executable
+    extracted = os.path.join(CLANG_FORMAT_EXTRACT_DIR, CLANG_FORMAT_PROGNAME)
+    if os.path.isfile(extracted) and callo(' '.join([extracted, '-h'])):
+        return extracted
+
     # Check system $PATH
     try:
-        path = callo('which clang-format')
+        return callo('which clang-format')
     except subprocess.CalledProcessError:
         msg = '''
-            Could not find suitable clang-format in path. Rerun script with
-            '-c/--clang-format-path' pointing to a valid clang-format
+            Could not find suitable clang-format in path. Attempting to download
+            from LLVM release page
         '''
-        logger.error(msg)
-        sys.exit(1)
+        logging.info(msg)
 
-    # Validate clang-format version
-    return path
+        # Fetch clang-format from LLVM servers
+        tarpath = ''
+        if platform.system() == 'Linux':
+            tarpath = fetch_clang_format(CLANG_FORMAT_HTTP_LINUX_CACHE)
+        elif platform.system() == 'Darwin':
+            tarpath = fetch_clang_format(CLANG_FORMAT_HTTP_DARWIN_CACHE)
+        else:
+            logging.error('Platorm {} not supported'.format(platform.system()))
+            sys.exit(1)
+
+        # Extract to correct directory
+        return extract_clang_format(tarpath, CLANG_FORMAT_EXTRACT_DIR)
 
 
 def cf_version(path):
@@ -83,7 +160,8 @@ def lint_file(cf_path, file_path):
         print('Suggested changes:\n')
         for line in result:
             print(line.strip())
-        sys.exit(1)
+
+    return formatted_text == original_text
 
 
 if __name__ == '__main__':
@@ -91,7 +169,7 @@ if __name__ == '__main__':
     logging.basicConfig(
         stream=sys.stdout,
         level=logging.DEBUG,
-        format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+        format='%(asctime)s %(name)s %(levelname)s %(message)s',
         datefmt='%m-%d %H:%M'
     )
 
@@ -123,5 +201,5 @@ if __name__ == '__main__':
     files = list(filter(lambda f: re.search(ext_re, f), files))
 
     # Validate each with clang-format
-    for f in files:
-        lint_file(cf, f)
+    clean = all(lint_file(f) for f in files)
+    sys.exit(0) if clean else sys.exit(1)
