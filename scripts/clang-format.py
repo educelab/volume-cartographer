4604c6f5ef43c760c199714dfe514eba039e9685
Sean Karlage <skarlage@get9.io>
2016-09-05 17:22:23 -0700

    Make new server-side clang-format validation script

---
+++ scripts/clang-format.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+
+import argparse
+import logging
+import subprocess
+import sys
+
+CLANG_FORMAT_VERSION = '3.9.0'
+CLANG_FORMAT_VERSION_SHORT = '3.9'
+
+
+def callo(cmd):
+    if isinstance(cmd, list):
+        cmd = ' '.join(cmd)
+    return subprocess.getoutput(cmd).strip()
+
+
+# Only return rc, not output
+def call(cmd):
+    if isinstance(cmd, str):
+        cmd = cmd.split()
+    return subprocess.call(cmd)
+
+
+# Get all changed files (even non-source ones)
+def changed_files():
+    files = callo('git diff --name-only @~..@').splitlines()
+    if len(files) == 0:
+        logging.info('No changed files')
+        sys.exit(0)
+
+    return files
+
+
+# Find clang-format either in passed-in path or system path
+def find_clang_format(argpath):
+    if argpath:
+        return argpath
+
+    # Check system $PATH
+    try:
+        path = callo('which clang-format')
+    except subprocess.CalledProcessError:
+        msg = '''
+            Could not find suitable clang-format in path. Rerun script with
+            '-c/--clang-format-path' pointing to a valid clang-format
+        '''
+        logger.error(msg)
+        sys.exit(1)
+
+    # Validate clang-format version
+    return path
+
+
+def cf_version(path):
+    return callo(' '.join([path, '--version']))
+
+
+def cf_version_is_correct(path):
+    return CLANG_FORMAT_VERSION in cf_version(path)
+
+
+if __name__ == '__main__':
+    # Set up some logging
+    logging.basicConfig(
+        stream=sys.stdout,
+        level=logging.DEBUG,
+        format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+        datefmt='%m-%d %H:%M'
+    )
+
+    parser = argparse.ArgumentParser('clang-format')
+    parser.add_argument(
+        '-c',
+        '--clang-format-path',
+        help='path to clang-format',
+        metavar='PATH',
+        dest='path',
+        type=str
+    )
+    args = parser.parse_args()
+
+    # Find clang-format, validate version
+    cf = find_clang_format(args.path)
+    if not cf_version_is_correct(cf):
+        logging.error(
+            'Incorrect version of clang-format: got {} but {} is required'.
+            format(cf_version(cf), CLANG_FORMAT_VERSION)
+        )
+        sys.exit(1)
+
+    # Get list of files to check
+    files = changed_files()
+    print(files)
+
+    # Validate each with clang-format
+    for f in files:
+        pass
