21b1a2d727b3d9c6c589b8ca0dffa7d46bbc9e86
Hannah Hatch <hjha225@g.uky.edu>
2016-09-20 10:30:56 -0400

    Update invertCloud to take PCL and VCPS, change UVtest to reflect changes in Shapes

--- texturing/test/simpleUVTest.cpp
+++ texturing/test/simpleUVTest.cpp
@@ -53,9 +53,9 @@ struct CreatePlaneSimpleUVFixture{
 
         //create ordered point cloud from shape and assign width and height
         //params: false noise and true ordered
-        _in_PlanePointCloud =_Plane.pointCloudXYZ(false);
-        _width = _in_PlanePointCloud.width;
-        _height = _in_PlanePointCloud.height;
+        _in_PlanePointCloud =_Plane.orderedPoints(false);
+        _width = _in_PlanePointCloud.width();
+        _height = _in_PlanePointCloud.height();
 
         _in_PlaneITKMesh = _Plane.itkMesh();
 
@@ -69,7 +69,7 @@ struct CreatePlaneSimpleUVFixture{
     //declare Plane mesh and width and height
     volcart::shapes::Plane _Plane;
     VC_MeshType::Pointer _in_PlaneITKMesh;
-    pcl::PointCloud <pcl::PointXYZ> _in_PlanePointCloud;
+    volcart::OrderedPointSet<volcart::Point3d> _in_PlanePointCloud;
     int _width, _height;
 
     //declare uvMap to hold output from simpleUV call
@@ -84,9 +84,9 @@ struct CreateArchSimpleUVFixture{
 
         //create ordered point cloud from shape and assign width and height
         //params: false noise and true ordered
-        _in_ArchPointCloud =_Arch.pointCloudXYZ(false);
-        _width = _in_ArchPointCloud.width;
-        _height = _in_ArchPointCloud.height;
+        _in_ArchPointCloud =_Arch.orderedPoints(false);
+        _width = _in_ArchPointCloud.width();
+        _height = _in_ArchPointCloud.height();
         
         _in_ArchITKMesh = _Arch.itkMesh();
 
@@ -101,7 +101,7 @@ struct CreateArchSimpleUVFixture{
     VC_MeshType::Pointer _in_ArchITKMesh;
     volcart::UVMap _out_ArchUVMap;
 
-    pcl::PointCloud <pcl::PointXYZ> _in_ArchPointCloud;
+    volcart::OrderedPointSet<volcart::Point3d> _in_ArchPointCloud;
     int _width, _height; 
     
 };
