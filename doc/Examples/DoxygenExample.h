14cc7c03a9689c5c2f15e47ae877966f0b85692b
Hannah Hatch <hjha225@g.uky.edu>
2016-10-17 13:54:50 -0400

    Clang-format changes that didn't make it into the last commit

--- doc/Examples/DoxygenExample.h
+++ doc/Examples/DoxygenExample.h
@@ -8,21 +8,26 @@
  * @class  TestClass
  * @brief A class used as a template for Doxygen
  *
- * First use class command to specify class name, the use brief for a brief description
+ * First use class command to specify class name, the use brief for a brief
+ * description
  *
  * This is where you will write a longer description of your class
  *
  * @ingroup Example
- * Use this command to specifiy which library your code belongs to, for example Meshing or Texturing
+ * Use this command to specifiy which library your code belongs to, for example
+ * Meshing or Texturing
  *
- * Then use see command to specify any other libraries referenced in your code or any other references that can
+ * Then use see command to specify any other libraries referenced in your code
+ * or any other references that can
  * help the user understand how this works
  *
  * @see examples/LoremIpsumError.cpp
  *
- * @warning There is no way to create a member of this class without having at least one parameter
+ * @warning There is no way to create a member of this class without having at
+ * least one parameter
  */
-class TestClass{
+class TestClass
+{
 public:
     /**
      * @brief a brief description of the function
@@ -38,7 +43,8 @@ public:
     /**
      * @brief Initializes an empty TestClass object and sets both parameters
      *
-     * This function takes in two integers and then saves them to that particular objects
+     * This function takes in two integers and then saves them to that
+     * particular objects
      * so that they can be used later
      *
      * @param parameter1 The integer that represents the first parameter
@@ -48,23 +54,24 @@ public:
     TestClass(int parameter1, int parameter2);
 
     /** @name Group1 */
-     //@{
+    //@{
     /**
      * @brief Gets the value for Parameter1
      * @return The first parameter
      */
-    int getParam1() const {return private_param1;}
+    int getParam1() const { return private_param1; }
     /**
      * @brief Gets the value for Parameter2
      * @return The second Parameter
      */
-    int getParam2() const {return private_param2; }
+    int getParam2() const { return private_param2; }
     //@}
 
     /**
      * @brief Adds the two parameters and returns the value
      *
-     * Takes the most recent values for parameter1 and parameter2 and then adds them together
+     * Takes the most recent values for parameter1 and parameter2 and then adds
+     * them together
      * and returns the value
      * @return the added value of the parameters
      */
@@ -74,7 +81,7 @@ public:
      * @brief Compares the parameters  and returns the result
      * @return 'TRUE' if parameter1 is bigger than parameter2
      */
-    bool OneBiggerThanTwo() const {return private_param1 > private_param2; };
+    bool OneBiggerThanTwo() const { return private_param1 > private_param2; };
 
 private:
     /** The first parameter */
@@ -82,7 +89,4 @@ private:
 
     /** The second parameter */
     int private_param2;
-
 };
-
-
