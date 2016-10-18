4161eb247cc6c962bc8b990e748da2149d8fe18c
Hannah Hatch <hjha225@g.uky.edu>
2016-10-18 11:09:36 -0400

    Added GenericHeaderExample to be an example of a standalone header

---
+++ doc/Examples/GenericHeaderExample.h
@@ -0,0 +1,29 @@
+/**
+ * @file GenericHeaderExample.h
+ * @author Hannah Hatch
+ * @date 10/18/16
+ *
+ * @brief Generic non-class oriented header
+ *
+ * This is the long description to explain what this
+ * header does. It can be multiple lines.
+ *
+ * @newline
+ *
+ * You can also use the newline command to create a new line to separate
+ * large blocks of text.
+ *
+ * @ingroup ExampleDocs
+ */
+
+#pragma once
+
+#include <string>
+/**
+ * A description of what this value is
+ * and what it means.
+ */
+string test = "Test String";
+
+/** Specific brief description of this value*/
+string red = "Red";
