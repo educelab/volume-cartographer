cb8e7b604bd124102f6635139659306d1d2a79b0
Hannah Hatch <hjha225@g.uky.edu>
2016-10-25 10:37:22 -0400

    Initial write up of rayTrace and scaleMesh

--- meshing/include/meshing/rayTrace.h
+++ meshing/include/meshing/rayTrace.h
@@ -3,6 +3,12 @@
 //
 #pragma once
 
+/**
+ * @class rayTrace
+ * @author Media Team
+ * @date 8/12/15
+ *
+ */
 #include <vector>
 #include <opencv2/opencv.hpp>
 #include "core/vc_defines.h"
@@ -11,8 +17,19 @@ namespace volcart
 {
 namespace meshing
 {
-// Using vtk's OBBTree to test a ray's intersection with the
-// faces/cells/triangles in the mesh
+/**
+ * @brief Creates a UV Map of the mesh for texturing
+ *
+ * This class uses vtk's OBBTree to test a ray's intersection with the
+ * faces/cells/triangles in the mesh.
+ *
+ * @param itkMesh Mesh that you want to trace
+ * @param aTraceDir Direction you want to move (1 for clockwise)
+ * @param width Width of the ray
+ * @param height Height of the ray
+ * @param uvMap Map of the UV coordinates found by the ray
+ * @return vector of CV points that tell where the intersections are
+ */
 std::vector<cv::Vec6f> rayTrace(
     ITKMesh::Pointer itkMesh,
     int aTraceDir,
--- meshing/include/meshing/scaleMesh.h
+++ meshing/include/meshing/scaleMesh.h
@@ -2,7 +2,11 @@
 // Created by Seth Parker on 10/22/15.
 //
 #pragma once
-
+/**
+ * @class scaleMesh
+ * @author Seth Parker
+ * @date 10/22/15
+ */
 #include "core/vc_defines.h"
 
 #include <itkScaleTransform.h>
@@ -12,6 +16,15 @@ namespace volcart
 {
 namespace meshing
 {
+/**
+ * @brief Scales the mesh down by a specifed factor
+ *
+ * This function scales the mesh uniformly by applying
+ * an ITK filter.
+ * @param input Pointer to an ITKMesh that needs to be scaled
+ * @param output Pointer to an ITKMesh that has been scaled
+ * @param scale_factor Factor by which the mesh should be scaled up or down
+ */
 void scaleMesh(
     ITKMesh::Pointer input, ITKMesh::Pointer output, double scale_factor);
 }  // meshing
