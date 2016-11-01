c4f2478b196d9ca8103e825904b3a70aecea24e8
Hannah Hatch <hjha225@g.uky.edu>
2016-11-01 12:30:51 -0400

    Removal of Clang-format test comment

--- common/src/PlyReader2.cpp
+++ common/src/PlyReader2.cpp
@@ -68,8 +68,6 @@ bool PLYReader2(boost::filesystem::path path, volcart::ITKMesh::Pointer mesh)
             }
         }
 
-        // Added comment for testing
-
         linecnt++;
         getline(plyFile, line);
     }
