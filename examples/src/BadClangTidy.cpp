6b65f2644b7516bcb9a576e16e278739e0f777d9
Sean Karlage <skarlage@get9.io>
2016-11-23 22:31:32 -0800

    Testing clang-tidy script

---
+++ examples/src/BadClangTidy.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    std::cout << "hello, world!" << std::endl;
+    bool a = true;
+    if (a)
+        std::cout << "a is true" << std::endl;
+    return 0;
+}
