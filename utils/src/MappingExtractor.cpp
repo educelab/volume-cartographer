671a950a237dab24c537f82c2d4ba9e41c8da3d4
Hannah Hatch <hjha225@g.uky.edu>
2017-01-23 15:48:14 -0500

    Replaces file extensions that use ReadPPM

--- utils/src/MappingExtractor.cpp
+++ utils/src/MappingExtractor.cpp
@@ -38,7 +38,7 @@ int main(int /*argc*/, char** argv)
     std::cout << "mask: " << perPixelMask.cols << ", " << perPixelMask.rows
               << std::endl;
 
-    std::string ppmPath = segPath.string() + "/PerPixelMapping.yml.gz";
+    std::string ppmPath = segPath.string() + "/PerPixelMapping.ppm";
     perPixelMap = PerPixelMap::ReadPPM(ppmPath);
 
     // ROI params
