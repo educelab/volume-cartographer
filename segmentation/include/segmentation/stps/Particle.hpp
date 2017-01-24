a887f4f6e2710aabe6748de748b2f3558104cf0b
Seth Parker <csparker247@gmail.com>
2017-01-23 22:16:35 -0500

    Remove unnecessary typedef that snuck back in.

--- segmentation/include/segmentation/stps/Particle.hpp
+++ segmentation/include/segmentation/stps/Particle.hpp
@@ -3,7 +3,6 @@
 
 #include <opencv2/core.hpp>
 
-typedef bool ParticleStopped;
 /**
  * @class Particle
  * @brief Defines how a particle moves
@@ -17,12 +16,16 @@ public:
         : position_{position}, isStopped_{false}
     {
     }
+
     /** @brief Returns particle position in 3D space */
     cv::Vec3d position() const { return position_; }
+
     /** @brief Returns True if particle is stopped*/
     bool isStopped() const { return isStopped_; }
+
     /** @brief Sets the particle as stopped */
     void stop() { isStopped_ = true; }
+
     /** @brief Overwrites operator to add to the position
     *
     * Overwrites the += operator so that it adds every
@@ -30,8 +33,10 @@ public:
     * vector of the particle.
     */
     void operator+=(const cv::Vec3d& v) { position_ += v; }
+
     /** @brief Sets the () operator to return the component stored at int */
     double operator()(int index) const { return position_(index); }
+
     /** @brief Subracts the position of the Particle from the position of the
      * current particle */
     cv::Vec3d operator-(const Particle& p) { return position_ - p.position(); }
