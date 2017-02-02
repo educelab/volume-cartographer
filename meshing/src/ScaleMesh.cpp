3edd2cfb1e97f6c7e4bb9655b88abc1c6fc4011e
Hannah Hatch <hjha225@g.uky.edu>
2017-02-02 15:59:24 -0500

    Now with comments!

--- meshing/src/ScaleMesh.cpp
+++ meshing/src/ScaleMesh.cpp
@@ -17,11 +17,14 @@ void ScaleMesh(
     const ITKMesh::Pointer& output,
     double scaleFactor)
 {
+    // Copies the input mesh onto the output mesh
     DeepCopy(input,output);
+    
+    // Multiplies each component of each vertex by the scale factor
     for (auto v = output->GetPoints()->Begin(); v != output->GetPoints()->End(); ++v) {
         v->Value()[0] *= scaleFactor;
         v->Value()[1] *= scaleFactor;
-        v->Value()[2] *= scaleFactor; 
+        v->Value()[2] *= scaleFactor;
     }
 };
 }
