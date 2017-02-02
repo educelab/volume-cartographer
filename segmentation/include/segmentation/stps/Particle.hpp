41b2f115d21efe5ab2f99ba164802a00e4c0a188
Seth Parker <csparker247@gmail.com>
2017-02-02 16:46:19 -0500

    Simplify and document STPS Particle class. I shouldn’t have done this in this branch, but I couldn’t leave it looking so dumb.

--- segmentation/include/segmentation/stps/Particle.hpp
+++ segmentation/include/segmentation/stps/Particle.hpp
@@ -1,49 +1,27 @@
-// What am I?
 #pragma once
 
 #include <opencv2/core.hpp>
 
 /**
  * @class Particle
- * @brief Defines how a particle moves
+ * @brief Extends cv::Vec3d with some other things for STPS
+ *
+ * @warning Why would you use this class? Don't use this class.
+ *
  * @ingroup stps
  */
-class Particle
+class Particle : public cv::Vec3d
 {
 public:
-    /**@brief Initializes a particle to have a specific position */
-    explicit Particle(cv::Vec3d position)
-        : position_{position}, isStopped_{false}
-    {
-    }
+    /**@brief Constructor with Stop status initialization */
+    explicit Particle(cv::Vec3d p) : cv::Vec3d(p), isStopped_(false) {}
 
-    /** @brief Returns particle position in 3D space */
-    cv::Vec3d position() const { return position_; }
-
-    /** @brief Returns True if particle is stopped*/
+    /** @brief Return Stop status of particle */
     bool isStopped() const { return isStopped_; }
 
-    /** @brief Sets the particle as stopped */
+    /** @brief Set Stop status on particle */
     void stop() { isStopped_ = true; }
 
-    /** @brief Overwrites operator to add to the position
-    *
-    * Overwrites the += operator so that it adds every
-    * component of the parameter vec to the position
-    * vector of the particle.
-    */
-    void operator+=(const cv::Vec3d& v) { position_ += v; }
-
-    /** @brief Sets the () operator to return the component stored at int */
-    double operator()(int index) const { return position_(index); }
-
-    /** @brief Subracts the position of the Particle from the position of the
-     * current particle */
-    cv::Vec3d operator-(const Particle& p) { return position_ - p.position(); }
-
 private:
-    /** Current position of the particle*/
-    cv::Vec3d position_;
-    /** Determines if the particle is still moving*/
     bool isStopped_;
 };
