c8b51bc4e4ad3460dba2543d6d38ac2185667ebc
Hannah Hatch <hjha225@g.uky.edu>
2017-02-02 15:39:40 -0500

    Attempt #1

--- meshing/src/ScaleMesh.cpp
+++ meshing/src/ScaleMesh.cpp
@@ -16,29 +16,31 @@ void ScaleMesh(
     const ITKMesh::Pointer& output,
     double scaleFactor)
 {
-    // Scale uniformly
-    using VC3DScaleType = itk::ScaleTransform<double, 3>;
-    auto scaleTransform = VC3DScaleType::New();
-    itk::FixedArray<double, 3> scale;
-    scale[0] = scaleFactor;
-    scale[1] = scale[0];  // uniform scaling
-    scale[2] = scale[0];
-    scaleTransform->SetScale(scale);
-
-    // To-Do: Translate/Set Scale origin? Unclear if this is necessary. - SP,
-    // 09-2015
-
-    // Apply the scale
-    std::cerr << "volcart::meshing::Scaling the mesh..." << std::endl;
-    using VCScaleMeshFilter =
-        itk::TransformMeshFilter<ITKMesh, ITKMesh, VC3DScaleType>;
-    auto scaleFilter = VCScaleMeshFilter::New();
-    scaleFilter->SetTransform(scaleTransform);
-    scaleFilter->SetInput(input);
-
-    // This method is deprecated. Need to find a better solution. - SP, 10/2015
-    scaleFilter->SetOutput(output);
-    scaleFilter->Update();
+    // Copy the points and their normals
+    ITKPoint p;
+    ITKPixel n;
+    for (auto pt = input->GetPoints()->Begin(); pt != input->GetPoints()->End();
+         ++pt) {
+        p = pt->Value();
+        input->GetPointData(pt.Index(), &n);
+        p[0] *= scaleFactor;
+        p[1] *= scaleFactor;
+        p[2] *= scaleFactor;
+        output->SetPoint(pt.Index(), p);
+        output->SetPointData(pt.Index(), n);
+    }
+    // Copy the faces
+    ITKCell::CellAutoPointer c;
+    for (auto cell = input->GetCells()->Begin();
+         cell != input->GetCells()->End(); ++cell) {
+        c.TakeOwnership(new ITKTriangle);
+        for (uint32_t pointId = 0; pointId < cell.Value()->GetNumberOfPoints();
+             ++pointId) {
+            c->SetPointId(
+                    pointId, cell.Value()->GetPointIdsContainer()[pointId]);
+        }
+        output->SetCell(cell->Index(), c);
+    }
 };
 }
 }
