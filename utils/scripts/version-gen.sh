9b9c5f3d558c49d3efa985c4708174a4e984ee15
Seth Parker <csparker247@gmail.com>
2017-02-21 11:24:20 -0500

    Fix version-gen script to show revisions since last tag.

--- utils/scripts/version-gen.sh
+++ utils/scripts/version-gen.sh
@@ -5,7 +5,7 @@ versionMajor=`cat CMakeLists.txt | grep "VC VERSION" | sed 's/.*VERSION \([0-9]\
 versionMinor=`cat CMakeLists.txt | grep "VC VERSION" | sed 's/.*VERSION [0-9]\.\([0-9]\).*/\1/'`
 versionPatch=`cat CMakeLists.txt | grep "VC VERSION" | sed 's/.*VERSION [0-9]\.[0-9].\([0-9]\).*/\1/'`
 commitHash=`git log -1 --format="%h"`
-revisioncount=`git log $(git rev-list --tags --max-count=1) ${commitHash} --oneline | wc -l | tr -d ' '`
+revisioncount=`git log $(git rev-list --tags --max-count=1)..${commitHash} --oneline | wc -l | tr -d ' '`
 
 if [[ ${1} == "--short" ]]; then
     echo "${versionMajor}.${versionMinor}.${versionPatch}"
