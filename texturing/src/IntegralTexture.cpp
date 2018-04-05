2571396503a24dd28c5b172b61d1f0075fb93d3e
Seth Parker <csparker247@gmail.com>
2018-04-05 09:56:34 -0400

    (PPM) Add ability to get sorted mapping values (Texturing) Rework all texturing algorithms to generate pixels in a more cache-friendly way

--- texturing/src/IntegralTexture.cpp
+++ texturing/src/IntegralTexture.cpp
@@ -27,40 +27,32 @@ Texture IntegralTexture::compute()
     // Setup the weights
     setup_weights_();
 
+    // Output image
     cv::Mat image = cv::Mat::zeros(height, width, CV_64FC1);
-    for (int y = 0; y < height; ++y) {
-        for (int x = 0; x < width; ++x) {
-            // Skip this pixel if we have no mapping
-            if (!ppm_.hasMapping(y, x)) {
-                continue;
-            }
 
-            // Find the xyz coordinate of the original point
-            auto pixelInfo = ppm_(y, x);
-            cv::Vec3d xyz{pixelInfo[0], pixelInfo[1], pixelInfo[2]};
-            cv::Vec3d xyzNorm{pixelInfo[3], pixelInfo[4], pixelInfo[5]};
+    // Iterate through the mappings
+    auto mappings = ppm_.getSortedMappings();
+    for (const auto& pixel : mappings) {
+        // Generate the neighborhood
+        auto neighborhood = vol_->getVoxelNeighborsLinearInterpolated(
+            pixel.pos, pixel.normal, radius_, interval_, direction_);
 
-            // Generate the neighborhood
-            auto neighborhood = vol_->getVoxelNeighborsLinearInterpolated(
-                xyz, xyzNorm, radius_, interval_, direction_);
-
-            // Clamp values
-            if (clampToMax_) {
-                std::replace_if(
-                    neighborhood.begin(), neighborhood.end(),
-                    [this](uint16_t v) { return v > clampMax_; }, clampMax_);
-            }
-
-            // Convert to double and weight the neighborhood
-            DoublesVec neighborhoodD(neighborhood.begin(), neighborhood.end());
-            auto weighted = apply_weights_(neighborhoodD);
-
-            // Sum the neighborhood
-            auto value = std::accumulate(weighted.begin(), weighted.end(), 0.0);
-
-            // Assign the intensity value at the UV position
-            image.at<double>(y, x) = value;
+        // Clamp values
+        if (clampToMax_) {
+            std::replace_if(
+                neighborhood.begin(), neighborhood.end(),
+                [this](uint16_t v) { return v > clampMax_; }, clampMax_);
         }
+
+        // Convert to double and weight the neighborhood
+        DoublesVec neighborhoodD(neighborhood.begin(), neighborhood.end());
+        auto weighted = apply_weights_(neighborhoodD);
+
+        // Sum the neighborhood
+        auto value = std::accumulate(weighted.begin(), weighted.end(), 0.0);
+
+        // Assign the intensity value at the UV position
+        image.at<double>(pixel.y, pixel.x) = value;
     }
 
     // Scale to uint16_t
