2fb9c433c2a1f0d0d33486917943567165666948
Seth Parker <csparker247@gmail.com>
2022-04-11 20:39:52 +0000

    Replace vc_flip_uv utility with full UV transform utility

--- core/test/UVMapTest.cpp
+++ core/test/UVMapTest.cpp
@@ -6,13 +6,15 @@
 #include "vc/core/types/UVMap.hpp"
 #include "vc/core/util/Logging.hpp"
 
+static constexpr auto PI =
+    double(3.141592653589793238462643383279502884198716939937510582097164L);
+
 using namespace volcart;
 
 /*
- * This fixture builds  a uv map object for each of the test cases that will use
+ * This fixture builds a uv map object for each of the test cases that will use
  * the
  */
-
 class CreateUVMapFixture : public ::testing::Test
 {
 public:
@@ -28,8 +30,8 @@ public:
 
         // Insert mappings relative to the top-left (default origin)
         int pnt_id = 0;
-        for (auto it = _Storage.begin(); it != _Storage.end(); ++it) {
-            _BaseUVMap.set(pnt_id, *it);
+        for (auto& it : _Storage) {
+            _BaseUVMap.set(pnt_id, it);
             ++pnt_id;
         }
     }
@@ -55,7 +57,6 @@ TEST(UVMapTest, GetSetTest)
 // Check the fun origin transformation part of this class
 TEST_F(CreateUVMapFixture, TransformationTest)
 {
-
     // get the original points
     volcart::UVMap map = _BaseUVMap;
     map.setOrigin(UVMap::Origin::TopLeft);  // standard origin
@@ -165,4 +166,88 @@ TEST(UVMapTest, WriteAndRead)
         const auto& uv = m.second;
         EXPECT_EQ(uv, uvMap.get(id));
     }
+}
+
+TEST(UVMapTest, Rotate45)
+{
+    // Construct UV map
+    UVMap uvMap;
+    uvMap.ratio(1., 1.);
+    uvMap.set(0, {0, 0});
+    uvMap.set(1, {1, 0});
+    uvMap.set(2, {0, 1});
+    uvMap.set(3, {1, 1});
+    uvMap.set(4, {0.5, 0.5});
+
+    auto uv45 = uvMap;
+    UVMap::Rotate(uv45, 45. * PI / 180.);
+    EXPECT_EQ(uv45.origin(), uvMap.origin());
+    EXPECT_EQ(uv45.size(), uvMap.size());
+    EXPECT_FLOAT_EQ(uv45.ratio().width, std::sqrt(2.));
+    EXPECT_FLOAT_EQ(uv45.ratio().height, std::sqrt(2.));
+    EXPECT_FLOAT_EQ(uv45.ratio().aspect, 1.);
+    EXPECT_FLOAT_EQ(uv45.get(0)[0], 0.);
+    EXPECT_FLOAT_EQ(uv45.get(0)[1], 0.5);
+    EXPECT_FLOAT_EQ(uv45.get(1)[0], 0.5);
+    EXPECT_FLOAT_EQ(uv45.get(1)[1], 0.);
+    EXPECT_FLOAT_EQ(uv45.get(2)[0], 0.5);
+    EXPECT_FLOAT_EQ(uv45.get(2)[1], 1.);
+    EXPECT_FLOAT_EQ(uv45.get(3)[0], 1.);
+    EXPECT_FLOAT_EQ(uv45.get(3)[1], 0.5);
+    EXPECT_FLOAT_EQ(uv45.get(4)[0], 0.5);
+    EXPECT_FLOAT_EQ(uv45.get(4)[1], 0.5);
+}
+
+TEST(UVMapTest, RotateMultiplesOf90)
+{
+    // Construct UV map
+    UVMap uvMap;
+    uvMap.ratio(10., 5.);
+    uvMap.set(0, {0, 0});
+    uvMap.set(1, {1, 0});
+    uvMap.set(2, {0, 1});
+    uvMap.set(3, {1, 1});
+    uvMap.set(4, {0.5, 0.5});
+
+    // Rotate 90
+    auto uv90 = uvMap;
+    UVMap::Rotate(uv90, UVMap::Rotation::CW90);
+    EXPECT_EQ(uv90.origin(), uvMap.origin());
+    EXPECT_EQ(uv90.size(), uvMap.size());
+    EXPECT_EQ(uv90.ratio().width, uvMap.ratio().height);
+    EXPECT_EQ(uv90.ratio().height, uvMap.ratio().width);
+    EXPECT_EQ(uv90.ratio().aspect, 1. / uvMap.ratio().aspect);
+    EXPECT_EQ(uv90.get(0), cv::Vec2d(1, 0));
+    EXPECT_EQ(uv90.get(1), cv::Vec2d(1, 1));
+    EXPECT_EQ(uv90.get(2), cv::Vec2d(0, 0));
+    EXPECT_EQ(uv90.get(3), cv::Vec2d(0, 1));
+    EXPECT_EQ(uv90.get(4), cv::Vec2d(0.5, 0.5));
+
+    // Rotate 180
+    auto uv180 = uvMap;
+    UVMap::Rotate(uv180, UVMap::Rotation::CW180);
+    EXPECT_EQ(uv180.origin(), uvMap.origin());
+    EXPECT_EQ(uv180.size(), uvMap.size());
+    EXPECT_EQ(uv180.ratio().width, uvMap.ratio().width);
+    EXPECT_EQ(uv180.ratio().height, uvMap.ratio().height);
+    EXPECT_EQ(uv180.ratio().aspect, uvMap.ratio().aspect);
+    EXPECT_EQ(uv180.get(0), cv::Vec2d(1, 1));
+    EXPECT_EQ(uv180.get(1), cv::Vec2d(0, 1));
+    EXPECT_EQ(uv180.get(2), cv::Vec2d(1, 0));
+    EXPECT_EQ(uv180.get(3), cv::Vec2d(0, 0));
+    EXPECT_EQ(uv180.get(4), cv::Vec2d(0.5, 0.5));
+
+    // Rotate 270 (90 CCW)
+    auto uv270 = uvMap;
+    UVMap::Rotate(uv270, UVMap::Rotation::CCW90);
+    EXPECT_EQ(uv270.origin(), uvMap.origin());
+    EXPECT_EQ(uv270.size(), uvMap.size());
+    EXPECT_EQ(uv270.ratio().width, uvMap.ratio().height);
+    EXPECT_EQ(uv270.ratio().height, uvMap.ratio().width);
+    EXPECT_EQ(uv270.ratio().aspect, 1. / uvMap.ratio().aspect);
+    EXPECT_EQ(uv270.get(0), cv::Vec2d(0, 1));
+    EXPECT_EQ(uv270.get(1), cv::Vec2d(0, 0));
+    EXPECT_EQ(uv270.get(2), cv::Vec2d(1, 1));
+    EXPECT_EQ(uv270.get(3), cv::Vec2d(1, 0));
+    EXPECT_EQ(uv270.get(4), cv::Vec2d(0.5, 0.5));
 }
