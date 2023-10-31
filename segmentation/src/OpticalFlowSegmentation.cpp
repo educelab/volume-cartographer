0098749dbf996634f9dc77b5b067c2b99ea3bef9
Philip Allgaier <mail@spacegaier.de>
2023-10-31 10:47:08 +0100

    proper fix for incorrect slice index being returned by OFS (replaces 6708310)

--- segmentation/src/OpticalFlowSegmentation.cpp
+++ segmentation/src/OpticalFlowSegmentation.cpp
@@ -157,7 +157,7 @@ std::vector<std::vector<Voxel>> OpticalFlowSegmentationClass::interpolatePoints(
                 // points[pointsIndex][j] = interpolate_point * points[pointsIndex][j] + interpolate_mastercloud * masterCloud_.getRow(masterRowIndex)[j];
         }
         // evenly space interpolated curve
-        FittedCurve evenlyInterpolationCurve(points[pointsIndex], pointsIndex);
+        FittedCurve evenlyInterpolationCurve(points[pointsIndex], std::round(points[pointsIndex][0][2]));
         points[pointsIndex] = evenlyInterpolationCurve.evenlySpacePoints();
     }
     return points;
