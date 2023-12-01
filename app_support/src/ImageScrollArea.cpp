2b35ae1d01220d63daba7de0a7110d8b1526158d
Stephen Parsons <srpa226@uky.edu>
2023-12-01 10:52:50 -0500

    Use QShortcut

--- app_support/src/ImageScrollArea.cpp
+++ app_support/src/ImageScrollArea.cpp
@@ -1,6 +1,7 @@
 #include "vc/gui_support/ImageScrollArea.hpp"
 
 #include <QGuiApplication>
+#include <QKeySequence>
 #include <QMouseEvent>
 #include <QScrollBar>
 
@@ -16,6 +17,12 @@ vcg::ImageScrollArea::ImageScrollArea()
     setBackgroundRole(QPalette::Dark);
     setWidget(imageLabel_);
     verticalScrollBar()->installEventFilter(this);
+
+    zoomIn_ = new QShortcut(QKeySequence::ZoomIn, this);
+    zoomOut_ = new QShortcut(QKeySequence::ZoomOut, this);
+
+    connect(zoomIn_, &QShortcut::activated, this, &ImageScrollArea::zoom_in_);
+    connect(zoomOut_, &QShortcut::activated, this, &ImageScrollArea::zoom_out_);
 
     this->grabGesture(Qt::PinchGesture);
 }
@@ -130,16 +137,6 @@ auto vcg::ImageScrollArea::event(QEvent* event) -> bool
 {
     if (event->type() == QEvent::Gesture) {
         return gestureEvent(dynamic_cast<QGestureEvent*>(event));
-    }
-    if (event->type() == QEvent::KeyPress) {
-        QKeyEvent* keyEvent = dynamic_cast<QKeyEvent*>(event);
-        if (keyEvent->key() == Qt::Key_Plus) {
-            this->zoom_in_();
-            return true;
-        } else if (keyEvent->key() == Qt::Key_Minus) {
-            this->zoom_out_();
-            return true;
-        }
     }
     return QScrollArea::event(event);
 }
