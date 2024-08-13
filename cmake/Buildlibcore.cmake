2e2411b9261723b9c6352c01eea0a72e24d2f07a
Seth Parker <csparker247@gmail.com>
2024-08-13 14:05:30 -0400

    Update libcore ref

--- cmake/Buildlibcore.cmake
+++ cmake/Buildlibcore.cmake
@@ -1,7 +1,7 @@
 FetchContent_Declare(
     libcore
     GIT_REPOSITORY https://github.com/educelab/libcore.git
-    GIT_TAG add-flag-utils
+    GIT_TAG c061afd
     EXCLUDE_FROM_ALL
 )
 FetchContent_MakeAvailable(libcore)
