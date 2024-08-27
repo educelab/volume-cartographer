bc6d1a191dc2c6e87f418b69c3d0e30227b87dbb
Seth Parker <csparker247@gmail.com>
2024-08-27 13:01:28 -0400

    Restore OFSv1 for base class

--- segmentation/include/vc/segmentation/OpticalFlowSegmentation.hpp
+++ segmentation/include/vc/segmentation/OpticalFlowSegmentation.hpp
@@ -5,14 +5,11 @@
 #include <cstddef>
 #include <cstdint>
 #include <optional>
-#include <shared_mutex>
 
 #include "vc/core/types/OrderedPointSet.hpp"
-#include "vc/core/types/Volume.hpp"
+#include "vc/core/types/VolumePkg.hpp"
 #include "vc/segmentation/ChainSegmentationAlgorithm.hpp"
 #include "vc/segmentation/lrps/FittedCurve.hpp"
-
-namespace fs = volcart::filesystem;
 
 namespace volcart::segmentation
 {
@@ -21,15 +18,12 @@ namespace volcart::segmentation
  *
  * @author    Julian Schilliger
  * @date      May 2023
- * @copyright 2023 Julian Schilliger, MIT License.
- *
+ * @copyright MIT License.
  *
  * This algorithm propagates a chain of points forward through a volume from a
  * starting z-index to an ending z-index (inclusive). It uses optical flow to
  * track the shape of the layer. Each seed point is assumed to be placed within
  * the layer rather than on its surface boundary.
- *
- * Contribution as part of the _Vesuvius Challenge 2023_.
  *
  * @warning This algorithm is non-deterministic and yields slightly different
  * results each run.
@@ -56,159 +50,68 @@ public:
             std::forward<Args>(args)...);
     }
 
-    /** @brief Set the start z-index */
-    void setStartZIndex(int z) { startIndex_ = z; }
-
-    /** @brief Get the start z-index */
-    int getStartZIndex() { return startIndex_; }
-
-    /** @brief Get the target z-index */
-    int getTargetZIndex() { return endIndex_; }
-
     /** @brief Set the target z-index */
-    void setTargetZIndex(int z) { endIndex_ = z; }
-
-    /** @brief Set the number of curve optimization iterations per step */
-    void setOptimizationIterations(int n) { numIters_ = n; }
+    void setTargetZIndex(int z);
 
     /**
-     * @brief Set the threshold of what pixel brightness is considered inside a sheet (higher as threshold) and outside (lower as threshold)
+     * @brief Set the threshold of what pixel brightness is considered inside a
+     * sheet (higher as threshold) and outside (lower as threshold)
      */
-    void setOutsideThreshold(int outside) { outside_threshold_ = outside; }
+    void setOutsideThreshold(std::uint8_t outside);
 
     /**
-     * @brief Set the threshold of what pixel brightness is considered while calculating optical flow, darker pixels OF is interpolated from brighter ones in the area
+     * @brief Set the threshold of what pixel brightness is considered while
+     * calculating optical flow, darker pixels OF is interpolated from brighter
+     * ones in the area
      */
-    void setOFThreshold(int of_thr) { optical_flow_pixel_threshold_ = of_thr; }
-
-    /** @brief Set whether to enable outlier points smoothening
-     */
-    void setEnableSmoothenOutlier(bool enable_smoothen_outlier) { enable_smoothen_outlier_ = enable_smoothen_outlier; }
-    /**@}*/
-
-    /** @brief Set whether to enable edge detection
-     */
-    void setEnableEdge(bool enable_edge) { enable_edge_ = enable_edge; }
-    /**@}*/
+    void setOFThreshold(std::uint8_t ofThr);
 
     /**
-     * @brief Set the threshold of what pixel brightness is considered while calculating optical flow, darker pixels OF is interpolated from brighter ones in the area
+     * @brief Set the maximum single pixel optical flow displacement before
+     * interpolating a pixel region
      */
-    void setEdgeJumpDistance(int jump_dist) { edge_jump_distance_ = jump_dist; }
+    void setOFDispThreshold(std::uint32_t ofDispThrs);
 
     /**
-     * @brief Set the threshold of what pixel brightness is considered while calculating optical flow, darker pixels OF is interpolated from brighter ones in the area
+     * @brief Set the threshold for what pixel brightness is considered as
+     * being outside the sheet. Pixels above this threshold are considered
+     * outside the sheet and are smoothed in an attempt to get them tracking
+     * the sheet again.
      */
-    void setEdgeBounceDistance(int bounce_dist) { edge_bounce_distance_ = bounce_dist; }
+    void setSmoothBrightnessThreshold(std::uint8_t brightness);
 
     /**
      * @brief Set the estimated thickness of the substrate (in um)
      *
      * Used to generate the radius of the structure tensor calculation
      */
-    void setMaterialThickness(double m) { materialThickness_ = m; }
+    void setMaterialThickness(double m);
 
-    /** @brief Set the maximum single pixel optical flow displacement before interpolating a pixel region */
-    void setOFDispThreshold(int of_disp_thrs) { optical_flow_displacement_threshold_ = of_disp_thrs; }
+    /** @brief Set the maximum number of threads */
+    void setMaxThreads(std::uint32_t t);
 
-    /** @brief Set whether to purge cache
-     */
-    void setPurgeCache(bool purge_c) { purge_cache_ = purge_c; }
-    /**@}*/
+    /** @brief Clear the maximum number of threads */
+    void resetMaxThreads();
 
-    /** @brief Set how many slices should be cached
-     */
-    void setCacheSlices(int cache_slices) { nr_cache_slices_ = cache_slices; }
-
-    /** @brief
-     */
-    void setSmoothBrightnessThreshold(int brightness) { smoothen_by_brightness_ = brightness; }
-
-    /** @brief Set how wide the interpolation window should be
-     */
-    void setInterpolationWindow(int window) { smoothness_interpolation_window_ = window; }
-
-    /** @brief Get how wide the interpolation window should be
-     */
-    int getInterpolationWindow() { return smoothness_interpolation_window_; }
-
-    /** @brief Set how many slices the interpolation center is away from the start slice
-     */
-    void setInterpolationDistance(int distance) { smoothness_interpolation_distance_ = distance; }
-
-    /** @brief Get how many slices the interpolation center is away from the start slice
-     */
-    int getInterpolationDistance() { return smoothness_interpolation_distance_; }
-
-    /** @brief Set the already computed masterCloud OrderedPointSet
-     */
-    void setOrderedPointSet(volcart::OrderedPointSet<cv::Vec3d> masterCloud) { masterCloud_ = masterCloud; }
-
-    /** @brief Set the input chain of re-segmentation points */
-    void setReSegmentationChain(Chain c) { reSegStartingChain_ = std::move(c); }
-
-    /** @brief Interpolate the points with the existing masterCloud OrderedPointSet to get a smooth final surface
-     */
-    std::vector<std::vector<Voxel>> interpolateWithMasterCloud(std::vector<std::vector<Voxel>> points, int window_size, bool backwards);
-
-    /** @brief Interpolate the gaps in the result point set
-     */
-    std::vector<std::vector<Voxel>> interpolateGaps(std::vector<std::vector<Voxel>> points);
-
-    /**@{*/
-    /** @brief Compute the segmentation 1 Line */
-    std::vector<Voxel> computeCurve(FittedCurve currentCurve, Chain& currentVs, int zIndex, int stepSize, int startIndex, bool backwards=false);
-    /**@}*/
-
-    /**@{*/
-    /** @brief Compute the segmentation */
-    PointSet compute() override;
-    /**@}*/
-
-    /**@{*/
     /** Debug: Shows intensity maps in GUI window */
-    void setVisualize(bool b) { visualize_ = b; }
+    void setVisualize(bool b);
 
     /** Debug: Dumps reslices and intensity maps to disk */
-    void setDumpVis(bool b) { dumpVis_ = b; }
+    void setDumpVis(bool b);
+
+    /** @brief Compute the segmentation */
+    auto compute() -> PointSet override;
 
     /** @brief Returns the maximum progress value */
     [[nodiscard]] auto progressIterations() const -> std::size_t override;
 
 private:
     /**
-     * @brief Estimate the normal to the curve at point index
-     * @param currentCurve Input curve
-     * @param index Index of point on curve
+     * @brief Compute the curve for z + 1 given a curve on z using the optical
+     * flow between the two slices
      */
-    auto estimate_normal_at_index_(const FittedCurve& currentCurve, int index)
-        -> cv::Vec3d;
-
-    /**
-     * @brief Estimate the 2D normal to the curve at point index in the z plane
-     * @param curve Input curve
-     * @param index Index of point on curve
-     */
-    auto estimate_2d_normal_at_index_(const volcart::segmentation::FittedCurve& curve, int index)
-        -> cv::Vec2f;
-
-    /**
-     * @brief Calculate the mean pixel value in a window around a point
-     * @param integral_img Intergral image of slice
-     * @param pt Point around which to calculate mean
-     * @param window_size Size of window
-     */
-    auto get_mean_pixel_value(const cv::Mat& integral_img, const cv::Point& pt, int window_size)
-        -> float;
-
-    /**
-     * @brief Calculate the mean pixel value in a window around a point
-     * @param integral_img Intergral image of slice
-     * @param pt Point around which to calculate mean
-     * @param window_size Size of window
-     */
-    auto compute_moving_average(const std::vector<Voxel>& points, int index, int window_size)
-        -> Voxel;
+    auto compute_curve_(const FittedCurve& currentCurve, int zIndex)
+        -> std::vector<Voxel>;
 
     /**
      * @brief Debug: Draw curve on slice image
@@ -223,69 +126,51 @@ private:
         int particleIndex = -1,
         bool showSpline = false) const -> cv::Mat;
 
-    /**
-     * @brief Debug: Draw curve on slice image
-     * @param curve Input curve
-     * @param pkgSlice Image to draw on
-     * @param particleIndex Highlight point at particleIndex
-     * @param showSpline Draw interpolated curve. Default only draws points
-     */
-    [[nodiscard]] auto draw_particle_on_image_(
-        const FittedCurve& curve,
-        cv::Mat pkgSlice,
-        int particleIndex = -1,
-        bool showSpline = false) const -> cv::Mat;
-
     /** @brief Convert the internal storage array into a final PointSet */
     auto create_final_pointset_(const std::vector<std::vector<Voxel>>& points)
         -> PointSet;
 
-    /** Default minimum energy gradient */
-    constexpr static double DEFAULT_MIN_ENERGY_GRADIENT = 1e-7;
-
-    /**
-     * Compute a sub portion of the algorithm, e.g. the regular "forward" portion or the backwards re-segmentation.
-     * @param startChainIndex index of the starting chain (!= first slice for which we are calculating a curve) that is used as input for the algorithm
-     * @param endChainIndex index of the last chain (used for clamping the valid values in the method)
-     * @param endIndex index until which the algorithm should run
-     * @param initialStepAdjustment is required for the re-segmentation run in case of step sizes that do not directly hit the target anchor
-    */
-    auto computeSub(std::vector<std::vector<Voxel>>& points, Chain currentVs, int startChainIndex, int endChainIndex, int endIndex, int initialStepAdjustment, bool backwards, size_t& iteration, bool insertFront, const fs::path outputDir, const fs::path wholeChainDir)
-        -> ChainSegmentationAlgorithm::Status;
-
-    /** Start z-index */
-    int startIndex_{0};
     /** Target z-index */
     int endIndex_{0};
-    /** Darker pixels are considered outside the sheet */
-    int outside_threshold_{60};
-    /** Disregarding pixel that are darker during optical flow computation */
-    int optical_flow_pixel_threshold_{80};
-    /** Threshold of how many pixel optical flow can displace a point, if higher, recompute optical flow with region's average flow */
-    int optical_flow_displacement_threshold_{10};
-    /** Purging Cache flag */
-    bool purge_cache_{false};
+    /**
+     * Darker pixels are considered outside the sheet. This parameter sets the
+     * threshold of what pixel brightness is considered too deep inside a sheet
+     * (higher than the threshold) and then tries to smoothen those points back
+     * towards the edge of the sheet. Range: 0-255.
+     */
+    std::uint8_t outsideThreshold_{80};
+    /**
+     * Disregarding pixel that are darker during optical flow computation. This
+     * parameter sets the threshold for what pixel brightness is considered
+     * while calculating optical flow. Darker pixels' optical flow is
+     * interpolated from brighter ones in the area. Range: 0-255. Higher values
+     * disregard more dark pixels during computation, while lower values
+     * include more dark pixels.
+     */
+    std::uint8_t opticalFlowPixelThreshold_{80};
+    /**
+     * Threshold of how many pixel optical flow can displace a point, if
+     * higher, recompute optical flow with region's average flow. This
+     * parameter sets the maximum single pixel optical flow displacement before
+     * interpolating a pixel region. Range minimum: 0. Higher values allow more
+     * displacement before interpolation, while lower values trigger
+     * interpolation more frequently.
+     */
+    std::uint32_t opticalFlowDisplacementThreshold_{10};
+    /**
+     * This parameter sets the threshold for what pixel brightness is considered
+     * as being outside the sheet. Pixels considered outside the sheet are
+     * smoothed in an attempt to get them tracking the sheet again.
+     * Range: 0-255. Smooth curve at pixels above this threshold.
+     */
+    std::uint8_t smoothByBrightness_{180};
+    /** Estimated material thickness in um */
+    double materialThickness_{100};
+    /** Maximum number of threads */
+    std::optional<std::uint32_t> maxThreads_;
     /** Dump visualization to disk flag */
     bool dumpVis_{false};
     /** Show visualization in GUI flag */
     bool visualize_{false};
-    /** Number of curve optimization iterations */
-    int numIters_{15};
-    /** Estimated material thickness in um */
-    double materialThickness_{100};
-    /** Number of slices to cache */
-    int nr_cache_slices_{300};
-    /** Number of slices to cache */
-    int smoothen_by_brightness_{180};
-    bool enable_smoothen_outlier_{true};
-    bool enable_edge_{false};
-    int edge_jump_distance_{6};
-    int edge_bounce_distance_{3};
-    bool interpolate_master_cloud{true};
-    int smoothness_interpolation_window_{5}; //  window for interpolation (number if slices from interpolation distance/center in either direction)
-    int smoothness_interpolation_distance_{25}; // distance from start slice where the interpolation center is
-    Chain reSegStartingChain_;
-    volcart::OrderedPointSet<cv::Vec3d> masterCloud_;
-    mutable std::shared_mutex display_mutex_;
 };
 }  // namespace volcart::segmentation
---
+++ segmentation/include/vc/segmentation/OpticalFlowSegmentationV2.hpp
@@ -0,0 +1,291 @@
+#pragma once
+
+/** @file */
+
+#include <cstddef>
+#include <cstdint>
+#include <optional>
+#include <shared_mutex>
+
+#include "vc/core/types/OrderedPointSet.hpp"
+#include "vc/core/types/Volume.hpp"
+#include "vc/segmentation/ChainSegmentationAlgorithm.hpp"
+#include "vc/segmentation/lrps/FittedCurve.hpp"
+
+namespace fs = volcart::filesystem;
+
+namespace volcart::segmentation
+{
+/**
+ * @brief Optical Flow Segmentation
+ *
+ * @author    Julian Schilliger
+ * @date      May 2023
+ * @copyright 2023 Julian Schilliger, MIT License.
+ *
+ *
+ * This algorithm propagates a chain of points forward through a volume from a
+ * starting z-index to an ending z-index (inclusive). It uses optical flow to
+ * track the shape of the layer. Each seed point is assumed to be placed within
+ * the layer rather than on its surface boundary.
+ *
+ * Contribution as part of the _Vesuvius Challenge 2023_.
+ *
+ * @warning This algorithm is non-deterministic and yields slightly different
+ * results each run.
+ *
+ * @ingroup Segmentation
+ */
+class OpticalFlowSegmentation : public ChainSegmentationAlgorithm
+{
+public:
+    /** Pointer */
+    using Pointer = std::shared_ptr<OpticalFlowSegmentation>;
+
+    /** @brief Default constructor */
+    OpticalFlowSegmentation() = default;
+
+    /** Default destructor */
+    ~OpticalFlowSegmentation() override = default;
+
+    /** Make a new shared instance */
+    template <typename... Args>
+    static auto New(Args... args) -> Pointer
+    {
+        return std::make_shared<OpticalFlowSegmentation>(
+            std::forward<Args>(args)...);
+    }
+
+    /** @brief Set the start z-index */
+    void setStartZIndex(int z) { startIndex_ = z; }
+
+    /** @brief Get the start z-index */
+    int getStartZIndex() { return startIndex_; }
+
+    /** @brief Get the target z-index */
+    int getTargetZIndex() { return endIndex_; }
+
+    /** @brief Set the target z-index */
+    void setTargetZIndex(int z) { endIndex_ = z; }
+
+    /** @brief Set the number of curve optimization iterations per step */
+    void setOptimizationIterations(int n) { numIters_ = n; }
+
+    /**
+     * @brief Set the threshold of what pixel brightness is considered inside a sheet (higher as threshold) and outside (lower as threshold)
+     */
+    void setOutsideThreshold(int outside) { outside_threshold_ = outside; }
+
+    /**
+     * @brief Set the threshold of what pixel brightness is considered while calculating optical flow, darker pixels OF is interpolated from brighter ones in the area
+     */
+    void setOFThreshold(int of_thr) { optical_flow_pixel_threshold_ = of_thr; }
+
+    /** @brief Set whether to enable outlier points smoothening
+     */
+    void setEnableSmoothenOutlier(bool enable_smoothen_outlier) { enable_smoothen_outlier_ = enable_smoothen_outlier; }
+    /**@}*/
+
+    /** @brief Set whether to enable edge detection
+     */
+    void setEnableEdge(bool enable_edge) { enable_edge_ = enable_edge; }
+    /**@}*/
+
+    /**
+     * @brief Set the threshold of what pixel brightness is considered while calculating optical flow, darker pixels OF is interpolated from brighter ones in the area
+     */
+    void setEdgeJumpDistance(int jump_dist) { edge_jump_distance_ = jump_dist; }
+
+    /**
+     * @brief Set the threshold of what pixel brightness is considered while calculating optical flow, darker pixels OF is interpolated from brighter ones in the area
+     */
+    void setEdgeBounceDistance(int bounce_dist) { edge_bounce_distance_ = bounce_dist; }
+
+    /**
+     * @brief Set the estimated thickness of the substrate (in um)
+     *
+     * Used to generate the radius of the structure tensor calculation
+     */
+    void setMaterialThickness(double m) { materialThickness_ = m; }
+
+    /** @brief Set the maximum single pixel optical flow displacement before interpolating a pixel region */
+    void setOFDispThreshold(int of_disp_thrs) { optical_flow_displacement_threshold_ = of_disp_thrs; }
+
+    /** @brief Set whether to purge cache
+     */
+    void setPurgeCache(bool purge_c) { purge_cache_ = purge_c; }
+    /**@}*/
+
+    /** @brief Set how many slices should be cached
+     */
+    void setCacheSlices(int cache_slices) { nr_cache_slices_ = cache_slices; }
+
+    /** @brief
+     */
+    void setSmoothBrightnessThreshold(int brightness) { smoothen_by_brightness_ = brightness; }
+
+    /** @brief Set how wide the interpolation window should be
+     */
+    void setInterpolationWindow(int window) { smoothness_interpolation_window_ = window; }
+
+    /** @brief Get how wide the interpolation window should be
+     */
+    int getInterpolationWindow() { return smoothness_interpolation_window_; }
+
+    /** @brief Set how many slices the interpolation center is away from the start slice
+     */
+    void setInterpolationDistance(int distance) { smoothness_interpolation_distance_ = distance; }
+
+    /** @brief Get how many slices the interpolation center is away from the start slice
+     */
+    int getInterpolationDistance() { return smoothness_interpolation_distance_; }
+
+    /** @brief Set the already computed masterCloud OrderedPointSet
+     */
+    void setOrderedPointSet(volcart::OrderedPointSet<cv::Vec3d> masterCloud) { masterCloud_ = masterCloud; }
+
+    /** @brief Set the input chain of re-segmentation points */
+    void setReSegmentationChain(Chain c) { reSegStartingChain_ = std::move(c); }
+
+    /** @brief Interpolate the points with the existing masterCloud OrderedPointSet to get a smooth final surface
+     */
+    std::vector<std::vector<Voxel>> interpolateWithMasterCloud(std::vector<std::vector<Voxel>> points, int window_size, bool backwards);
+
+    /** @brief Interpolate the gaps in the result point set
+     */
+    std::vector<std::vector<Voxel>> interpolateGaps(std::vector<std::vector<Voxel>> points);
+
+    /**@{*/
+    /** @brief Compute the segmentation 1 Line */
+    std::vector<Voxel> computeCurve(FittedCurve currentCurve, Chain& currentVs, int zIndex, int stepSize, int startIndex, bool backwards=false);
+    /**@}*/
+
+    /**@{*/
+    /** @brief Compute the segmentation */
+    PointSet compute() override;
+    /**@}*/
+
+    /**@{*/
+    /** Debug: Shows intensity maps in GUI window */
+    void setVisualize(bool b) { visualize_ = b; }
+
+    /** Debug: Dumps reslices and intensity maps to disk */
+    void setDumpVis(bool b) { dumpVis_ = b; }
+
+    /** @brief Returns the maximum progress value */
+    [[nodiscard]] auto progressIterations() const -> std::size_t override;
+
+private:
+    /**
+     * @brief Estimate the normal to the curve at point index
+     * @param currentCurve Input curve
+     * @param index Index of point on curve
+     */
+    auto estimate_normal_at_index_(const FittedCurve& currentCurve, int index)
+        -> cv::Vec3d;
+
+    /**
+     * @brief Estimate the 2D normal to the curve at point index in the z plane
+     * @param curve Input curve
+     * @param index Index of point on curve
+     */
+    auto estimate_2d_normal_at_index_(const volcart::segmentation::FittedCurve& curve, int index)
+        -> cv::Vec2f;
+
+    /**
+     * @brief Calculate the mean pixel value in a window around a point
+     * @param integral_img Intergral image of slice
+     * @param pt Point around which to calculate mean
+     * @param window_size Size of window
+     */
+    auto get_mean_pixel_value(const cv::Mat& integral_img, const cv::Point& pt, int window_size)
+        -> float;
+
+    /**
+     * @brief Calculate the mean pixel value in a window around a point
+     * @param integral_img Intergral image of slice
+     * @param pt Point around which to calculate mean
+     * @param window_size Size of window
+     */
+    auto compute_moving_average(const std::vector<Voxel>& points, int index, int window_size)
+        -> Voxel;
+
+    /**
+     * @brief Debug: Draw curve on slice image
+     * @param curve Input curve
+     * @param sliceIndex %Slice on which to draw
+     * @param particleIndex Highlight point at particleIndex
+     * @param showSpline Draw interpolated curve. Default only draws points
+     */
+    [[nodiscard]] auto draw_particle_on_slice_(
+        const FittedCurve& curve,
+        int sliceIndex,
+        int particleIndex = -1,
+        bool showSpline = false) const -> cv::Mat;
+
+    /**
+     * @brief Debug: Draw curve on slice image
+     * @param curve Input curve
+     * @param pkgSlice Image to draw on
+     * @param particleIndex Highlight point at particleIndex
+     * @param showSpline Draw interpolated curve. Default only draws points
+     */
+    [[nodiscard]] auto draw_particle_on_image_(
+        const FittedCurve& curve,
+        cv::Mat pkgSlice,
+        int particleIndex = -1,
+        bool showSpline = false) const -> cv::Mat;
+
+    /** @brief Convert the internal storage array into a final PointSet */
+    auto create_final_pointset_(const std::vector<std::vector<Voxel>>& points)
+        -> PointSet;
+
+    /** Default minimum energy gradient */
+    constexpr static double DEFAULT_MIN_ENERGY_GRADIENT = 1e-7;
+
+    /**
+     * Compute a sub portion of the algorithm, e.g. the regular "forward" portion or the backwards re-segmentation.
+     * @param startChainIndex index of the starting chain (!= first slice for which we are calculating a curve) that is used as input for the algorithm
+     * @param endChainIndex index of the last chain (used for clamping the valid values in the method)
+     * @param endIndex index until which the algorithm should run
+     * @param initialStepAdjustment is required for the re-segmentation run in case of step sizes that do not directly hit the target anchor
+    */
+    auto computeSub(std::vector<std::vector<Voxel>>& points, Chain currentVs, int startChainIndex, int endChainIndex, int endIndex, int initialStepAdjustment, bool backwards, size_t& iteration, bool insertFront, const fs::path outputDir, const fs::path wholeChainDir)
+        -> ChainSegmentationAlgorithm::Status;
+
+    /** Start z-index */
+    int startIndex_{0};
+    /** Target z-index */
+    int endIndex_{0};
+    /** Darker pixels are considered outside the sheet */
+    int outside_threshold_{60};
+    /** Disregarding pixel that are darker during optical flow computation */
+    int optical_flow_pixel_threshold_{80};
+    /** Threshold of how many pixel optical flow can displace a point, if higher, recompute optical flow with region's average flow */
+    int optical_flow_displacement_threshold_{10};
+    /** Purging Cache flag */
+    bool purge_cache_{false};
+    /** Dump visualization to disk flag */
+    bool dumpVis_{false};
+    /** Show visualization in GUI flag */
+    bool visualize_{false};
+    /** Number of curve optimization iterations */
+    int numIters_{15};
+    /** Estimated material thickness in um */
+    double materialThickness_{100};
+    /** Number of slices to cache */
+    int nr_cache_slices_{300};
+    /** Number of slices to cache */
+    int smoothen_by_brightness_{180};
+    bool enable_smoothen_outlier_{true};
+    bool enable_edge_{false};
+    int edge_jump_distance_{6};
+    int edge_bounce_distance_{3};
+    bool interpolate_master_cloud{true};
+    int smoothness_interpolation_window_{5}; //  window for interpolation (number if slices from interpolation distance/center in either direction)
+    int smoothness_interpolation_distance_{25}; // distance from start slice where the interpolation center is
+    Chain reSegStartingChain_;
+    volcart::OrderedPointSet<cv::Vec3d> masterCloud_;
+    mutable std::shared_mutex display_mutex_;
+};
+}  // namespace volcart::segmentation
