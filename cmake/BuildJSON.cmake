17d42109b01507e7b5a8337a161225ae59fb8d70
Seth Parker <csparker247@gmail.com>
2025-01-23 08:12:47 -0500

    Simplify fetchcontent builds

--- cmake/BuildJSON.cmake
+++ cmake/BuildJSON.cmake
@@ -1,18 +1,14 @@
-option(VC_BUILD_JSON "Build in-source JSON library" on)
+option(VC_BUILD_JSON "Build in-source JSON library" off)
 if(VC_BUILD_JSON)
     FetchContent_Declare(
         json
         URL https://github.com/nlohmann/json/archive/v3.11.3.tar.gz
         DOWNLOAD_EXTRACT_TIMESTAMP ON
+        EXCLUDE_FROM_ALL
     )
-
-    FetchContent_GetProperties(json)
-    if(NOT json_POPULATED)
-        set(JSON_BuildTests OFF CACHE INTERNAL "")
-        set(JSON_Install ON CACHE INTERNAL "")
-        FetchContent_Populate(json)
-        add_subdirectory(${json_SOURCE_DIR} ${json_BINARY_DIR} EXCLUDE_FROM_ALL)
-    endif()
+    set(JSON_BuildTests OFF CACHE INTERNAL "")
+    set(JSON_Install ON CACHE INTERNAL "")
+    FetchContent_MakeAvailable(json)
 else()
     find_package(nlohmann_json 3.9.1 REQUIRED)
 endif()
