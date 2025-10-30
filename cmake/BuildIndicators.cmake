bcb6253a0fe7af698cd3fb18791f9fe51016411b
Seth P <csparker247@gmail.com>
2025-10-30 12:51:58 -0400

    Patch the CMake minimum required version for in-source dependencies (#126)

--- cmake/BuildIndicators.cmake
+++ cmake/BuildIndicators.cmake
@@ -2,6 +2,7 @@ FetchContent_Declare(
     indicators
     GIT_REPOSITORY https://github.com/p-ranav/indicators.git
     GIT_TAG 222382c
+    PATCH_COMMAND patch -p1 -i ${CMAKE_SOURCE_DIR}/cmake/patches/Indicators-CMakeMinVer.patch
     EXCLUDE_FROM_ALL
 )
 FetchContent_MakeAvailable(indicators)
--- cmake/Buildbvh.cmake
+++ cmake/Buildbvh.cmake
@@ -2,6 +2,7 @@ FetchContent_Declare(
   bvh
   GIT_REPOSITORY https://github.com/madmann91/bvh.git
   GIT_TAG 2fd0db6
+  PATCH_COMMAND patch -p1 -i ${CMAKE_SOURCE_DIR}/cmake/patches/bvh-CMakeMinVer.patch
   EXCLUDE_FROM_ALL
 )
 FetchContent_MakeAvailable(bvh)
