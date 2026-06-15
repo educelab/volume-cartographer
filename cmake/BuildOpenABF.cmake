33702e8c71ece838f9c12df2f8ee755b15a04a63
csparker247 <1434526+csparker247@users.noreply.github.com>
2026-06-15 16:53:27 +0000

    ci: update OpenABF GIT_TAG to v2.1.0

--- cmake/BuildOpenABF.cmake
+++ cmake/BuildOpenABF.cmake
@@ -1,7 +1,7 @@
 FetchContent_Declare(
     OpenABF
     GIT_REPOSITORY https://github.com/educelab/OpenABF.git
-    GIT_TAG 53490b386e55459588b9f255be9afac45de646fb
+    GIT_TAG v2.1.0
     EXCLUDE_FROM_ALL
 )
 FetchContent_MakeAvailable(OpenABF)
